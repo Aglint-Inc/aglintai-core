f262ac384f53dfc3ab000674b55d265b70a12a67
Ravi K <ravi@aglinthq.com>
2023-11-09 16:56:26 +0530

    Added a logic to view doc docx file using gview. need testing

--- src/components/JobApplicationsDashboard/ApplicationCard/ApplicationDetails/ResumePreviewer/index.tsx
+++ src/components/JobApplicationsDashboard/ApplicationCard/ApplicationDetails/ResumePreviewer/index.tsx
@@ -1,15 +1,31 @@
 function ResumePreviewer({ url }: { url: string }) {
-  return (
+  const isDocOrDocx = /\.(doc|docx)$/i.test(url);
+  const iframeSrc = isDocOrDocx ? `https://docs.google.com/gview?url=${url}&embedded=true` : '';
+
+  // Style for both iframe and embed tags
+  const commonStyle = {
+    width: '100%',
+    height: '95vh',
+    border: '0',
+  };
+
+  // Decide which component to render based on the file extension
+  const fileViewer = isDocOrDocx ? (
+    <iframe
+      src={iframeSrc}
+      style={commonStyle}
+      frameBorder="0"
+      title='Document Preview'
+    ></iframe>
+  ) : (
     <embed
-      style={{
-        height: '95vh',
-        border: '0',
-      }}
-      width='100%'
       src={url}
-      title='Resume Preview'
+      style={commonStyle}
+      title='PDF Preview'
     ></embed>
   );
+
+  return <>{fileViewer}</>;
 }
 
 export default ResumePreviewer;
