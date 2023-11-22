f7b757cd33c59079a29cc6a3a750114d8da3ccba
chinmaicraglint <chinmai@aglinthq.com>
2023-11-22 18:50:00 +0530

    fixed overview api for cron job

--- src/pages/api/google/overview-handler.ts
+++ src/pages/api/google/overview-handler.ts
@@ -1,26 +1,27 @@
 /* eslint-disable security/detect-object-injection */
 /* eslint-disable no-console */
-// import axios from 'axios';
+import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    console.log(req.body);
-    // if (req.body && Array.isArray(req.body)) {
-    //   const applications = req.body;
-    //   applications.map((application) => {
-    //     axios.post(
-    //       `${process.env.NEXT_PUBLIC_HOTS_NAME}/api/overview/overview-palm`,
-    //       { application: application },
-    //     );
-    //   });
-    // } else {
-    //   console.log('missing req body');
-    //   res.status(400).send('missing req body');
-    // }
+    if (req.body && Array.isArray(req.body)) {
+      console.log(req.body.length, 'applications length');
+
+      const applications = req.body;
+      applications.map((application) => {
+        axios.post(
+          `${process.env.NEXT_PUBLIC_HOST_NAME}/api/overview/overview-palm`,
+          { application: application },
+        );
+      });
+    } else {
+      console.log('missing req body');
+      return res.status(400).send('missing req body');
+    }
   } catch (error) {
     console.log(error, req.body.application_id);
-    res.status(500).send(error.message);
+    return res.status(500).send(error.message);
   }
 };
 
