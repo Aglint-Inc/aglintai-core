1533e05f53651ab90599f3f973ed96123031a44c
chinmai <chinmai@aglinhq.com>
2023-12-04 21:19:28 +0530

    middleware fix for shby apis

--- src/middleware.js
+++ src/middleware.js
@@ -29,7 +29,7 @@ const whiteListedEndPoints = [
   '/api/resumecron/batchscore',
   '/api/greenhouse/batchsave',
   '/api/resumeScoring',
-  '/api/:function*',
+  '/api/ashby/createapplication',
 ];
 
 export async function middleware(request) {
