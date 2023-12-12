71139ce83ab398d0ab2197344f3059c99c977f40
Ogyen Thoga <ogyen@aglinthq.com>
2023-12-12 06:31:57 +0000

    Merged in devlink/webflowFix (pull request #401)
    Fixed: Webflow bug fixed
    
    * Fixed: Webflow bug fixed
    
    
    Approved-by: Admin Aglint

--- src/components/Interview/InterviewInstructions/index.tsx
+++ src/components/Interview/InterviewInstructions/index.tsx
@@ -219,6 +219,9 @@ function InterviewInstructions() {
                 sx={{
                   width: '50px',
                   height: '50px',
+                  '& > img': {
+                    objectFit: 'contain',
+                  },
                 }}
               />
             }
