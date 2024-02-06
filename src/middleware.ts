70c76fb75163b6e5906362d0825269c3ddfc37c0
Chinmai C R <chinmai@aglinthq.com>
2024-02-06 07:56:15 +0000

    Merged in feature/google-indexing (pull request #640)
    Feature/google indexing
    
    * api created for indexing google
    
    * job post indexing progress
    
    * job post indexing progress

--- src/middleware.ts
+++ src/middleware.ts
@@ -120,10 +120,10 @@ const allowedPaths = new Set([
   '/api/jobApplications/candidateUpload/manualUpload',
   '/api/jobApplications/candidateUpload/resumeUpload',
   '/api/jobApplications/read',
-
   '/api/job-assistant/createThread',
-
   '/api/job-assistant/cluoud-functions/assistant',
+  '/api/jobpost/indexing',
+  '/api/sitemap.xml',
 ]);
 
 const isAllowedPaths = (reqUrl = '') => {
