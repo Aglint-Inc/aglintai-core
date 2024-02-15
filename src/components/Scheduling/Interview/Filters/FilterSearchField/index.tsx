95ccd11613dd9f8e8455797b03d9f44b5285c0d8
Chinmai <chinmai@aglinthq.com>
2024-02-15 16:15:23 +0530

    filter ui fix

--- src/components/Scheduling/Interview/Filters/FilterSearchField/index.tsx
+++ src/components/Scheduling/Interview/Filters/FilterSearchField/index.tsx
@@ -1,5 +1,7 @@
+import { InputAdornment } from '@mui/material';
 import React from 'react';
 
+import Icon from '@/src/components/Common/Icons/Icon';
 import UITextField from '@/src/components/Common/UITextField';
 
 import { setFilter } from '../../store';
@@ -8,7 +10,23 @@ function FilterSearchField() {
   return (
     <>
       <UITextField
-        placeholder='Search in all interviews'
+        rest={{
+          style: {
+            borderRadius: '41px',
+          },
+        }}
+        InputProps={{
+          sx: {
+            borderRadius: '10px',
+            minWidth: '250px',
+          },
+          endAdornment: (
+            <InputAdornment position='end'>
+              <Icon variant='JobSearch' height='14' />
+            </InputAdornment>
+          ),
+        }}
+        placeholder='Search by name'
         onChange={(e) => {
           setFilter({ textSearch: e.target.value });
         }}
