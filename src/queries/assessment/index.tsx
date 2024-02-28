71e31ec44a9ee9ef6d4030d107c551437533440c
Punith G <punithg@aglinthq.com>
2024-02-28 11:26:15 +0530

    fix: remove all settled queries

--- src/queries/assessment/index.tsx
+++ src/queries/assessment/index.tsx
@@ -65,10 +65,10 @@ export const useCreateAssessment = () => {
         data,
       ]);
     },
-    onSettled: async () => {
-      await queryClient.cancelQueries({ queryKey });
-      queryClient.invalidateQueries({ queryKey });
-    },
+    // onSettled: async () => {
+    //   await queryClient.cancelQueries({ queryKey });
+    //   queryClient.invalidateQueries({ queryKey });
+    // },
   });
   return {
     mutation,
@@ -105,10 +105,10 @@ export const useEditAssessment = () => {
         context.previousAssessments,
       );
     },
-    onSettled: async () => {
-      await queryClient.cancelQueries({ queryKey });
-      queryClient.invalidateQueries({ queryKey });
-    },
+    // onSettled: async () => {
+    //   await queryClient.cancelQueries({ queryKey });
+    //   queryClient.invalidateQueries({ queryKey });
+    // },
   });
   return {
     mutation,
