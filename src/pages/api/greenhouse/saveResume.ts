5b403703354dc2939dea9a572f30dd44acbbad5b
Chinmai <chinmai@aglinthq.com>
2024-03-22 20:46:13 +0530

    fix apistatus for null resume in ats intergrations

--- src/pages/api/greenhouse/saveResume.ts
+++ src/pages/api/greenhouse/saveResume.ts
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
       return res.status(200).json('No application found');
     }
 
-    let fileUrl = payload.resume;
+    let fileUrl = payload.resume.toLowerCase();
     let bucketName = 'resume-job-post';
     let fileId = uuidv4();
 
