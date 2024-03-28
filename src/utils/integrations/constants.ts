5112c806b8220503005be9bb1dee18cf925e37fd
Dileep B C <dileep@aglinthq.com>
2024-03-28 17:06:07 +0530

    update outbound emails

--- src/utils/integrations/constants.ts
+++ src/utils/integrations/constants.ts
@@ -1,2 +1,9 @@
 export const ZOOM_REDIRECT_URI = `${process.env.NEXT_PUBLIC_HOST_NAME}/auth/zoom`;
 export const ZOOM_API_URL = `https://api.zoom.us/v2`;
+export const allowed_outbound_emails = [
+  'dileepwert@gmail.com',
+  'dm89f11@gmail.com',
+  'ravik@gmail.com',
+  'krhebbar@gmail.com',
+  'ravi.webflow@gmail.com',
+];
