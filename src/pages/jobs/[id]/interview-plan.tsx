36c1d2d4b6f24491b886c4f2276d56bd16f204ed
Chinmai <chinmai@aglinthq.com>
2024-04-03 20:20:45 +0530

    build fix

--- src/pages/jobs/[id]/interview-plan.tsx
+++ src/pages/jobs/[id]/interview-plan.tsx
@@ -1,21 +1,21 @@
-// import React from 'react';
+import React from 'react';
 
-// import Seo from '@/src/components/Common/Seo';
+import Seo from '@/src/components/Common/Seo';
 // import JobInterviewPlan from '@/src/components/JobInterviewPlan/JobInterviewPlan';
 // import JobInterviewPlanHoc from '@/src/components/JobInterviewPlan/JobInterviewPlanProvider';
 
-// const Page = () => {
-//   return (
-//     <>
-//       <Seo
-//         title='Aglint | Interview Plan'
-//         description='AI Powered Talent Development Platform.'
-//       />
-//       <JobInterviewPlanHoc>
-//         <JobInterviewPlan />
-//       </JobInterviewPlanHoc>
-//     </>
-//   );
-// };
+const Page = () => {
+  return (
+    <>
+      <Seo
+        title='Aglint | Interview Plan'
+        description='AI Powered Talent Development Platform.'
+      />
+      {/* <JobInterviewPlanHoc>
+        <JobInterviewPlan />
+      </JobInterviewPlanHoc> */}
+    </>
+  );
+};
 
-// export default Page;
+export default Page;
