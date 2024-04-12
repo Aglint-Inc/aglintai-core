a03bae0d3f1eeb6207778a4fc58ae65bf4c96a2e
Vivek Singh <vivek@aglinthq.com>
2024-04-12 08:03:30 +0000

    Merged in fix/interview_role (pull request #985)
    * fix: role features

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -6,7 +6,9 @@ import { useEffect } from 'react';
 import { BodyWithSublink, PageLayout, SublinkTab } from '@/devlink2';
 import { useAuthDetails } from '@/src/context/AuthContext/AuthContext';
 import { useTasksAgentContext } from '@/src/context/TaskContext/TaskContextProvider';
+import { DatabaseEnums } from '@/src/types/customSchema';
 import { pageRoutes } from '@/src/utils/pageRouting';
+import toast from '@/src/utils/toast';
 
 import AddTaskButton from './Components/AddTaskButton';
 import TaskBody from './TaskBody';
@@ -14,28 +16,46 @@ import { TaskStatesProvider } from './TaskStatesContext';
 
 const Tasks = () => {
   const { filter, handelFilter } = useTasksAgentContext();
-  const { userDetails } = useAuthDetails();
+  const { userDetails, isAllowed } = useAuthDetails();
   const searchParam = useSearchParams();
   const router = useRouter();
   const navItems = [
-    { name: 'My Tasks', pathName: 'myTasks' },
-    { name: 'All Tasks', pathName: 'allTasks' },
+    {
+      name: 'My Tasks',
+      pathName: 'myTasks',
+      roles: [
+        'admin',
+        'recruiter',
+        'scheduler',
+        'interviewer',
+      ] as DatabaseEnums['user_roles'][],
+    },
+    {
+      name: 'All Tasks',
+      pathName: 'allTasks',
+      roles: ['admin'] as DatabaseEnums['user_roles'][],
+    },
   ];
 
   useEffect(() => {
+    if (
+      !isAllowed(navItems.find((item) => searchParam.has(item.pathName)).roles)
+    ) {
+      toast.error("You don't have Access to this Feature.");
+      router.replace(pageRoutes.LOADING);
+    }
     if (searchParam.has('myTasks')) {
       filter.assignee.values = [userDetails.user.id];
       handelFilter({
         ...filter,
       });
-    }
-    if (searchParam.has('allTasks')) {
+    } else if (searchParam.has('allTasks')) {
       filter.assignee.values = [];
       handelFilter({
         ...filter,
       });
     }
-  }, []);
+  }, [searchParam]);
   return (
     <TaskStatesProvider>
       <PageLayout
@@ -43,33 +63,37 @@ const Tasks = () => {
         slotTopbarRight={<AddTaskButton />}
         slotBody={
           <BodyWithSublink
-            slotSublinkTab={navItems.map((item, i) => {
-              return (
-                <SublinkTab
-                  onClickTab={{
-                    onClick: () => {
-                      if (item.pathName === 'myTasks') {
-                        filter.assignee.values = [userDetails.user.id];
-                        handelFilter({
-                          ...filter,
-                        });
-                      }
-                      if (item.pathName === 'allTasks') {
-                        filter.assignee.values = [];
-                        handelFilter({
-                          ...filter,
-                        });
-                      }
+            slotSublinkTab={navItems
+              .filter((item) => (item.roles ? isAllowed(item.roles) : true))
+              .map((item, i) => {
+                return (
+                  <SublinkTab
+                    onClickTab={{
+                      onClick: () => {
+                        if (item.pathName === 'myTasks') {
+                          filter.assignee.values = [userDetails.user.id];
+                          handelFilter({
+                            ...filter,
+                          });
+                        }
+                        if (item.pathName === 'allTasks') {
+                          filter.assignee.values = [];
+                          handelFilter({
+                            ...filter,
+                          });
+                        }
 
-                      router.push('/' + pageRoutes.TASKS + '?' + item.pathName);
-                    },
-                  }}
-                  isActtive={searchParam.has(item.pathName)}
-                  key={i}
-                  text={item.name}
-                />
-              );
-            })}
+                        router.push(
+                          '/' + pageRoutes.TASKS + '?' + item.pathName,
+                        );
+                      },
+                    }}
+                    isActtive={searchParam.has(item.pathName)}
+                    key={i}
+                    text={item.name}
+                  />
+                );
+              })}
             slotTabContent={<TaskBody />}
           />
         }
