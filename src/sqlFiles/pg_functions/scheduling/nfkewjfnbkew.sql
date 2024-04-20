ee781cd5b5ba59404373dc3005518aa88333ab14
Dileep B C <dileep@aglinthq.com>
2024-04-20 23:07:48 +0530

    sql func

---
+++ src/sqlFiles/pg_functions/scheduling/nfkewjfnbkew.sql
@@ -0,0 +1,119 @@
+DROP FUNCTION IF EXISTS upd_get_interview_session_data;
+
+CREATE
+OR REPLACE FUNCTION upd_get_interview_session_data (session_ids uuid[], company_id uuid,meet_start_date timestamp , meet_end_date timestamp) RETURNS TABLE (
+  interview_sessions jsonb[],
+  interviewers jsonb[],
+  service_cred text,
+  interview_modules jsonb[],
+  comp_schedule_setting jsonb,
+  int_meetings jsonb[]
+) AS $$
+DECLARE
+  session_record interview_session;
+  interviewers jsonb[] := '{}'::jsonb[]; 
+BEGIN
+  -- Open cursor to fetch interview sessions (consider error handling)
+  FOR session_record IN
+    SELECT *
+    FROM interview_session AS int_sess
+    WHERE int_sess.id = any(session_ids)
+  LOOP
+    -- Append each session as JSONB directly to interview_sessions
+    interview_sessions := interview_sessions || ROW_TO_JSON(session_record)::jsonb;
+
+ IF session_record.session_type = 'debrief' THEN
+     interviewers := interviewers || (
+      SELECT jsonb_agg(jsonb_build_object(
+      'user_id', rec_user.user_id,
+      'first_name', rec_user.first_name,
+      'last_name', rec_user.last_name,
+      'scheduling_settings', rec_user.scheduling_settings,
+      'schedule_auth', rec_user.schedule_auth,
+      'profile_image', rec_user.profile_image,       
+      'email', rec_user.email,
+      'session_id', sess_reln.session_id,
+      'training_type', sess_reln.training_type,
+      'interviewer_type', sess_reln.interviewer_type,
+      'pause_json', null,
+      'interview_module_relation_id', null
+      )) 
+        FROM recruiter_user rec_user
+        LEFT JOIN interview_session_relation sess_reln ON sess_reln.user_id = rec_user.user_id
+        WHERE sess_reln.session_id = session_record.id
+      );
+    ELSE
+      interviewers := interviewers || (
+        SELECT jsonb_agg(jsonb_build_object(
+          'user_id', recruiter_user.user_id,
+        'first_name', recruiter_user.first_name,
+        'last_name', recruiter_user.last_name,
+        'scheduling_settings',recruiter_user.scheduling_settings,
+        'schedule_auth',recruiter_user.schedule_auth,
+        'profile_image',recruiter_user.profile_image,       
+        'email',recruiter_user.email,
+        'session_id',session_record.id,
+        'training_type',sess_reln.training_type,
+        'interviewer_type', sess_reln.interviewer_type,
+        'pause_json',interview_module_relation.pause_json,
+        'interview_module_relation_id',sess_reln.interview_module_relation_id
+        ))
+        FROM interview_session_relation sess_reln
+        LEFT JOIN interview_module_relation ON sess_reln.interview_module_relation_id = interview_module_relation.id
+        LEFT JOIN recruiter_user ON interview_module_relation.user_id = recruiter_user.user_id
+        WHERE sess_reln.session_id = session_record.id
+      );
+    END IF;
+
+    interview_modules := interview_modules||(select jsonb_agg(interview_module.*) from interview_module where  interview_module.id=session_record.module_id);
+
+  END LOOP;
+
+  IF meet_start_date IS NOT NULL THEN
+    SELECT ARRAY(select 
+      jsonb_build_object(
+            'meeting_start_time', interview_meeting.start_time,
+            'meeting_id', interview_meeting.id,
+            'int_session_id', interview_session.id,
+            'session_duration', interview_session.session_duration            
+        )
+     from 
+      interview_module_relation
+      right join interview_session_relation on interview_session_relation.interview_module_relation_id=interview_module_relation.id
+      right join interview_session on interview_session.id = interview_session_relation.session_id
+      left join interview_meeting on interview_meeting.id = interview_session.meeting_id
+      where interview_module_relation.user_id = any(select interview_module_relation.user_id from 
+      interview_module_relation
+      right join interview_session_relation on interview_session_relation.interview_module_relation_id=interview_module_relation.id
+      where interview_session_relation.session_id = any(session_ids) 
+    ) and interview_session_relation.is_confirmed=true and interview_meeting.status in ('confirmed','completed')) INTO int_meetings;
+  END IF ;
+
+  SELECT INTO service_cred r.service_json
+      FROM recruiter r
+      WHERE r.id = company_id; 
+  
+  SELECT scheduling_settings INTO comp_schedule_setting
+  FROM recruiter
+  WHERE id = company_id;
+
+
+  -- Return the interview_sessions and interviewers arrays
+  RETURN QUERY SELECT interview_sessions, interviewers, service_cred, interview_modules, comp_schedule_setting, int_meetings;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+SELECT
+  *
+FROM
+  upd_get_interview_session_data (
+    '{d083d42d-c5f9-4a65-8f13-647c31b6ad0b}',
+    'd353b3a0-3e19-45d0-8623-4bd35577f548',
+    '2024-04-18 15:52:15+00'::TIMESTAMP,
+    '2024-04-18 15:52:15+00'::TIMESTAMP
+  );
+
+-- SELECT '2024-04-19 12:00:00-07'::TIMESTAMP WITH TIME ZONE;
