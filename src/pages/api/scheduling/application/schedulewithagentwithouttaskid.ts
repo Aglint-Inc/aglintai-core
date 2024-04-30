ff798b45ede0c985ccea0f6a3ec50bbc8dcd1ced
Chinmai <chinmai@aglinthq.com>
2024-04-30 14:28:43 +0530

    created seperate function for debugging with and without taskId scheduleWithAgent

--- src/pages/api/scheduling/application/schedulewithagentwithouttaskid.ts
+++ src/pages/api/scheduling/application/schedulewithagentwithouttaskid.ts
@@ -2,7 +2,10 @@
 /* eslint-disable no-console */
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { scheduleWithAgent } from '@/src/components/Scheduling/AllSchedules/SchedulingApplication/utils';
+import {
+  scheduleWithAgent,
+  scheduleWithAgentWithoutTaskId,
+} from '@/src/components/Scheduling/AllSchedules/SchedulingApplication/utils';
 import { supabaseAdmin } from '@/src/utils/supabase/supabaseAdmin';
 
 export interface ApiBodyParamsScheduleAgent {
@@ -65,7 +68,21 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         trigger_count,
       });
     } else {
-      console.log('no task id');
+      resAgent = await scheduleWithAgentWithoutTaskId({
+        application_id,
+        dateRange,
+        recruiter_id,
+        recruiter_user_name,
+        session_ids,
+        type: type,
+        candidate_name: candidate_name,
+        company_name: company_name,
+        rec_user_email,
+        rec_user_phone,
+        rec_user_id,
+        supabase: supabaseAdmin,
+        user_tz,
+      });
     }
 
     return res.status(200).send(resAgent);
