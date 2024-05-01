8ea71da5d4086cca9d0e4d8bb6ca13edb9ec2d48
Admin Aglint <admin@aglinthq.com>
2024-05-01 00:23:54 -0700

    Updated default route to go to Login

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/signup',
+        destination: '/login',
         basePath: false,
         permanent: false,
       },
