804cbc8d18adf4b0333ff230310b30db8aabc304
Dileep B C <dileep@aglinthq.com>
2024-05-10 07:19:51 +0000

    Merged in feat/merge-staging-to-dev (pull request #1261)
    Feat/merge staging to dev
    
    * Merged in bugfix/devlink124 (pull request #1226)
    
    Spacing Issue
    
    * Spacing Issue
    
    * Merged in bugfix/devlink125 (pull request #1227)
    
    Spacing Issues
    
    * Spacing Issues
    
    * Merged in fix/May8StagingFixes (pull request #1228)
    
    Fix/May8StagingFixes
    
    * fix: banner state fix
    
    * fix: Duplicate phone
    
    * Changed placeholder for Company name
    
    * Merged in fix/cron-forign-relation (pull request #1260)
    
    fix:cron relation
    
    * fix:cron relation
    
    * Merge branch 'development' of bitbucket.org:project-aglint/aglint-recruiter into feat/merge-staging-to-dev

--- devlink2/InterviewConfirmed.js
+++ devlink2/InterviewConfirmed.js
@@ -1,9 +1,9 @@
 "use client";
 import React from "react";
+
 import * as _Builtin from "./_Builtin";
 import { InterviewConfirmedCard } from "./InterviewConfirmedCard";
 import * as _utils from "./utils";
-import _styles from "./InterviewConfirmed.module.css";
 
 export function InterviewConfirmed({
   as: _Component = _Builtin.Block,
