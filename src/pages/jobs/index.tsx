4a31eb4aae19bcc7c07bd0ad9b6f06efbc0e6db8
VivekS <vivek@aglinthq.com>
2024-05-13 18:44:44 +0530

    refactor: remove Support
    - commented all Support feature related code.

--- src/pages/jobs/index.tsx
+++ src/pages/jobs/index.tsx
@@ -1,19 +1,19 @@
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import { useEffect } from 'react';
 
 import Seo from '@/src/components/Common/Seo';
 import DashboardComp from '@/src/components/JobsDashboard';
-import { useAuthDetails } from '@/src/context/AuthContext/AuthContext';
+// import { useAuthDetails } from '@/src/context/AuthContext/AuthContext';
 import IntegrationProvider from '@/src/context/IntegrationProvider/IntegrationProvider';
-import { pageRoutes } from '@/src/utils/pageRouting';
+// import { pageRoutes } from '@/src/utils/pageRouting';
 
 const Dashboard = () => {
-  const { recruiter } = useAuthDetails();
-  const router = useRouter();
-  useEffect(() => {
-    if (recruiter?.id === process.env.NEXT_PUBLIC_DEFAULT_SUPPORT_COMPANY_ID)
-      router.replace(pageRoutes.SUPPORT);
-  }, [recruiter]);
+  // const { recruiter } = useAuthDetails();
+  // const router = useRouter();
+  // useEffect(() => {
+  //   if (recruiter?.id === process.env.NEXT_PUBLIC_DEFAULT_SUPPORT_COMPANY_ID)
+  //     router.replace(pageRoutes.SUPPORT);
+  // }, [recruiter]);
   return (
     <>
       <Seo title={`Jobs`} description='AI for People Products' />
