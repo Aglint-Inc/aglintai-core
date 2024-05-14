05b9063ad1d7f2799ef6c716da10ecdbe8af8d52
Dileep B C <dileep@aglinthq.com>
2024-05-14 19:49:55 +0530

    feat:created shared-utils package

--- packages/shared-utils/src/scheduling/constants.ts
+++ packages/shared-utils/src/scheduling/constants.ts
@@ -1,3 +1,3 @@
-export const SINGLE_DAY_TIME = 24 * 60;
 export const EmailAgentId = '5acd5b49-a53d-4fc6-9365-ed5c7a7c08c1';
 export const PhoneAgentId = '241409e5-45c6-451a-b576-c54388924e76';
+export const SINGLE_DAY_TIME = 24 * 60;
