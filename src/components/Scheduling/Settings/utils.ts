b3b42330548d73d6a9d530647fb9e47f317167be
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-05-14 12:15:48 +0530

    feat: Generate hoursList array for time selection

--- src/components/Scheduling/Settings/utils.ts
+++ src/components/Scheduling/Settings/utils.ts
@@ -1,3 +1,1 @@
-export const hoursList = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40
-];
+export const hoursList = Array.from({ length: 40 }, (_, index) => index + 1);
