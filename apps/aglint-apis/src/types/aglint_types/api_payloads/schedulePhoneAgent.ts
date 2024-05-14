4031158f7fe7c097f0e5267156bf50f978c458a1
Dileep B C <dileep@aglinthq.com>
2024-05-14 16:45:43 +0530

    fix : delete eslint and aglint_types

--- apps/aglint-apis/src/types/aglint_types/api_payloads/schedulePhoneAgent.ts
+++
@@ -1,9 +0,0 @@
-export interface SchedulePhoneAgentApi {
-  begin_sentence_template: string;
-  interviewer_name: string;
-  to_phone_no: string;
-  filter_json_id: string;
-  phone_agent_type: 'scheduling';
-  cand_email: string;
-  task_id: string;
-}
--- apps/aglint-apis/src/types/aglint_types/candidates.types.ts
+++
@@ -1,7 +0,0 @@
-import { Database } from "./schema";
-
-export type Candidate = Database['public']['Tables']['candidates']['Row'];
-export type CandidateInsert =
-  Database['public']['Tables']['candidates']['Insert'];
-export type CandidateUpdate =
-  Database['public']['Tables']['candidates']['Update'];
