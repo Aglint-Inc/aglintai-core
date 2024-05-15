87eeed1ee27d718236c027416b83ae14e0e519db
VivekS <vivek@aglinthq.com>
2024-05-15 18:56:57 +0530

    fix: remove SchedulingDashboard flag
    - remove enum isSchedulingDashboardEnabled

--- apps/aglint-recruiter/src/utils/Constants.ts
+++ apps/aglint-recruiter/src/utils/Constants.ts
@@ -1,7 +1,5 @@
 /* eslint-disable no-unused-vars */
-export enum featureFlag {
-  isSchedulingDashboardEnabled = 'isSchedulingDashboardEnabled',
-}
+export enum featureFlag {}
 
 export const interviewPlanRecruiterUserQuery =
   'user_id, first_name, last_name, email, profile_image, position, department' as const;
