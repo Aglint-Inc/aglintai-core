bb9d25b69b215db46991bd051024eaa239c0a5e5
VivekS <vivek@aglinthq.com>
2024-05-15 20:03:20 +0530

    fix:lint error
    - add values in featureFlag enum

--- apps/aglint-recruiter/src/utils/Constants.ts
+++ apps/aglint-recruiter/src/utils/Constants.ts
@@ -1,5 +1,8 @@
 /* eslint-disable no-unused-vars */
-export enum featureFlag {}
+export enum featureFlag {
+  isAssistantEnabled = 'isAssistantEnabled',
+  isSupportEnabled = 'isSupportEnabled',
+}
 
 export const interviewPlanRecruiterUserQuery =
   'user_id, first_name, last_name, email, profile_image, position, department' as const;
