60722afe3a737f9947b73b5132c7c8f60d3ee45c
Dileep B C <dileep@aglinthq.com>
2024-05-16 21:46:26 +0530

    fix : update interviewer attendees update

--- apps/aglint-recruiter/src/pages/api/scheduling/v1/update_meeting_interviewers.ts
+++ apps/aglint-recruiter/src/pages/api/scheduling/v1/update_meeting_interviewers.ts
@@ -3,12 +3,14 @@ import {
   APIUpdateMeetingInterviewers,
   CalendarEvent,
   CompServiceKeyCred,
+  ScheduleAuthType,
 } from '@aglint/shared-types';
 import { has } from 'lodash';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { supabaseWrap } from '@/src/components/JobsDashboard/JobPostCreateUpdate/utils';
 import { GoogleCalender } from '@/src/services/GoogleCalender/google-calender';
+import { CalEventAttendeesAuthDetails } from '@/src/utils/event_book/book_session';
 import { decrypt_string } from '@/src/utils/integrations/crypt-funcs';
 import { supabaseAdmin } from '@/src/utils/supabase/supabaseAdmin';
 
@@ -20,141 +22,62 @@ const required_fields: (keyof APIUpdateMeetingInterviewers)[] = [
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const { replaced_inters, candidate_email, meeting_id } =
-      req.body as APIUpdateMeetingInterviewers;
     required_fields.forEach((field) => {
       if (!has(req.body, field)) {
         return res.status(400).send(`missing Field ${field}`);
       }
     });
+    const { replaced_inters } = req.body as APIUpdateMeetingInterviewers;
+
     const {
-      curr_cal_event,
-      curr_organizer,
-      confirmed_inters,
-      not_confirmed_inters,
-      service_json,
-      confirmed_inter,
-      curr_ints_auth,
+      company_cred,
+      meeting_organizer_auth,
+      session_ints_auth,
+      meeting_details,
+      session_intes,
     } = await fetch_details(req.body);
 
-    const curr_meeting_ints = [...confirmed_inters, ...not_confirmed_inters];
-    const curr_org_email =
-      (curr_organizer.recruiter_user.schedule_auth as any)?.email ??
-      curr_organizer.recruiter_user.email;
-    let is_organiser_changed = Boolean(
-      !replaced_inters.find((i) => i.email === curr_org_email),
+    const google_cal = new GoogleCalender({
+      company_cred: company_cred,
+      recruiter: {
+        email: meeting_organizer_auth.email,
+        schedule_auth: meeting_organizer_auth.schedule_auth,
+        user_id: meeting_organizer_auth.user_id,
+      },
+    });
+    await google_cal.authorizeUser();
+    let updated_attendees_auth = session_ints_auth.filter((int) =>
+      replaced_inters.find((int2) => int2.user_id === int.user_id),
     );
-    let updated_event: CalendarEvent;
+    const updated_event = await google_cal.updateEvent({
+      id: (meeting_details.meeting_json as CalendarEvent).id,
+      attendees: updated_attendees_auth.map((int) => ({
+        email: (int.schedule_auth as ScheduleAuthType)?.email ?? int.email,
+        organizer: false,
+      })),
+    });
+    console.log('updateed event');
 
-    let updated_db_sess_reln: {
-      session_relation_id: string;
-      is_confirmed: boolean;
-    }[] = [];
-
-    if (is_organiser_changed) {
-      const google_cal = new GoogleCalender(null, null);
-      await google_cal.authSuperAdmin(confirmed_inter.recruiter.id);
-      const new_organiser = replaced_inters[0];
-      updated_db_sess_reln = [
-        {
-          session_relation_id: confirmed_inters.find(
-            (i) => i.email === curr_org_email,
-          ).session_relation_id,
-          is_confirmed: false,
-        },
-        {
-          session_relation_id: not_confirmed_inters.find(
-            (i) => i.email === new_organiser.email,
-          ).session_relation_id,
-          is_confirmed: true,
-        },
-      ];
-
-      const new_organizer_auth = curr_ints_auth.find(
-        (i) => i.recruiter_user.user_id === new_organiser.user_id,
-      );
-      let new_organiser_email = new_organiser.email;
-      if (new_organizer_auth.recruiter_user.schedule_auth) {
-        new_organiser_email = (
-          new_organizer_auth.recruiter_user.schedule_auth as any
-        ).email;
-      }
-      updated_event = await google_cal.changeMeetingOrganizer(
-        curr_org_email,
-        curr_cal_event.id,
-        new_organiser_email,
-      );
-    } else {
-      const organizer_auth = curr_organizer;
-      const google_cal = new GoogleCalender(
-        {
-          company_cred: service_json,
-          recruiter: {
-            email: organizer_auth.recruiter_user.email,
-            schedule_auth: organizer_auth.recruiter_user.schedule_auth,
-            user_id: organizer_auth.recruiter_user.user_id,
-          },
-        },
-        null,
-      );
-      await google_cal.authorizeUser();
-      const new_inter_emails = replaced_inters.slice(1).map((i) => i.email);
-      const attendees = [...new_inter_emails, candidate_email];
-      updated_event = await google_cal.updateEvent({
-        id: curr_cal_event.id,
-        attendees: attendees.map((i) => ({
-          email: i,
-          organizer: false,
-        })),
+    const attendees_promises = updated_attendees_auth.map(async (int) => {
+      const email = (int.schedule_auth as ScheduleAuthType)?.email ?? int.email;
+      const int_cal = new GoogleCalender({
+        company_cred: company_cred,
+        recruiter: int,
       });
-      const attendees_promises = new_inter_emails.map(async (int_email) => {
-        const int = curr_ints_auth.find(
-          (a) => a.recruiter_user.email === int_email,
-        );
+      await int_cal.authorizeUser();
+      await int_cal.importEvent(updated_event, email);
+    });
+    await Promise.all(attendees_promises);
+    console.log('imported event');
+    const updated_session_attendees = session_intes.map((int) => ({
+      session_relation_id: int.session_relation_id,
+      is_confirmed: Boolean(
+        updated_attendees_auth.find((att) => att.user_id === int.user_id),
+      ),
+    }));
 
-        const email =
-          (int.recruiter_user.schedule_auth as any)?.email ??
-          int.recruiter_user.email;
-        const int_cal = new GoogleCalender({
-          company_cred: service_json,
-          recruiter: int.recruiter_user,
-        });
-        await int_cal.authorizeUser();
-        await int_cal.importEvent(updated_event, email);
-      });
-      await Promise.all(attendees_promises);
-
-      confirmed_inters.forEach((conf_int) => {
-        if (
-          conf_int.email !== curr_org_email &&
-          !attendees.find((email) => email === conf_int.email)
-        ) {
-          updated_db_sess_reln.push({
-            is_confirmed: false,
-            session_relation_id: conf_int.session_relation_id,
-          });
-        }
-      });
-      attendees.forEach((email) => {
-        const int = curr_meeting_ints.find((int) => int.email === email);
-        if (int) {
-          updated_db_sess_reln.push({
-            session_relation_id: int.session_relation_id,
-            is_confirmed: true,
-          });
-        }
-      });
-    }
-    supabaseWrap(
-      await supabaseAdmin
-        .from('interview_meeting')
-        .update({
-          meeting_json: updated_event,
-        })
-        .eq('id', meeting_id),
-    );
     await Promise.all(
-      updated_db_sess_reln.map(async (reln) =>
+      updated_session_attendees.map(async (reln) =>
         updateInterviewers({
           id: reln.session_relation_id,
           is_confirmed: reln.is_confirmed,
@@ -172,9 +95,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 export default handler;
 
 const fetch_details = async (payload: APIUpdateMeetingInterviewers) => {
-  const { meeting_id, replaced_inters } = payload;
+  const { meeting_id } = payload;
 
-  const [meeting_details, current_meeting_inters] = await Promise.all([
+  const [meeting_details, session_intes] = await Promise.all([
     (async () => {
       const [meeting_details] = supabaseWrap(
         await supabaseAdmin
@@ -182,31 +105,37 @@ const fetch_details = async (payload: APIUpdateMeetingInterviewers) => {
           .select()
           .eq('id', meeting_id),
       );
-      return meeting_details;
+      const [rec_auth] = await supabaseWrap(
+        await supabaseAdmin
+          .from('recruiter_user')
+          .select('user_id,email,schedule_auth')
+          .eq('user_id', meeting_details.organizer_id),
+      );
+      return {
+        ...meeting_details,
+        organizer_email: rec_auth.email,
+        organizer_id: rec_auth.user_id,
+        schedule_auth: rec_auth.schedule_auth as ScheduleAuthType,
+      };
     })(),
     (async () => {
-      const current_meeting_inters = supabaseWrap(
+      const session_intes = supabaseWrap(
         await supabaseAdmin
           .from('meeting_interviewers')
           .select()
           .eq('meeting_id', meeting_id),
       );
-      return current_meeting_inters;
+      return session_intes;
     })(),
   ]);
 
-  // validation
-  replaced_inters.forEach((new_att) => {
-    if (!current_meeting_inters.find((int) => int.email === new_att.email)) {
-      throw new Error(`${new_att} does not exist in the session`);
-    }
-  });
+  const meeting_organizer_auth: CalEventAttendeesAuthDetails = {
+    email: meeting_details.organizer_email,
+    schedule_auth: meeting_details.schedule_auth,
+    user_id: meeting_details.organizer_id,
+  };
 
-  const confirmed_inters = current_meeting_inters.filter((c) => c.is_confirmed);
-  const not_confirmed_inters = current_meeting_inters.filter(
-    (c) => !c.is_confirmed,
-  );
-  const curr_ints_auth = await supabaseWrap(
+  const session_ints_auth_details = await supabaseWrap(
     await supabaseAdmin
       .from('recruiter_relation')
       .select(
@@ -214,47 +143,32 @@ const fetch_details = async (payload: APIUpdateMeetingInterviewers) => {
       )
       .in(
         'user_id',
-        current_meeting_inters.map((i) => i.user_id),
+        session_intes.map((i) => i.user_id),
       ),
   );
-  const confirmed_inter = curr_ints_auth.find(
-    (int) => int.recruiter_user.email === confirmed_inters[0].email,
-  );
-  let service_json: CompServiceKeyCred = null;
-  if (confirmed_inter.recruiter.service_json) {
-    service_json = JSON.parse(
-      decrypt_string(confirmed_inter.recruiter.service_json),
+  const session_ints_auth: CalEventAttendeesAuthDetails[] =
+    session_ints_auth_details.map((int) => ({
+      email: int.recruiter_user.email,
+      schedule_auth: int.recruiter_user.schedule_auth as ScheduleAuthType,
+      user_id: int.recruiter_user.user_id,
+    }));
+  const hashed_comp_cred = session_ints_auth_details[0].recruiter.service_json;
+  let company_cred: CompServiceKeyCred;
+  if (hashed_comp_cred) {
+    company_cred = JSON.parse(
+      decrypt_string(hashed_comp_cred),
     ) as CompServiceKeyCred;
   }
-  const google_cal = new GoogleCalender({
-    company_cred: service_json,
-    recruiter: {
-      email: confirmed_inter.recruiter_user.email,
-      schedule_auth: confirmed_inter.recruiter_user.schedule_auth,
-      user_id: confirmed_inter.recruiter_user.user_id,
-    },
-  });
-  await google_cal.authorizeUser();
-  const meeting_event = await google_cal.getCalenderEventById(
-    (meeting_details.meeting_json as CalendarEvent).id,
-  );
-  curr_ints_auth.find(
-    (i) => i.recruiter_user.email === meeting_event.organizer.email,
-  );
-  const curr_organizer = curr_ints_auth.find(
-    (i) => i.recruiter_user.email === meeting_event.organizer.email,
-  );
 
   return {
-    curr_cal_event: meeting_event,
-    curr_organizer,
-    confirmed_inters,
-    not_confirmed_inters,
-    service_json,
-    confirmed_inter,
-    curr_ints_auth,
+    company_cred,
+    meeting_organizer_auth,
+    session_ints_auth,
+    meeting_details,
+    session_intes,
   };
 };
+
 const updateInterviewers = async ({ id, is_confirmed }) => {
   supabaseWrap(
     await supabaseAdmin
