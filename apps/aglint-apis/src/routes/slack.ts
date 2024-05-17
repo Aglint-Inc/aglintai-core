6d702f524eb52677a053369b7f2876ead656d769
Dileep B C <dileep@aglinthq.com>
2024-05-17 19:22:37 +0530

    fix:file imports

--- apps/aglint-apis/src/routes/slack.ts
+++ apps/aglint-apis/src/routes/slack.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import {getSession} from 'src/controllers/slack/getSessions';
+import {groupMessage} from 'src/controllers/slack/groupMessage';
+import {rsvp} from 'src/controllers/slack/rsvp';
+import {sendDirectMessage} from 'src/controllers/slack/sendDirectMessage';
 
 const slackRoutes = express.Router();
 
-slackRoutes.post('/send-direct-message');
-slackRoutes.post('/private-channel-message');
-slackRoutes.post('/group-message');
+slackRoutes.post('/send-direct-message', sendDirectMessage);
+slackRoutes.post('/rsvp', rsvp);
+slackRoutes.post('/group-message', groupMessage);
+slackRoutes.post('/get-session', getSession);
 
 export default slackRoutes;
