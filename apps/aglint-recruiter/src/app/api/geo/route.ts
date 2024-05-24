6dd96b03e1812ef2cf7d7b32f09383de93d629f9
ChandraAglint <chandra@aglinthq.com>
2024-05-24 10:08:58 +0530

    getting conuntry

--- apps/aglint-recruiter/src/app/api/geo/route.ts
+++ apps/aglint-recruiter/src/app/api/geo/route.ts
@@ -1,8 +1,8 @@
 import { geolocation } from '@vercel/edge';
- 
+
 export function GET(request: Request) {
-  const { city } = geolocation(request);
-  return new Response(`<h1>Your location is ${city}</h1>`, {
+  const { country } = geolocation(request);
+  return new Response(`<h1>Your location is ${country}</h1>`, {
     headers: { 'content-type': 'text/html' },
   });
 }
