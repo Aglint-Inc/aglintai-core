0955def93367b08960b6328e28a1cce8b5fa73ba
ChandraAglint <chandra@aglinthq.com>
2024-05-24 12:31:55 +0530

    cors header added

--- apps/aglint-recruiter/src/app/api/getUserLocation/route.ts
+++ apps/aglint-recruiter/src/app/api/getUserLocation/route.ts
@@ -1,9 +1,15 @@
 import { geolocation } from '@vercel/edge';
 import { NextRequest, NextResponse } from 'next/server';
 
+const headers = {
+  'Access-Control-Allow-Origin': '*', // Allow requests from any origin
+  'Access-Control-Allow-Methods': 'GET', // Allow only GET requests
+  'Access-Control-Allow-Headers': 'Content-Type', // Allow only Content-Type header
+};
+
 export function GET(request: NextRequest) {
   const geo = geolocation(request);
-  if (geo.country) return NextResponse.json(geo);
+  if (geo.country) return NextResponse.json(geo, { headers });
   else
     return NextResponse.json(
       { error: 'Failed to getting the user location' },
