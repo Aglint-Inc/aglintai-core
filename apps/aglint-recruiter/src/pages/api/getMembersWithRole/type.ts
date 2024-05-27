2ced777b343c08d4bfde9c997144dc0475ed0c53
VivekS <vivek@aglinthq.com>
2024-05-27 20:05:44 +0530

    feat: Update getMembersWithRole API type

--- apps/aglint-recruiter/src/pages/api/getMembersWithRole/type.ts
+++ apps/aglint-recruiter/src/pages/api/getMembersWithRole/type.ts
@@ -2,19 +2,10 @@ import { DatabaseEnums, DatabaseTable } from '@aglint/shared-types';
 
 export type API_getMembersWithRole = {
   request: {
-    id: string;
+    id?: string;
   };
-  response:
-    | {
-        members: (DatabaseTable['recruiter_user'] & {
-          role: DatabaseEnums['user_roles'];
-          manager_id: string;
-        })[];
-
-        error: null;
-      }
-    | {
-        members: null;
-        error: string;
-      };
+  response: (DatabaseTable['recruiter_user'] & {
+    role: DatabaseEnums['user_roles'];
+    manager_id: string;
+  })[];
 };
