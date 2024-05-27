d333ea39620b01e0986fa322407fa7eec2e52d20
Punith G <punithg@aglinthq.com>
2024-05-27 15:49:36 +0530

    chore: devlink sync

--- apps/aglint-recruiter/devlink3/WorkflowEmpty.d.ts
+++ apps/aglint-recruiter/devlink3/WorkflowEmpty.d.ts
@@ -1,7 +1,5 @@
 import * as React from "react";
-import * as Types from "./types";
 
 declare function WorkflowEmpty(props: {
   as?: React.ElementType;
-  onClickCreate?: Types.Devlink.RuntimeProps;
 }): React.JSX.Element;
