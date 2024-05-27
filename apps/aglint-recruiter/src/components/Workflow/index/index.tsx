0d129282592484dddf62dd552aa8587e5516d9e0
Punith G <punithg@aglinthq.com>
2024-05-27 18:36:58 +0530

    fix: move store  reset to context

--- apps/aglint-recruiter/src/components/Workflow/index/index.tsx
+++ apps/aglint-recruiter/src/components/Workflow/index/index.tsx
@@ -1,14 +1,7 @@
-import { useEffect } from 'react';
-
-import { useWorkflowStore } from '../store';
 import Body from './body';
 import Layout from './layout';
 
 const Workflows = () => {
-  const reset = useWorkflowStore((state) => state.resetAll);
-  useEffect(() => {
-    return () => reset();
-  }, []);
   return (
     <Layout>
       <Body />
