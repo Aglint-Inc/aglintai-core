14e68c21a089865437de8f21893aa2c5694bb4cf
Oggy223 <ogyen@aglinthq.com>
2024-05-31 12:00:32 +0530

    Devlink3/cleanup (#452)
    Devlink3 CleanUp

--- apps/aglint-recruiter/devlink3/InstructionsButton.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import * as Types from "./types";
-
-declare function InstructionsButton(props: {
-  as?: React.ElementType;
-  onClickInstructions?: Types.Devlink.RuntimeProps;
-}): React.JSX.Element;
