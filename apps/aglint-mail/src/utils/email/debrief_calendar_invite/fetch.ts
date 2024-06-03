05d4776d23927be767ad892dbe8bb9447162a344
Chandan <chandan@aglinthq.com>
2024-06-03 19:05:09 +0530

    api

--- apps/aglint-mail/src/utils/email/debrief_calendar_invite/fetch.ts
+++ apps/aglint-mail/src/utils/email/debrief_calendar_invite/fetch.ts
@@ -6,27 +6,29 @@ import {
   sessionTypeIcon,
 } from '../common/functions';
 
-export default async function Index(
+export default async function DebriefCalenderInvite(
   session_id: string,
   application_id: string,
   meeting_id: string,
 ) {
+  console.log('function');
+
   const { data: session } = await supabaseAdmin
     .from('interview_session')
     .select(
       'session_type,session_duration,schedule_type,name,interview_meeting(start_time,end_time)',
     )
     .eq('id', session_id);
-
+  console.log('function 2');
   const {
     data: [candidateJob],
   } = await supabaseAdmin
     .from('applications')
     .select(
-      'candidates(first_name,email,recruiter_id),public_jobs(recruiter(logo),job_title,company)',
+      'candidates(first_name,email,recruiter_id),public_jobs(job_title,company)',
     )
     .eq('id', application_id);
-
+  console.log('function 3');
   const [
     {
       interview_meeting: { start_time, end_time },
@@ -36,31 +38,26 @@ export default async function Index(
       session_type,
     },
   ] = session;
-
+  console.log('function 4');
   const Session = {
     date: dayjs(start_time).format('ddd MMMM DD, YYYY'),
-    Time: `${dayjs(start_time).format('hh:mm A')} - ${dayjs(end_time).format('hh:mm A')}`,
+    time: `${dayjs(start_time).format('hh:mm A')} - ${dayjs(end_time).format('hh:mm A')}`,
     sessionType: name,
     platform: schedule_type,
     duration: DurationCalculator(session_duration),
     sessionTypeIcon: sessionTypeIcon(session_type),
     meetingIcon: scheduleTypeIcon(schedule_type),
   };
-
+  console.log('function 5');
   const {
     candidates: { email, recruiter_id, first_name },
-    public_jobs: {
-      company,
-      job_title,
-      recruiter: { logo },
-    },
+    public_jobs: { company, job_title },
   } = candidateJob;
-
+  console.log('function 6');
   const body = {
     recipient_email: email,
-    mail_type: 'debrief_mail_to_organizer',
+    mail_type: 'debrief_calendar_invite',
     recruiter_id,
-    company_logo: logo,
     payload: {
       '[companyName]': company,
       '[firstName]': first_name,
@@ -69,6 +66,6 @@ export default async function Index(
       'meetingDetail': Session,
     },
   };
-
+  console.log(company);
   return body;
 }
