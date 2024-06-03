dd318f634e91e36c55c44bb20e00c190881c44bd
Punith G <punithg@aglinthq.com>
2024-06-03 11:47:09 +0530

    fix: query factory setup

--- apps/aglint-recruiter/src/queries/job/index.tsx
+++ apps/aglint-recruiter/src/queries/job/index.tsx
@@ -1,28 +1,28 @@
 import type { DatabaseTable } from '@aglint/shared-types';
-import { useQuery } from '@tanstack/react-query';
+import { queryOptions, useQuery } from '@tanstack/react-query';
 
 import { supabase } from '@/src/utils/supabase/client';
 
-import { GC_TIME } from '..';
+import { jobsQueryKeys } from '../jobs/keys';
+
+const queries = {
+  job: ({ id }: JobRequisite) =>
+    queryOptions({
+      queryKey: [...jobsQueryKeys.jobs().queryKey, { id }],
+      queryFn: async () =>
+        (
+          await supabase
+            .from('public_jobs')
+            .select('*')
+            .eq('id', id)
+            .single()
+            .throwOnError()
+        ).data,
+    }),
+};
 
 export type JobRequisite = Pick<DatabaseTable['public_jobs'], 'id'>;
 export const useJobRead = (args: JobRequisite) => {
   const { id } = args;
-  return useQuery({
-    queryKey: ['as'],
-    queryFn: () => jobRead({ id }),
-    enabled: !!id,
-    gcTime: id ? GC_TIME : 0,
-  });
-};
-
-const jobRead = async ({ id }: Pick<DatabaseTable['public_jobs'], 'id'>) => {
-  return (
-    await supabase
-      .from('public_jobs')
-      .select('*')
-      .eq('id', id)
-      .single()
-      .throwOnError()
-  ).data;
+  return useQuery(queries.job({ id }));
 };
