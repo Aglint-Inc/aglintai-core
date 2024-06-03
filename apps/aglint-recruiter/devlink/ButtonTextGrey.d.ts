3a1de780b3aa2b2d9018422573d8353ee5471e86
ogyen@aglinthq.com <ogyen@aglinthq.com>
2024-06-03 18:11:27 +0530

    Devlink1 cleaned up

--- apps/aglint-recruiter/devlink/ButtonTextGrey.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import * as Types from "./types";
-
-declare function ButtonTextGrey(props: {
-  as?: React.ElementType;
-  buttonText?: React.ReactNode;
-  onClickCancel?: Types.Devlink.RuntimeProps;
-}): React.JSX.Element;
