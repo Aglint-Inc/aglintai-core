558ad10c0210defe627f0b91c7e1b1dd14e36693
Punith G <punithg@aglinthq.com>
2024-06-04 15:45:59 +0530

    chore: application view update

--- supabase/migrations/20240604091402_current_job_title-to-candidates-table.sql
+++ supabase/migrations/20240604091402_current_job_title-to-candidates-table.sql
@@ -1,3 +1,3 @@
-current_job_title-to-candidates-tablealter table "public"."candidates" add column "current_job_title" text;
+alter table "public"."candidates" add column "current_job_title" text;
 
 
