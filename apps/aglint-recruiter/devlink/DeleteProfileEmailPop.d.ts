0014b1376b1d00711feab5efc62b94f72216cc53
Ravi K <ravi@aglinthq.com>
2024-06-04 08:17:51 +0530

    Sync Devlink 1

---
+++ apps/aglint-recruiter/devlink/DeleteProfileEmailPop.d.ts
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as Types from "./types";
+
+declare function DeleteProfileEmailPop(props: {
+  as?: React.ElementType;
+  onClickClose?: Types.Devlink.RuntimeProps;
+  slotInput?: Types.Devlink.Slot;
+  onClickSendLink?: Types.Devlink.RuntimeProps;
+  changeText?: React.ReactNode;
+}): React.JSX.Element;
