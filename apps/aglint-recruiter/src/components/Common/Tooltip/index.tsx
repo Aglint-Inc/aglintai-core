0163ba008648b6345530864cfc720747c26007cd
Ravi K <ravi@aglinthq.com>
2024-06-05 12:43:18 +0530

    Fixed Tooltip

--- apps/aglint-recruiter/src/components/Common/Tooltip/index.tsx
+++ apps/aglint-recruiter/src/components/Common/Tooltip/index.tsx
@@ -1,19 +1,17 @@
 import { styled, Tooltip, tooltipClasses, TooltipProps } from '@mui/material';
 
-import { palette } from '@/src/context/Theme/Theme';
-
 export const CustomTooltip = styled(({ className, ...props }: TooltipProps) => (
   <Tooltip {...props} classes={{ popper: className }} />
 ))({
   [`& .${tooltipClasses.tooltip}`]: {
     maxWidth: 'none',
-    background: '#fff',
+    background: 'var(--white)',
     padding: '0px',
-    boxShadow: '0px 4px 8px 0px #04444D26',
+    boxShadow: 'var(--shadow-3)',
     border: '1px solid var(--neutral-6)',
-    borderRadius: '10px',
-    color: palette.grey[600],
-    fontSize: '14px',
+    borderRadius: 'var(--radius-2)',
+    color: 'var(--neutral-12)',
+    fontSize: 'var(--font-size-2)',
     fontWeight: 400,
   },
 });
