a032f26402cdea8effe48c5d88e4c66a2db84189
viveks14 <129153058+viveks14@users.noreply.github.com>
2024-06-05 14:53:06 +0530

    Feat/workflow main api init (#497)
    * refactor: fix api file names
    
    - rename file from .tsx to .ts
    
    * chore: Add responseFactory utility function for API responses
    
    - add generic function getResponseFactory with arg res of nextResponse type
    - return getResponse function to handel api res sending
    
    * feat: API endpoint for workflow cron
    
    - add includes the necessary logic to process the request body and implement basic logic for trial run.
    
    *todo*
    - implement proper logic for api routing.
    
    * refactor: Update APIWorkFlowCron type definition

--- packages/shared-types/src/aglintApi/workflowCrown.ts
+++ packages/shared-types/src/aglintApi/workflowCrown.ts
@@ -1,3 +1,21 @@
+import { DatabaseEnums, DatabaseTable } from "..";
+
 export type APIWorkFlowCron = {
-  //
+  request: {
+    workflow_id: string;
+    workflow_action_id: string;
+    meta: {
+      start_time: string;
+      application_id: string;
+    };
+    payload: DatabaseTable["workflow_action"]["payload"];
+    target: DatabaseTable["workflow_action"]["target"];
+    medium: DatabaseTable["workflow_action"]["medium"];
+    // trigger: DatabaseTable["workflow"]["trigger"];
+    trigger: DatabaseEnums["email_types"];
+    execution_time: string;
+  };
+  response: {
+    success: boolean;
+  };
 };
