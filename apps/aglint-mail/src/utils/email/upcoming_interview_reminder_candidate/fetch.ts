ef7996fc586035bdf4ce42f1de0229e81477b88a
Chandan <chandan@aglinthq.com>
2024-06-06 16:16:18 +0530

    email template upated

---
+++ apps/aglint-mail/src/utils/email/upcoming_interview_reminder_candidate/fetch.ts
@@ -0,0 +1,49 @@
+import { supabaseAdmin, supabaseWrap } from '../../../supabase/supabaseAdmin';
+import type { InterviewType } from '../../types/supabase-fetch';
+
+export default async function interviewReaminder(application_id: string) {
+  const [candidateJob] = supabaseWrap(
+    await supabaseAdmin
+      .from('applications')
+      .select(
+        'candidates(first_name,email,recruiter_id,recruiter(logo)),public_jobs(job_title,company)',
+      )
+      .eq('id', application_id),
+  );
+
+  if (!candidateJob) {
+    throw new Error('candidate and jobs details are not available');
+  }
+
+  const {
+    candidates: {
+      email,
+      recruiter_id,
+      first_name,
+      recruiter: { logo },
+    },
+    public_jobs: { company, job_title },
+  } = candidateJob;
+
+  const body: InterviewType = {
+    recipient_email: email,
+    mail_type: 'upcoming_interview_reminder_candidate',
+    recruiter_id,
+    companyLogo: logo,
+    payload: {
+      '[firstName]': first_name,
+      '[jobTitle]': job_title,
+      '[companyName]': company,
+      '[supportLink]': '',
+      '[interviewLink]': ``,
+    },
+  };
+
+  return body;
+}
+
+// http://localhost:3100/api/interview
+
+// {
+//   "application_id": "0ab5542d-ae98-4255-bb60-358a9c8e0637"
+// }
