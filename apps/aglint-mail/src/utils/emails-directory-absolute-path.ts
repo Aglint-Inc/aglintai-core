c3c7b5eb288bc510e3cd058af0b0ad60440cba46
Dileep B C <135205237+dileepbc3141@users.noreply.github.com>
2024-06-12 02:12:12 +0530

    fix: removes os dependecy (#569)
    * fix: removes os dependecy
    
    * fix: removed development condition
    
    ---------
    
    Co-authored-by: Dileep B C <dileepc01@gmail.com>

--- apps/aglint-mail/src/utils/emails-directory-absolute-path.ts
+++ apps/aglint-mail/src/utils/emails-directory-absolute-path.ts
@@ -1,32 +1,7 @@
-export const emailsDirRelativePath =
-  process.env.NEXT_PUBLIC_EMAILS_DIR_RELATIVE_PATH ??
-  `.${process.env.NEXT_PUBLIC_OS_PATH_SEPARATOR}src${process.env.NEXT_PUBLIC_OS_PATH_SEPARATOR}emails`;
+/* eslint-disable @typescript-eslint/no-var-requires */
+'useServer';
+const path = require('node:path');
 
-export const userProjectLocation = '.';
-// this trickery to find the path separator for the OS is for this to work both on the client
-// and on the server properly
-export const pathSeparator = process.env.NEXT_PUBLIC_OS_PATH_SEPARATOR as
-  | '/'
-  | '\\';
+export const emailsDirRelativePath = path.join(process.cwd(), '/src/emails');
 
-export const normalizePath = (path: string) => {
-  let newPath = path;
-
-  while (newPath.startsWith(`.${pathSeparator}`)) {
-    newPath = newPath.slice(2);
-  }
-
-  while (newPath.startsWith(pathSeparator)) {
-    newPath = newPath.slice(1);
-  }
-
-  while (newPath.endsWith(pathSeparator)) {
-    newPath = newPath.slice(0, -1);
-  }
-
-  return newPath;
-};
-
-export const emailsDirectoryAbsolutePath = `.${pathSeparator}${normalizePath(
-  emailsDirRelativePath,
-)}`;
+export const emailsDirectoryAbsolutePath = emailsDirRelativePath;
