d7b6b76553f4e967e9bcf634015832f3755c0e76
ChandraAglint <chandra@aglinthq.com>
2024-06-12 16:32:47 +0530

    template update with tailwind

--- apps/aglint-mail/src/emails/confirmInterview_email_applicant.tsx
+++ apps/aglint-mail/src/emails/confirmInterview_email_applicant.tsx
@@ -13,6 +13,8 @@ import { Parser } from 'html-to-react';
 import * as React from 'react';
 import type { EmailTemplateAPi } from '@aglint/shared-types';
 import { aglintLogo } from '../utils/assets/common';
+import config from '../../tailwind.config';
+import { Session } from '../components/template/Sessions';
 
 type EmailType = EmailTemplateAPi<'confirmInterview_email_applicant'>;
 
@@ -30,9 +32,9 @@ export const dummy: EmailType['react_email_placeholders'] = {
       platform: 'Google meet',
       duration: '45 minutes',
       sessionTypeIcon:
-        'https://plionpfmgvenmdwwjzac.supabase.co/storage/v1/object/public/email_template_assets/personal.png?t=2024-06-12T05%3A11%3A22.771Z',
+        'https://plionpfmgvenmdwwjzac.supabase.co/storage/v1/object/public/email_template_assets/individual.png',
       meetingIcon:
-        'https://plionpfmgvenmdwwjzac.supabase.co/storage/v1/object/public/email_template_assets/online.png?t=2024-06-12T05%3A11%3A07.207Z',
+        'https://plionpfmgvenmdwwjzac.supabase.co/storage/v1/object/public/email_template_assets/google_meet.png',
     },
   ],
   subject: '',
@@ -42,53 +44,7 @@ export const dummy: EmailType['react_email_placeholders'] = {
 // export get subject
 export const getSubject = (companyName: any) => `${companyName}`;
 
-const Sessions = ({ meetingDetail }) => {
-  const htmlParser = Parser();
-  return (
-    <Container
-      style={{
-        border: '1px solid #E9EBED',
-        padding: '10px 20px',
-        borderRadius: '8px', // assuming 'rounded-md' means border-radius of 8px
-        marginTop: '1rem',
-        marginBottom: '1rem',
-      }}
-    >
-      <Text style={{ lineHeight: '24px', marginBottom: '5px', margin: 0, fontSize: '1rem' }}>
-        <strong>{htmlParser.parse(meetingDetail.date)}</strong> {htmlParser.parse(meetingDetail.time)}
-      </Text>
-      <Text style={{ display: 'flex', alignItems: 'center', lineHeight: '24px', marginBottom: '5px', margin: 0, fontSize: '1rem' }}>
-        <span style={{ display: 'inline-flex', alignItems: 'center', lineHeight: '24px' }}>
-          <Img
-            src={meetingDetail.sessionTypeIcon}
-            style={{ width: '24px', height: '24px' }}
-          />
-          &nbsp;
-          {htmlParser.parse(meetingDetail.sessionType)}
-        </span>
-      </Text>
-      <Text style={{ display: 'flex', alignItems: 'center', lineHeight: '24px', marginBottom: '5px', margin: 0, fontSize: '0.75rem' }}>
-        <span style={{ display: 'inline-flex', alignItems: 'center', lineHeight: '24px' }}>
-          <Img
-            src={meetingDetail.meetingIcon}
-            style={{ width: '24px', height: '24px' }}
-          />
-          &nbsp;
-          {htmlParser.parse(meetingDetail.platform)}
-          &nbsp;&nbsp;
-          <Img
-            src="https://plionpfmgvenmdwwjzac.supabase.co/storage/v1/object/public/email_template_assets/duration.png?t=2024-06-12T05%3A03%3A30.582Z"
-            style={{ width: '24px', height: '24px' }}
-          />
-          &nbsp;
-          {htmlParser.parse(meetingDetail.duration)}
-        </span>
-      </Text>
-    </Container>
-
-
-  );
-};
+const currentYear = new Date().getFullYear();
 
 export const InterviewBookingConfirmation = ({
   emailBody = dummy.emailBody,
@@ -100,37 +56,38 @@ export const InterviewBookingConfirmation = ({
   return (
     <Html>
       <Head />
-      <Tailwind>
+      <Tailwind config={config}>
         <Preview>Interview Booking Confirmation</Preview>
-        <Body className="bg-[#f0f0f0] font-sans  p-[20px]">
+        <Body className="bg-neutral-2 font-sans  p-[20px]">
           <Container className="px-[3px] mx-auto">
-            <Container className="p-[50px] bg-white">
+            <Container className="p-[50px] bg-neutral-1 rounded-[8px]">
               <Img
                 alt="Company logo"
                 className="w-[80px] mb-[10px]"
                 src={companyLogo}
               />
 
-              <Text className="">{htmlParser.parse(emailBody)}</Text>
+              <Container className="text-text-sm text-neutral-12">
+                {htmlParser.parse(emailBody)}
+              </Container>
               {meetingDetails.map((meetingDetail, i) => (
-                <Sessions key={i} meetingDetail={meetingDetail} />
+                <Session key={i} meetingDetail={meetingDetail} />
               ))}
               <Button
-                className="px-3 py-2 bg-[#337FBD] text-white br rounded-md text-[14px]"
+                className="px-3 py-2 bg-accent-9 text-white br rounded-[4px] text-text-xs"
                 href={candidateLink}
               >
                 View Details
               </Button>
             </Container>
-            <Text className="flex items-center text-[10px]  mx-auto w-fit text-gray-500">
+            <Text className="flex items-center text-[10px]  mx-auto w-fit text-neutral-11">
               Powered By
               <Img
                 alt="Aglint Logo"
-                className="line-block"
+                className="line-block mx-2 w-[24px] h-[24px]"
                 src={aglintLogo}
-                style={{ width: '24px', height: '24px' }}
               />
-              @ 2024 Aglint Inc. All Right Reserved
+              @ {currentYear} Aglint Inc. All Right Reserved
             </Text>
           </Container>
         </Body>
@@ -140,8 +97,3 @@ export const InterviewBookingConfirmation = ({
 };
 
 export default InterviewBookingConfirmation;
-
-// [firstName]
-// [jobTitle]
-// [viewDetailsLink]
-// [companyName]
--- apps/aglint-mail/tailwind.config.ts
+++ apps/aglint-mail/tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'tailwindcss';
-import colors = require('@radix-ui/colors');
-import { fontFamily } from 'tailwindcss/defaultTheme';
+// import colors = require('@radix-ui/colors');
+// import { fontFamily } from 'tailwindcss/defaultTheme';
 import plugin from 'tailwindcss/plugin';
 
 const iOsHeight = plugin(({ addUtilities }) => {
@@ -45,37 +45,55 @@ const config: Config = {
           'linear-gradient(45deg, rgba(255,255,255,0) 45%,rgba(255,255,255,1) 50%,rgba(255,255,255,0) 55%,rgba(255,255,255,0) 100%)',
       },
       colors: {
-        cyan: {
-          1: colors.cyanDarkA.cyanA1,
-          2: colors.cyanDarkA.cyanA2,
-          3: colors.cyanDarkA.cyanA3,
-          4: colors.cyanDarkA.cyanA4,
-          5: colors.cyanDarkA.cyanA5,
-          6: colors.cyanDarkA.cyanA6,
-          7: colors.cyanDarkA.cyanA7,
-          8: colors.cyanDarkA.cyanA8,
-          9: colors.cyanDarkA.cyanA9,
-          10: colors.cyanDarkA.cyanA10,
-          11: colors.cyanDarkA.cyanA11,
-          12: colors.cyanDarkA.cyanA12,
+        accent: {
+          1: '#FEFCFB',
+          2: '#FFF7ED',
+          3: '#FFEFD6',
+          4: '#FFDFB5',
+          5: '#FFD19A',
+          6: '#FFC182',
+          7: '#F5AE73',
+          8: '#EC9455',
+          9: '#F76B15',
+          10: '#EF5F00',
+          11: '#CC4E00',
+          12: '#582D1D',
         },
-        slate: {
-          1: colors.slateDarkA.slateA1,
-          2: colors.slateDarkA.slateA2,
-          3: colors.slateDarkA.slateA3,
-          4: colors.slateDarkA.slateA4,
-          5: colors.slateDarkA.slateA5,
-          6: colors.slateDarkA.slateA6,
-          7: colors.slateDarkA.slateA7,
-          8: colors.slateDarkA.slateA8,
-          9: colors.slateDarkA.slateA9,
-          10: colors.slateDarkA.slateA10,
-          11: colors.slateDarkA.slateA11,
-          12: colors.slateDarkA.slateA12,
+        neutral: {
+          1: '#FDFDFC',
+          2: '#F9F9F8',
+          3: '#F1F0EF',
+          4: '#E9E8E6',
+          5: '#E2E1DE',
+          6: '#DAD9D6',
+          7: '#CFCECA',
+          8: '#BCBBB5',
+          9: '#8D8D86',
+          10: '#82827C',
+          11: '#63635E',
+          12: '#21201C',
         },
       },
       fontFamily: {
-        sans: ['var(--font-inter)', ...fontFamily.sans],
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'San Francisco',
+          'Helvetica Neue',
+          'Arial',
+          'sans-serif',
+        ],
+      },
+      fontSize: {
+        'text-xs': ['12px', '16px'], // size 1
+        'text-sm': ['14px', '20px'], // size 2
+        'text-base': ['16px', '24px'], // size 3
+        'text-lg': ['18px', '28px'], // size 4
+        'text-xl': ['20px', '28px'], // size 5
+        'text-2xl': ['24px', '32px'], // size 6
+        'text-3xl': ['30px', '36px'], // size 7
+        'text-4xl': ['36px', '40px'], // size 8
+        'text-5xl': ['48px', '48px'], // size 9
       },
       keyframes: {
         shine: {
@@ -92,3 +110,32 @@ const config: Config = {
   plugins: [iOsHeight],
 };
 export default config;
+
+// accent: {
+//   1: colors.orange.orange1,
+//   2: colors.orange.orange2,
+//   3: colors.orange.orange3,
+//   4: colors.orange.orange4,
+//   5: colors.orange.orange5,
+//   6: colors.orange.orange6,
+//   7: colors.orange.orange7,
+//   8: colors.orange.orange8,
+//   9: colors.orange.orange9,
+//   10: colors.orange.orange10,
+//   11: colors.orange.orange11,
+//   12: colors.orange.orange12,
+// },
+// neutral: {
+//   1: colors.sand.sand1,
+//   2: colors.sand.sand2,
+//   3: colors.sand.sand3,
+//   4: colors.sand.sand4,
+//   5: colors.sand.sand5,
+//   6: colors.sand.sand6,
+//   7: colors.sand.sand7,
+//   8: colors.sand.sand8,
+//   9: colors.sand.sand9,
+//   10: colors.sand.sand10,
+//   11: colors.sand.sand11,
+//   12: colors.sand.sand12,
+// },
