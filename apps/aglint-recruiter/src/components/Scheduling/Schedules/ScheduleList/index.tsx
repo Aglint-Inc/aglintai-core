2db218be7748ba2a839b111106867131906002c8
Ravi K <ravi@aglinthq.com>
2024-06-12 12:35:00 +0530

    No schedule found layout fix

--- apps/aglint-recruiter/src/components/Scheduling/Schedules/ScheduleList/index.tsx
+++ apps/aglint-recruiter/src/components/Scheduling/Schedules/ScheduleList/index.tsx
@@ -1,11 +1,11 @@
 import { Stack } from '@mui/material';
+import { Box } from '@mui/material';
 
 import { AllInterviewEmpty } from '@/devlink2/AllInterviewEmpty';
 import Loader from '@/src/components/Common/Loader';
 
 import ScheduleMeetingList from '../../Common/ModuleSchedules/ScheduleMeetingList';
 import { useScheduleStatesContext } from '../ScheduleStatesContext';
-import { Box } from '@mui/material';
 
 function ScheduleList() {
   const { filterSchedules, loadingSchedules } = useScheduleStatesContext();
@@ -29,7 +29,7 @@ function ScheduleList() {
           }}
         >
           <Box maxWidth="sm" width="300px" p={2}>
-          <AllInterviewEmpty textDynamic='No schedule found' />
+            <AllInterviewEmpty textDynamic='No schedule found' />
           </Box>
         </Box>
       )}
