339f224302336e9c34de964b6628f9f76c5a6b26
ChandraAglint <chandra@aglinthq.com>
2024-06-12 16:45:30 +0530

    radix ui

--- apps/aglint-mail/tailwind.config.ts
+++ apps/aglint-mail/tailwind.config.ts
@@ -45,33 +45,34 @@ const config: Config = {
           'linear-gradient(45deg, rgba(255,255,255,0) 45%,rgba(255,255,255,1) 50%,rgba(255,255,255,0) 55%,rgba(255,255,255,0) 100%)',
       },
       colors: {
+        //radix ui colors
         accent: {
-          1: '#FEFCFB',
-          2: '#FFF7ED',
-          3: '#FFEFD6',
-          4: '#FFDFB5',
-          5: '#FFD19A',
-          6: '#FFC182',
-          7: '#F5AE73',
-          8: '#EC9455',
-          9: '#F76B15',
-          10: '#EF5F00',
-          11: '#CC4E00',
-          12: '#582D1D',
+          1: '#FEFCFB', //orange1
+          2: '#FFF7ED', //orange2
+          3: '#FFEFD6', //orange3
+          4: '#FFDFB5', //orange4
+          5: '#FFD19A', //orange5
+          6: '#FFC182', //orange6
+          7: '#F5AE73', //orange7
+          8: '#EC9455', //orange8
+          9: '#F76B15', //orange9
+          10: '#EF5F00', //orange10
+          11: '#CC4E00', //orange11
+          12: '#582D1D', //orange12
         },
         neutral: {
-          1: '#FDFDFC',
-          2: '#F9F9F8',
-          3: '#F1F0EF',
-          4: '#E9E8E6',
-          5: '#E2E1DE',
-          6: '#DAD9D6',
-          7: '#CFCECA',
-          8: '#BCBBB5',
-          9: '#8D8D86',
-          10: '#82827C',
-          11: '#63635E',
-          12: '#21201C',
+          1: '#FDFDFC', //sand1
+          2: '#F9F9F8', //sand2
+          3: '#F1F0EF', //sand3
+          4: '#E9E8E6', //sand4
+          5: '#E2E1DE', //sand5
+          6: '#DAD9D6', //sand6
+          7: '#CFCECA', //sand7
+          8: '#BCBBB5', //sand8
+          9: '#8D8D86', //sand9
+          10: '#82827C', //sand10
+          11: '#63635E', //sand11
+          12: '#21201C', //sand12
         },
       },
       fontFamily: {
@@ -110,32 +111,3 @@ const config: Config = {
   plugins: [iOsHeight],
 };
 export default config;
-
-// accent: {
-//   1: colors.orange.orange1,
-//   2: colors.orange.orange2,
-//   3: colors.orange.orange3,
-//   4: colors.orange.orange4,
-//   5: colors.orange.orange5,
-//   6: colors.orange.orange6,
-//   7: colors.orange.orange7,
-//   8: colors.orange.orange8,
-//   9: colors.orange.orange9,
-//   10: colors.orange.orange10,
-//   11: colors.orange.orange11,
-//   12: colors.orange.orange12,
-// },
-// neutral: {
-//   1: colors.sand.sand1,
-//   2: colors.sand.sand2,
-//   3: colors.sand.sand3,
-//   4: colors.sand.sand4,
-//   5: colors.sand.sand5,
-//   6: colors.sand.sand6,
-//   7: colors.sand.sand7,
-//   8: colors.sand.sand8,
-//   9: colors.sand.sand9,
-//   10: colors.sand.sand10,
-//   11: colors.sand.sand11,
-//   12: colors.sand.sand12,
-// },
