83d58f08246ca1a26f570d6662730b8409376735
VivekS <vivek@aglinthq.com>
2024-06-12 17:33:12 +0530

    refactor: Update SideNavbar permissions to use arrays

--- apps/aglint-recruiter/src/utils/routing/permissions.ts
+++ apps/aglint-recruiter/src/utils/routing/permissions.ts
@@ -6,7 +6,7 @@ import { PATHS } from '@/script/paths';
 
 type Permissions = {
   // eslint-disable-next-line no-unused-vars
-  [id in (typeof PATHS)[number]]: DatabaseEnums['permissions_type'] | null;
+  [id in (typeof PATHS)[number]]: DatabaseEnums['permissions_type'][];
 };
 
 const DEFAULT: Permissions = Object.assign(
@@ -16,45 +16,56 @@ const DEFAULT: Permissions = Object.assign(
 
 const PERMISSIONS: Permissions = {
   ...DEFAULT,
-  '/tasks': 'tasks_enabled',
-  '/jobs': 'jobs_enabled',
-  '/jobs/create': 'jobs_create',
-  '/jobs/[id]': 'jobs_read',
-  '/jobs/[id]/job-details': 'jobs_update',
-  '/jobs/[id]/profile-score': 'jobs_update',
-  '/jobs/[id]/interview-plan': 'jobs_update',
-  '/jobs/[id]/assessment': 'jobs_update',
-  '/jobs/[id]/screening': 'jobs_update',
-  '/jobs/[id]/hiring-team': 'jobs_update',
-  '/jobs/[id]/email-templates': 'jobs_update',
+  /** All of the permission are required to access this routes
+   * permissions will reduced  using 'and'
+   */
+  '/tasks': ['tasks_enabled'],
+  '/jobs': ['jobs_enabled'],
+  '/jobs/create': ['jobs_create'],
+  '/jobs/[id]': ['jobs_read'],
+  '/jobs/[id]/job-details': ['jobs_update'],
+  '/jobs/[id]/profile-score': ['jobs_update'],
+  '/jobs/[id]/interview-plan': ['jobs_update'],
+  '/jobs/[id]/assessment': ['jobs_update'],
+  '/jobs/[id]/screening': ['jobs_update'],
+  '/jobs/[id]/hiring-team': ['jobs_update'],
+  '/jobs/[id]/email-templates': ['jobs_update'],
 
-  '/scheduling': 'scheduler_enabled',
+  '/scheduling': ['scheduler_enabled'],
 
-  '/company': 'company_setting_enabled',
+  '/company': ['company_setting_enabled'],
 
-  '/api/job/jobApplications/read': 'jobs_read',
-  '/api/getMembersWithRole': 'jobs_read', //change to correct permission
-  '/api/scheduling/fetchUserDetails': 'scheduler_enabled',
-  '/api/scheduling/fetch_interview_session_task': 'scheduler_enabled',
-  '/api/scheduling/fetch_activities': 'scheduler_enabled',
-  '/api/scheduling/get_interview_modules': 'scheduler_enabled',
-  '/api/scheduling/fetch_interview_module_by_id': 'scheduler_enabled',
+  /** Any one of the permission is required to access this apis
+   * permission will reduced  using 'or'
+   */
+  '/api/job/jobApplications/read': ['jobs_read', 'candidates_add'],
+  '/api/getMembersWithRole': ['jobs_read'], //change to correct permission
+  '/api/scheduling/fetchUserDetails': ['scheduler_enabled'],
+  '/api/scheduling/fetch_interview_session_task': ['scheduler_enabled'],
+  '/api/scheduling/fetch_activities': ['scheduler_enabled'],
+  '/api/scheduling/get_interview_modules': ['scheduler_enabled'],
+  '/api/scheduling/fetch_interview_module_by_id': ['scheduler_enabled'],
   // scheduling application apis
-  '/api/scheduling/application/sendtocandidate': 'scheduler_enabled',
-  '/scheduling/application/[application_id]': 'scheduler_enabled',
+  '/api/scheduling/application/sendtocandidate': ['scheduler_enabled'],
+  '/scheduling/application/[application_id]': ['scheduler_enabled'],
   //v1 apis
-  '/api/scheduling/v1/get-candidate-selected-slots': 'scheduler_enabled',
+  '/api/scheduling/v1/get-candidate-selected-slots': ['scheduler_enabled'],
   // request availability apis
-  '/api/scheduling/request_availability/getCandidateRequestData':
+  '/api/scheduling/request_availability/getCandidateRequestData': [
     'scheduler_enabled',
-  '/api/scheduling/request_availability/getTaskIdDetailsByRequestId':
+  ],
+  '/api/scheduling/request_availability/getTaskIdDetailsByRequestId': [
     'scheduler_enabled',
-  '/api/scheduling/request_availability/insertScheduleActivities':
+  ],
+  '/api/scheduling/request_availability/insertScheduleActivities': [
     'scheduler_enabled',
-  '/api/scheduling/request_availability/insertTaskProgress':
+  ],
+  '/api/scheduling/request_availability/insertTaskProgress': [
     'scheduler_enabled',
-  '/api/scheduling/request_availability/updateRequestAvailability':
+  ],
+  '/api/scheduling/request_availability/updateRequestAvailability': [
     'scheduler_enabled',
+  ],
 };
 
 export default PERMISSIONS;
