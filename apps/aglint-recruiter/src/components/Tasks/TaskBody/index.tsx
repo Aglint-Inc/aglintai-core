49c38182d8b30e830b2d37d5a6b06076b4df3a73
Ravi K <ravi@aglinthq.com>
2024-06-14 14:48:01 +0530

    Layout and tax fix

--- apps/aglint-recruiter/src/components/Tasks/TaskBody/index.tsx
+++ apps/aglint-recruiter/src/components/Tasks/TaskBody/index.tsx
@@ -79,7 +79,7 @@ function TaskBody() {
                     );
                   })}
                 <ShowCode.When isTrue={!loadingTasks && tasks.length === 0}>
-                  <Stack height={'40vh'}>
+                  <Stack height={'calc(100vh - 176px)'} style={{ backgroundColor: 'var(--neutral-2)' }}>
                     <TaskEmpty />
                   </Stack>
                 </ShowCode.When>
