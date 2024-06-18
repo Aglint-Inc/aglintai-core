6553c0751bd8a70d338d5288d3d3063729c71e10
Punith G <91651608+MlNl-PEKKA@users.noreply.github.com>
2024-06-18 20:33:23 +0530

    Workflow qol 3 (#651)
    * fix: workflow connector arrow duplication
    
    * fix: workflow deletion popup

---
+++ apps/aglint-recruiter/src/context/Workflows/store/delete.ts
@@ -0,0 +1,32 @@
+import { DatabaseTable, DatabaseView } from '@aglint/shared-types';
+import { CreateSlice } from '.';
+
+type Deletion = {
+  open: boolean;
+  workflow: Pick<DatabaseView['workflow_view'], 'id' | 'jobs'>;
+};
+
+export type DeletionSlice = {
+  deletion: Deletion;
+  // eslint-disable-next-line no-unused-vars
+  setDeletion: (deletion: Deletion) => void;
+  closeDeletion: () => void;
+};
+
+const initialDeletion: Deletion = {
+  open: false,
+  workflow: null,
+};
+
+export const createDeletionSlice: CreateSlice<DeletionSlice> = (set) => ({
+  deletion: initialDeletion,
+  setDeletion: (deletion) => set({ deletion }),
+  closeDeletion: () => {
+    set((state) => ({ deletion: { ...state.deletion, open: false } }));
+    setTimeout(
+      () =>
+        set((state) => ({ deletion: { ...state.deletion, workflow: null } })),
+      400,
+    );
+  },
+});
