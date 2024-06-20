47f64ff8e860f091d68823b0ce82c582188ed2b4
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-06-20 16:42:13 +0530

    Tasks view created (#662)
    * fix: icon added for Session card
    
    * tasks view created
    
    * tasks view created

---
+++ supabase/migrations/20240620110849_created_tasks_view.sql
@@ -0,0 +1,27 @@
+create or replace view "public"."tasks_view" as  SELECT DISTINCT ON (new_tasks.id) new_tasks.id,
+    new_tasks.created_at,
+    new_tasks.name,
+    new_tasks.due_date,
+    new_tasks.assignee,
+    new_tasks.start_date,
+    new_tasks.session_ids,
+    new_tasks.application_id,
+    new_tasks.recruiter_id,
+    new_tasks.schedule_date_range,
+    new_tasks.created_by,
+    new_tasks.type,
+    new_tasks.status,
+    new_tasks.agent,
+    new_tasks.filter_id,
+    new_tasks.priority,
+    new_tasks.task_owner,
+    new_tasks.trigger_count,
+    new_tasks.task_action,
+    new_tasks.request_availability_id,
+    json_build_object('id', new_tasks_progress.id, 'progress_type', new_tasks_progress.progress_type, 'created_at', new_tasks_progress.created_at, 'created_by', new_tasks_progress.created_by, 'jsonb_data', new_tasks_progress.jsonb_data, 'title_meta', new_tasks_progress.title_meta) AS last_progress
+   FROM (new_tasks
+     LEFT JOIN new_tasks_progress ON ((new_tasks.id = new_tasks_progress.task_id)))
+  ORDER BY new_tasks.id, new_tasks_progress.created_at DESC;
+
+
+
