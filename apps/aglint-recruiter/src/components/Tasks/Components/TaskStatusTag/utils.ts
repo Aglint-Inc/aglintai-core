13948b5b7ce637f74981433ab3c104c2aab6fef4
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-06-24 19:42:59 +0530

    fix: Status tags update

--- apps/aglint-recruiter/src/components/Tasks/Components/TaskStatusTag/utils.ts
+++ apps/aglint-recruiter/src/components/Tasks/Components/TaskStatusTag/utils.ts
@@ -3,41 +3,30 @@ import { DatabaseEnums, DatabaseView } from '@aglint/shared-types';
 import dayjs from '@/src/utils/dayjs';
 
 export type indicatorType =
-  | 'REQUEST_AVAILABILITY'
-  | 'REQUEST_SUBMITTED'
+  | 'READY_TO_SCHEDULE'
   | 'AWAITING_RESPONSE'
   | 'NO_RESPONSE'
   | 'MAIL_SENT'
-  | 'SCHEDULE'
-  | 'SEND_LINK'
   | 'BOOKED'
   | 'SCHEDULED'
   | 'RESCHEDULE'
-  | 'ACTION_NEEDED_AGENT_FAIL'
+  | 'ACTION_NEEDED'
   | 'UNKNOWN_STATUS';
 
 export function getIndicatorMessage(indicator: indicatorType): string {
   switch (indicator) {
-    case 'REQUEST_AVAILABILITY':
-      return 'Request Availability';
-    case 'REQUEST_SUBMITTED':
-      return 'Request Submitted';
+    case 'READY_TO_SCHEDULE':
+      return 'Ready to Schedule';
     case 'AWAITING_RESPONSE':
       return 'Awaiting Response';
     case 'NO_RESPONSE':
       return `No Response`;
-    case 'SCHEDULE':
-      return 'Schedule';
-    case 'SEND_LINK':
-      return 'Send Link';
     case 'BOOKED':
       return 'Booked';
     case 'SCHEDULED':
-      return 'Scheduled';
-    case 'RESCHEDULE':
-      return 'Reschedule';
-    case 'ACTION_NEEDED_AGENT_FAIL':
-      return 'Action Needed (Agent Fail)';
+      return 'Ready to Schedule';
+    case 'ACTION_NEEDED':
+      return 'Action Needed';
     case 'UNKNOWN_STATUS':
       return 'Unknown Status';
     case 'MAIL_SENT':
@@ -69,21 +58,19 @@ export function getIndicator({
     return 'NO_RESPONSE' as indicatorType;
   }
 
-  if (progress_type === 'send_email') {
-    return 'MAIL_SENT' as indicatorType;
+  if (
+    progress_type === 'request_availability' ||
+    progress_type === 'send_email'
+  ) {
+    return 'AWAITING_RESPONSE' as indicatorType;
   }
 
-  // progress_type
-  if (progress_type === 'request_availability') {
-    return 'REQUEST_AVAILABILITY' as indicatorType;
-  }
-
-  if (progress_type === 'self_schedule' || progress_type === 'schedule') {
-    return 'SCHEDULE' as indicatorType;
-  }
-
-  if (progress_type === 'request_availability_list') {
-    return 'REQUEST_SUBMITTED' as indicatorType;
+  if (
+    progress_type === 'self_schedule' ||
+    progress_type === 'schedule' ||
+    progress_type === 'request_availability_list'
+  ) {
+    return 'READY_TO_SCHEDULE' as indicatorType;
   }
 
   if (
@@ -99,7 +86,7 @@ export function getIndicator({
     progress_type === 'email_failed' ||
     task.status === 'failed'
   ) {
-    return 'ACTION_NEEDED_AGENT_FAIL' as indicatorType;
+    return 'ACTION_NEEDED' as indicatorType;
   }
   return 'UNKNOWN_STATUS' as indicatorType;
 }
