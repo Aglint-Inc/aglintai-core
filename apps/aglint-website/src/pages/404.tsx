ebaf32a2ffd1b7be80be3d22b4d942868737ad48
Dileep B C <135205237+dileepbc3141@users.noreply.github.com>
2024-06-24 13:35:31 +0530

    feat : Moved aglint-website to monorepo (#682)
    * feat : moved agint-website to monorepo
    
    * fix : react dependecies

---
+++ apps/aglint-website/src/pages/404.tsx
@@ -0,0 +1,12 @@
+import Seo from '../components/Seo';
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <Seo
+        title='Page Not Found - 404 | Aglint AI'
+        description='AI for People Products'
+      />
+    </>
+  );
+}
