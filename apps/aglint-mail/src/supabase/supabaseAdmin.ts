940c1a78447b0b6c6a282fd20abc6746fb3d979a
Chinmai <chinmai@aglinthq.com>
2024-06-26 12:45:33 +0530

    aglint mail testing

--- apps/aglint-mail/src/supabase/supabaseAdmin.ts
+++ apps/aglint-mail/src/supabase/supabaseAdmin.ts
@@ -3,7 +3,7 @@ import type { DB } from '@aglint/shared-types';
 
 export const supabaseAdmin = createClient<DB>(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY,
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaW9ucGZtZ3Zlbm1kd3dqemFjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5ODY1ODE4MywiZXhwIjoyMDE0MjM0MTgzfQ.mSmvcr8K9STsaMCuMwbtdA9uwa7RIrRVs1KPzD2Inws',
 );
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-constraint
