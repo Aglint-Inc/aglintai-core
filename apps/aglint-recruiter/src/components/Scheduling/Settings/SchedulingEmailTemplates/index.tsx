57aec954f9ebd1002c42410c9d491e553b459797
ChandraAglint <chandra@aglinthq.com>
2024-06-27 12:12:25 +0530

    mulitple time reload fix

--- apps/aglint-recruiter/src/components/Scheduling/Settings/SchedulingEmailTemplates/index.tsx
+++ apps/aglint-recruiter/src/components/Scheduling/Settings/SchedulingEmailTemplates/index.tsx
@@ -37,16 +37,13 @@ function SchedulerEmailTemps() {
   useEffect(() => {
     if (emailTemplates.data) {
       setEmailTemplate([...emailTemplates.data]);
+      setSelectedTemplate(emailTemplates.data[11]);
     }
 
     setTimeout(() => {
       setIsEditorLoad(false);
     }, 500);
   }, [emailTemplates]);
-
-  useEffect(() => {
-    setSelectedTemplate(emailTemplates.data[3]);
-  }, []);
 
   async function updateEmail({
     id,
@@ -124,11 +121,13 @@ function SchedulerEmailTemps() {
                   textTitle={emailTemplateCopy[emailPath.type]?.listing}
                   onClickApplicationRecieved={{
                     onClick: () => {
-                      setTipTapLoder(true);
-                      setSelectedTemplate(emailPath);
-                      setTimeout(() => {
-                        setTipTapLoder(false);
-                      }, 500);
+                      if (selectedTemplate.id !== emailPath.id) {
+                        setTipTapLoder(true);
+                        setSelectedTemplate(emailPath);
+                        setTimeout(() => {
+                          setTipTapLoder(false);
+                        }, 500);
+                      }
                     },
                   }}
                 />
