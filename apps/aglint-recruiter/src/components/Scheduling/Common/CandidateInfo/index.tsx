1cda43d83036cef03a9357ee73d4bfe71cc6a033
Punith G <91651608+MlNl-PEKKA@users.noreply.github.com>
2024-06-27 12:07:28 +0530

    fix: add candidate info to candidate details (#773)

--- apps/aglint-recruiter/src/components/Scheduling/Common/CandidateInfo/index.tsx
+++ apps/aglint-recruiter/src/components/Scheduling/Common/CandidateInfo/index.tsx
@@ -6,6 +6,9 @@ function CandidateInfo({ application_id, job_id }) {
   return (
     <Stack spacing={'var(--space-4)'} padding={'var(--space-4)'}>
       <Application application_id={application_id} job_id={job_id}>
+        <Application.Body.TopBar>
+          <Application.Body.TopBar.Info />
+        </Application.Body.TopBar>
         <Application.Body.Meta />
         <Application.Body.Details />
       </Application>
