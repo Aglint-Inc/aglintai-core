02dbc4bc65197fa024a85533171059e9c66d5a25
Dileep B C <dileep@aglinthq.com>
2024-06-28 23:08:44 +0530

    fix : lint

--- apps/aglint-recruiter/src/components/Jobs/Job/Dashboard/index.tsx
+++ apps/aglint-recruiter/src/components/Jobs/Job/Dashboard/index.tsx
@@ -61,7 +61,7 @@ import { capitalize, capitalizeAll } from '@/src/utils/text/textUtils';
 import toast from '@/src/utils/toast';
 
 import { UploadApplications } from '../Common/UploadApplications';
-import DashboardBarChart from './Bar';
+import DashboardBarChart from './BarChart2';
 import DashboardDoughnutChart from './doughnut';
 import DashboardLineChart from './lineChart';
 import TenureAndExpSummary from './tenureAndExpSummary';
