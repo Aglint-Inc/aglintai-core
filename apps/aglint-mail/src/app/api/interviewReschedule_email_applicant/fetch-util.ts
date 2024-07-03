d2c7ef5b51f684166b5b35b2c9758d51f6f01adb
Dileep B C <dileep@aglinthq.com>
2024-07-03 15:31:32 +0530

    fix : lint

--- apps/aglint-mail/src/app/api/interviewReschedule_email_applicant/fetch-util.ts
+++ apps/aglint-mail/src/app/api/interviewReschedule_email_applicant/fetch-util.ts
@@ -20,30 +20,27 @@ import { fetchCompEmailTemp } from '../../../utils/apiUtils/fetchCompEmailTemp';
 export async function fetchUtil(
   req_body: EmailTemplateAPi<'interviewReschedule_email_applicant'>['api_payload'],
 ) {
-  const { application_id, session_ids, self_schedule_link } = req_body;
+  const { self_schedule_link } = req_body;
 
+  const int_sessions = supabaseWrap(
+    await supabaseAdmin
+      .from('interview_session')
+      .select(
+        '*,interview_meeting(*, recruiter_user(first_name,last_name,email,scheduling_settings))',
+      )
+      .in('id', req_body.session_ids),
+  );
   const [candidateJob] = supabaseWrap(
     await supabaseAdmin
       .from('applications')
       .select(
-        'candidates(first_name,last_name,email,recruiter_id,recruiter(logo),timezone),public_jobs(job_title,company)',
+        'candidates(first_name,last_name,email,recruiter_id,recruiter(logo)),public_jobs(job_title,company)',
       )
-      .eq('id', application_id),
+      .eq('id', req_body.application_id),
   );
-
-  const [recruiter_user] = supabaseWrap(
-    await supabaseAdmin
-      .from('recruiter_user')
-      .select('first_name,last_name,scheduling_settings')
-      .eq('user_id', candidateJob.public_jobs.recruiter),
-  );
-  const sessions = supabaseWrap(
-    await supabaseAdmin
-      .from('interview_session')
-      .select('*,interview_meeting(*)')
-      .in('id', session_ids),
-  );
-  const recruiter_tz = recruiter_user.scheduling_settings.timeZone.tzCode;
+  const meeting_organizer = int_sessions[0].interview_meeting.recruiter_user;
+  const org_tz = meeting_organizer.scheduling_settings.timeZone.tzCode;
+  //
   const {
     candidates: {
       email: cand_email,
@@ -61,26 +58,28 @@ export async function fetchUtil(
   );
   const cand_tz = 'America/Los_Angeles';
 
-  const meeting_details: MeetingDetailCardType[] = sessions.map((session) => {
-    const {
-      interview_meeting: { start_time, end_time },
-      name,
-      schedule_type,
-      session_duration,
-      session_type,
-    } = session;
-    return {
-      date: dayjsLocal(start_time)
-        .tz(cand_tz)
-        .format(DAYJS_FORMATS.DATE_FORMAT),
-      time: `${dayjsLocal(start_time).tz(cand_tz).format(DAYJS_FORMATS.STAR_TIME_FORMAT)} - ${dayjsLocal(end_time).tz(cand_tz).format(DAYJS_FORMATS.END_TIME_FORMAT)}`,
-      sessionType: name,
-      platform: platformRemoveUnderscore(schedule_type),
-      duration: durationCalculator(session_duration),
-      sessionTypeIcon: sessionTypeIcon(session_type),
-      meetingIcon: scheduleTypeIcon(schedule_type),
-    };
-  });
+  const meeting_details: MeetingDetailCardType[] = int_sessions.map(
+    (session) => {
+      const {
+        interview_meeting: { start_time, end_time },
+        name,
+        schedule_type,
+        session_duration,
+        session_type,
+      } = session;
+      return {
+        date: dayjsLocal(start_time)
+          .tz(cand_tz)
+          .format(DAYJS_FORMATS.DATE_FORMAT),
+        time: `${dayjsLocal(start_time).tz(cand_tz).format(DAYJS_FORMATS.STAR_TIME_FORMAT)} - ${dayjsLocal(end_time).tz(cand_tz).format(DAYJS_FORMATS.END_TIME_FORMAT)}`,
+        sessionType: name,
+        platform: platformRemoveUnderscore(schedule_type),
+        duration: durationCalculator(session_duration),
+        sessionTypeIcon: sessionTypeIcon(session_type),
+        meetingIcon: scheduleTypeIcon(schedule_type),
+      };
+    },
+  );
 
   const scheduleLink = `<a href='${self_schedule_link}'>click here</a>`;
 
@@ -90,15 +89,15 @@ export async function fetchUtil(
       jobRole: job_title,
       companyName: company,
       selfScheduleLink: scheduleLink,
-      recruiterName: getFullName(
-        recruiter_user.first_name,
-        recruiter_user.last_name,
+      OrganizerName: getFullName(
+        meeting_organizer.first_name,
+        meeting_organizer.last_name,
       ),
       candidateLastName: last_name,
       candidateName: getFullName(first_name, last_name),
-      recruiterFirstName: recruiter_user.first_name,
-      recruiterLastName: recruiter_user.last_name,
-      recruiterTimeZone: recruiter_tz,
+      OrganizerFirstName: meeting_organizer.first_name,
+      OrganizerLastName: meeting_organizer.last_name,
+      OrganizerTimeZone: org_tz,
     };
 
   const filled_comp_template = fillCompEmailTemplate(
