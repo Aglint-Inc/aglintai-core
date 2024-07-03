cb82e89747f42cde68018e8598dae5968d3fee0b
Chinmai <chinmai@aglinthq.com>
2024-07-03 20:23:52 +0530

    fix toast message timings

--- apps/aglint-recruiter/src/utils/toast.js
+++ apps/aglint-recruiter/src/utils/toast.js
@@ -4,7 +4,7 @@ import { createToast } from 'vercel-toast';
 const toast = {
   error: (message) => {
     createToast(message, {
-      timeout: 300000,
+      timeout: 3000,
       type: 'error',
       cancel: '✕',
     });
@@ -12,7 +12,7 @@ const toast = {
 
   success: (message) => {
     createToast(message, {
-      timeout: 300000,
+      timeout: 3000,
       type: 'success',
       cancel: '✕',
     });
@@ -20,7 +20,7 @@ const toast = {
 
   warning: (message) => {
     createToast(message, {
-      timeout: 300000,
+      timeout: 3000,
       type: 'warning',
       cancel: '✕',
     });
@@ -28,7 +28,7 @@ const toast = {
 
   action: (message, onUndo) => {
     createToast(message, {
-      timeout: 300000,
+      timeout: 3000,
       type: 'dark',
       action: {
         text: 'Undo',
@@ -43,7 +43,7 @@ const toast = {
 
   message: (message) => {
     createToast(message, {
-      timeout: 300000,
+      timeout: 3000,
       cancel: '✕',
       default: '',
       type: 'dark',
