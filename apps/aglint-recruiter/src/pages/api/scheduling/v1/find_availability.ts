d24e0a0744e54c3c459f5091edd1b44b9c7aceee
Dileep B C <dileep@aglinthq.com>
2024-07-05 16:27:02 +0530

    fix : api pause and calender not connected error info

--- apps/aglint-recruiter/src/pages/api/scheduling/v1/find_availability.ts
+++ apps/aglint-recruiter/src/pages/api/scheduling/v1/find_availability.ts
@@ -26,11 +26,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     await cand_schedule.fetchDetails();
     await cand_schedule.fetchIntsEventsFreeTimeWorkHrs();
     const slots = cand_schedule.findAvailabilitySlotsDateRange();
-    // const combs = ScheduleUtils.createCombsForMultiDaySlots(slots);
     return res.status(200).json(slots);
   } catch (error) {
     console.log(error);
-    return res.status(500).send(error.message);
+    return res
+      .status(error.status ?? 500)
+      .json({ name: error.name, message: error.message });
   }
 };
 
