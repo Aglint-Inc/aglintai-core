04c0efaa7a699bb4524c91e953c8f7d388fbea4d
Chinmai <chinmai@aglinthq.com>
2024-07-09 13:49:37 +0530

    wip filter

--- apps/aglint-recruiter/src/components/Scheduling/CandidateDetails/SchedulingDrawer/StepScheduleFilter/utils.ts
+++ apps/aglint-recruiter/src/components/Scheduling/CandidateDetails/SchedulingDrawer/StepScheduleFilter/utils.ts
@@ -2,9 +2,10 @@
 import { PlanCombinationRespType } from '@aglint/shared-types';
 import dayjs from 'dayjs';
 
+import { createCombsForMultiDaySlots } from '@/src/services/CandidateScheduleV2/utils/createCombsForMultiDaySlots';
+
 import { ApiResponseFindAvailability } from '../../types';
 import { SchedulingFlow } from '../store';
-import { createCombsForMultiDaySlots } from '@/src/services/CandidateScheduleV2/utils/createCombsForMultiDaySlots';
 
 export const filterByDateRanges = ({
   schedulingOptions,
@@ -51,118 +52,127 @@ export function filterSchedulingOptionsArray({
   filters: SchedulingFlow['filters'];
   selectedSessionsNo: number;
 }) {
-  const allFilteredOptions = schedulingOptions.map((option) =>
-    option.interview_rounds.map((items) => {
-      let allOptions = items;
+  const allFilteredOptions: ApiResponseFindAvailability = schedulingOptions.map(
+    (option) => ({
+      ...option,
+      interview_rounds: option.interview_rounds.map((items) => {
+        let allOptions = items;
 
-      if (filters.preferredDateRanges.length > 0) {
-        allOptions = filterByDateRanges({
-          schedulingOptions: allOptions,
-          preferredDateRanges: filters.preferredDateRanges,
-        });
-      }
+        if (filters.preferredDateRanges.length > 0) {
+          allOptions = filterByDateRanges({
+            schedulingOptions: allOptions,
+            preferredDateRanges: filters.preferredDateRanges,
+          });
+        }
 
-      if (filters.isWorkLoad) {
-        allOptions.plans
-          .map((option) => {
-            const totalWorkload = option.sessions.reduce(
-              (acc, session) => acc + session.day_load_den,
-              0,
-            );
-            return {
-              ...option,
-              totalWorkload,
-            };
-          })
-          .sort((a, b) => a.totalWorkload - b.totalWorkload);
-      }
+        if (filters.isWorkLoad) {
+          allOptions.plans
+            .map((option) => {
+              const totalWorkload = option.sessions.reduce(
+                (acc, session) => acc + session.day_load_den,
+                0,
+              );
+              return {
+                ...option,
+                totalWorkload,
+              };
+            })
+            .sort((a, b) => a.totalWorkload - b.totalWorkload);
+        }
 
-      if (filters.preferredInterviewers.length > 0) {
-        allOptions.plans.sort((a, b) => {
-          const aHasPreferred = a.sessions.some((session) =>
-            hasPreferredInterviewers({ session, filters }),
-          )
-            ? 0
-            : 1;
-          const bHasPreferred = b.sessions.some((session) =>
-            hasPreferredInterviewers({ session, filters }),
-          )
-            ? 0
-            : 1;
-          return aHasPreferred - bHasPreferred;
-        });
-      }
+        if (filters.preferredInterviewers.length > 0) {
+          allOptions.plans.sort((a, b) => {
+            const aHasPreferred = a.sessions.some((session) =>
+              hasPreferredInterviewers({ session, filters }),
+            )
+              ? 0
+              : 1;
+            const bHasPreferred = b.sessions.some((session) =>
+              hasPreferredInterviewers({ session, filters }),
+            )
+              ? 0
+              : 1;
+            return aHasPreferred - bHasPreferred;
+          });
+        }
 
-      let noConflicts: PlanCombinationRespType[] = [];
-      let softConflicts: PlanCombinationRespType[] = [];
-      let hardConflicts: PlanCombinationRespType[] = [];
-      let outSideWorkHours: PlanCombinationRespType[] = [];
+        let noConflicts: ApiResponseFindAvailability[0]['interview_rounds'][0]['plans'] =
+          null;
+        let softConflicts: ApiResponseFindAvailability[0]['interview_rounds'][0]['plans'] =
+          null;
+        let hardConflicts: ApiResponseFindAvailability[0]['interview_rounds'][0]['plans'] =
+          null;
+        let outSideWorkHours: ApiResponseFindAvailability[0]['interview_rounds'][0]['plans'] =
+          null;
 
-      // Filter for no conflicts
-      if (filters.isNoConflicts) {
-        noConflicts = allOptions.plans.filter((option) =>
-          option.sessions.every((session) => !session.is_conflict),
-        );
-      }
+        // Filter for no conflicts
+        if (filters.isNoConflicts) {
+          noConflicts = allOptions.plans.filter((option) =>
+            option.sessions.every(
+              (session) => session.conflict_types.length === 0,
+            ),
+          );
+        }
 
-      // Filter for soft conflicts
-      if (filters.isSoftConflicts) {
-        softConflicts = allOptions.plans.filter((option) =>
-          option.sessions.some((session) =>
-            session.ints_conflicts.some((conflict) =>
-              conflict.conflict_reasons.some(
-                (reason) => reason.conflict_type === 'soft',
+        // Filter for soft conflicts
+        if (filters.isSoftConflicts) {
+          softConflicts = allOptions.plans.filter((option) =>
+            option.sessions.every(
+              (session) =>
+                session.conflict_types.length > 0 &&
+                (session.conflict_types.every(
+                  (conflict) => conflict === 'soft',
+                ) ||
+                  session.conflict_types.length === 0),
+            ),
+          );
+        }
+
+        // Filter for hard conflicts
+        if (filters.isHardConflicts) {
+          hardConflicts = allOptions.plans.filter((option) =>
+            option.sessions.some((session) =>
+              session.conflict_types.some(
+                (conflict) =>
+                  conflict !== 'soft' && conflict !== 'out_of_working_hours',
               ),
             ),
-          ),
-        );
-      }
+          );
+        }
 
-      // Filter for hard conflicts
-      if (filters.isHardConflicts) {
-        hardConflicts = allOptions.plans.filter((option) =>
-          option.sessions.some((session) =>
-            session.ints_conflicts.some((conflict) =>
-              conflict.conflict_reasons.some(
-                (reason) =>
-                  reason.conflict_type !== 'soft' &&
-                  reason.conflict_type !== 'out_of_working_hours',
-              ),
+        // Filter for out of working hours
+        if (filters.isOutSideWorkHours) {
+          outSideWorkHours = allOptions.plans.filter((option) =>
+            option.sessions.some((session) =>
+              session.conflict_types.includes('out_of_working_hours'),
             ),
-          ),
-        );
-      }
+          );
+        }
 
-      if (filters.isOutSideWorkHours) {
-        outSideWorkHours = allOptions.plans.filter((option) =>
-          option.sessions.some((session) =>
-            session.ints_conflicts.some((conflict) =>
-              conflict.conflict_reasons.some(
-                (reason) => reason.conflict_type === 'out_of_working_hours',
-              ),
-            ),
-          ),
-        );
-      }
+        const filteredOptions = {
+          ...allOptions,
+          plans: [
+            ...new Set([
+              ...(noConflicts ? noConflicts : []),
+              ...(hardConflicts ? hardConflicts : []),
+              ...(outSideWorkHours ? outSideWorkHours : []),
+              ...(softConflicts ? softConflicts : []),
+            ]),
+          ],
+        };
 
-      const allFilteredOptionsSet = new Set([
-        ...noConflicts,
-        ...softConflicts,
-        ...hardConflicts,
-        ...outSideWorkHours,
-      ]);
-
-      return [...allFilteredOptionsSet].filter(
-        (option) => option.sessions.length > 0,
-      );
+        return filteredOptions;
+      }),
     }),
   );
 
-  const combs = createCombsForMultiDaySlots(allFilteredOptions).flatMap(
-    (comb) => comb,
-  );
+  const combs = [];
 
-  console.log(combs);
+  // const combs = createCombsForMultiDaySlots(allFilteredOptions).flatMap(
+  //   (comb) => comb,
+  // );
+
+  console.log(allFilteredOptions, 'allFilteredOptions');
 
   const numberNoConflicts = combs
     .map((comb) => comb.sessions.filter((session) => !session.is_conflict))
