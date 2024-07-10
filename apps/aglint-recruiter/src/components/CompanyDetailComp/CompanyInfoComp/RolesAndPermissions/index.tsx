661e1af2aee0e124ff6b6ec118dea57a362c503d
viveks14 <129153058+viveks14@users.noreply.github.com>
2024-07-10 07:28:59 +0530

    Fix/role setting order (#1105)
    * sync:devlink
    
    * fix:order
    
    * fix:lint

--- apps/aglint-recruiter/src/components/CompanyDetailComp/CompanyInfoComp/RolesAndPermissions/index.tsx
+++ apps/aglint-recruiter/src/components/CompanyDetailComp/CompanyInfoComp/RolesAndPermissions/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable security/detect-object-injection */
-import { Avatar, Skeleton, Stack } from '@mui/material';
+import { Avatar, Skeleton } from '@mui/material';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import React, { useState } from 'react';
 
@@ -39,17 +39,15 @@ function RolesAndPermissionsComponent() {
       updateRoles={handelUpdateRole}
     />
   ) : (
-    <Stack width={'100%'} padding={3}>
-      <RolesAndPermissions
-        slotRolesRow={
-          <RoleTable
-            roles={data?.rolesAndPermissions || {}}
-            loading={loading}
-            setRole={setRole}
-          />
-        }
-      />
-    </Stack>
+    <RolesAndPermissions
+      slotRolesRow={
+        <RoleTable
+          roles={data?.rolesAndPermissions || {}}
+          loading={loading}
+          setRole={setRole}
+        />
+      }
+    />
   );
 }
 
@@ -124,47 +122,49 @@ const RoleTable = ({
           }
         />,
       ]
-    : Object.keys(roles || {}).map((item) => {
-        const role = roles[item];
-        const count = role.assignedTo.length;
-        return (
-          <RolesRow
-            key={role.id}
-            textRole={capitalizeFirstLetter(role.name)}
-            textDescription={role.description}
-            onClickRow={{
-              onClick: () => {
-                setRole({ ...role });
-              },
-            }}
-            slotAvatars={
-              <>
-                {role.assignedTo.slice(0, 3).map((user_id) => {
-                  const user = members.find(
-                    (member) => member.user_id === user_id,
-                  );
-                  if (!user) return;
-                  return (
-                    <Avatar
-                      key={user_id}
-                      src={user.profile_image}
-                      variant='rounded'
-                      alt={user.first_name}
-                      sx={{ height: '24px', width: '24px' }}
+    : Object.keys(roles || {})
+        .sort((a, b) => rolesOrder[roles[a].name] - rolesOrder[roles[b].name])
+        .map((item) => {
+          const role = roles[item];
+          const count = role.assignedTo.length;
+          return (
+            <RolesRow
+              key={role.id}
+              textRole={capitalizeFirstLetter(role.name)}
+              textDescription={role.description}
+              onClickRow={{
+                onClick: () => {
+                  setRole({ ...role });
+                },
+              }}
+              slotAvatars={
+                <>
+                  {role.assignedTo.slice(0, 3).map((user_id) => {
+                    const user = members.find(
+                      (member) => member.user_id === user_id,
+                    );
+                    if (!user) return;
+                    return (
+                      <Avatar
+                        key={user_id}
+                        src={user.profile_image}
+                        variant='rounded'
+                        alt={user.first_name}
+                        sx={{ height: '24px', width: '24px' }}
+                      />
+                    );
+                  })}
+                  {count > 3 && (
+                    <GlobalBadge
+                      textBadge={`+${count - 3} more.`}
+                      color={'neutral'}
                     />
-                  );
-                })}
-                {count > 3 && (
-                  <GlobalBadge
-                    textBadge={`+${count - 3} more.`}
-                    color={'neutral'}
-                  />
-                )}
-              </>
-            }
-          />
-        );
-      });
+                  )}
+                </>
+              }
+            />
+          );
+        });
 };
 
 const useRoleAndPermissions = () => {
@@ -178,29 +178,24 @@ const useRoleAndPermissions = () => {
       name: string;
     }
   >(null);
-  const roleDetails =
-    role?.permissions.reduce(
-      (acc, curr) => {
-        const temp = temp_modules.find((name) =>
-          app_modules
-            .find((mod) => mod.name == name)
-            ?.permissions.includes(curr.name),
-        );
-        if (temp) {
-          acc[temp] = acc[temp] || ({} as (typeof acc)[string]);
-          acc[temp].description =
-            app_modules.find((mod) => mod.name == temp)?.description || '';
-          acc[temp].permissions = [...(acc[temp]?.permissions || []), curr];
-        }
-        return acc;
-      },
-      {} as {
-        [key: string]: {
-          description: string;
-          permissions: (typeof role)['permissions'];
-        };
-      },
-    ) || {};
+  const roleDetails = app_modules.reduce(
+    (acc, curr) => {
+      acc[curr.name] = {
+        description: curr.description,
+        permissions: curr.permissions.map((permission) =>
+          role?.permissions.find((pre) => pre.name == permission),
+        ),
+      };
+      return acc;
+    },
+    {} as {
+      [key: string]: {
+        description: string;
+        permissions: (typeof role)['permissions'];
+      };
+    },
+  );
+
   const handelUpdateRole = async (data: Parameters<typeof updateRole>['0']) => {
     return mutateAsync(data);
   };
@@ -324,137 +319,6 @@ const getRoleAndPermissionsWithUserCount = async () => {
     {},
   );
 };
-
-const app_modules: {
-  name: string;
-  description: string;
-  permissions: string[];
-}[] = [
-  {
-    name: 'candidate permissions',
-    description:
-      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
-    permissions: [
-      'candidates_add',
-      'candidates_read',
-      'candidates_update',
-      'candidates_delete',
-      'candidates_moveStage',
-    ],
-  },
-  {
-    name: 'job permissions',
-    description:
-      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Jobs module:',
-    permissions: [
-      'jobs_create',
-      'jobs_read',
-      'jobs_update',
-      'jobs_delete',
-      'jobs_publish',
-      'jobs_unpublish',
-      'jobs_archive',
-      'jobs_restore',
-      'jobs_assignHiringManager',
-      'jobs_assignRecruiter',
-      'jobs_assignCoordinator',
-      'jobs_assignSourcer',
-    ],
-  },
-
-  {
-    name: 'profile score permissions',
-    description:
-      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
-    permissions: ['profileScore_view', 'profileScore_update'],
-  },
-  {
-    name: 'interview permissions',
-    description:
-      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
-    permissions: [
-      'interviews_schedule',
-      'interviews_read',
-      'interviews_update',
-      'interviews_delete',
-    ],
-  },
-  {
-    name: 'report permissions',
-    description:
-      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
-    permissions: ['reports_generate', 'reports_view', 'reports_export'],
-  },
-  {
-    name: 'settings permissions',
-    description:
-      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
-    permissions: ['settings_view', 'settings_update'],
-  },
-  {
-    name: 'task permissions',
-    description:
-      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
-    permissions: [
-      'tasks_enabled',
-      'tasks_create',
-      'tasks_read',
-      'tasks_update',
-      'tasks_delete',
-    ],
-  },
-  {
-    name: 'scheduler permissions',
-    description:
-      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
-    permissions: [
-      'scheduler_enabled',
-      'scheduler_create',
-      'scheduler_read',
-      'scheduler_update',
-      'scheduler_delete',
-      'scheduler_request_availability',
-      'scheduler_send_scheduling',
-      'scheduler_interview_types_create',
-      'scheduler_interview_types_read',
-      'scheduler_interview_types_update',
-      'scheduler_interviewer_edit',
-    ],
-  },
-  {
-    name: 'miscellaneous permissions',
-    description:
-      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
-    permissions: [
-      'jobs_enabled',
-      'sourcing_enabled',
-      'phone_screening_enabled',
-      'assessment_enabled',
-      'integrations_enabled',
-      'company_setting_enabled',
-      'workflow_enabled',
-      'workflow_create',
-      'workflow_read',
-      'workflow_update',
-      'workflow_delete',
-      'team_enabled',
-      'team_create',
-      'team_read',
-      'team_update',
-      'team_delete',
-      'settings_scheduler_enable',
-      'settings_scheduler_update',
-      'settings_company_enable',
-      'settings_company_update',
-      'settings_team_enable',
-      'settings_team_update',
-      'settings_roles_enable',
-      'settings_roles_update',
-    ],
-  },
-];
-
-const temp_modules = app_modules.map((item) => item.name);
 function RoleDetails({
   role,
   roleDetails,
@@ -497,40 +361,43 @@ function RoleDetails({
         />
       }
       slotPermissions={Object.entries(roleDetails || {}).map(
-        ([module, { description, permissions }]) => (
-          <Permissions
-            key={module}
-            textDescription={description}
-            textTitle={capitalizeFirstLetter(module)}
-            slotToggleWithText={permissions?.map((permission) => {
-              return (
-                <ToggleWithText
-                  key={permission.id}
-                  textToggleLight={permission.title}
-                  slotToggle={
-                    <AntSwitch
-                      checked={permission.isActive}
-                      onClick={() => {
-                        const data = {
-                          add: null,
-                          delete: null,
-                          role_id: role.id,
-                        };
+        ([module, { description, permissions }]) => {
+          return (
+            <Permissions
+              key={module}
+              textDescription={description}
+              textTitle={capitalizeFirstLetter(module)}
+              slotToggleWithText={permissions?.map((permission) => {
+                if (!permission) return null;
+                return (
+                  <ToggleWithText
+                    key={permission.id}
+                    textToggleLight={permission.title}
+                    slotToggle={
+                      <AntSwitch
+                        checked={permission.isActive}
+                        onClick={() => {
+                          const data = {
+                            add: null,
+                            delete: null,
+                            role_id: role.id,
+                          };
 
-                        if (permission.isActive) {
-                          data.delete = permission.relation_id;
-                        } else {
-                          data.add = permission.id;
-                        }
-                        updateRoles(data);
-                      }}
-                    />
-                  }
-                />
-              );
-            })}
-          />
-        ),
+                          if (permission.isActive) {
+                            data.delete = permission.relation_id;
+                          } else {
+                            data.add = permission.id;
+                          }
+                          updateRoles(data);
+                        }}
+                      />
+                    }
+                  />
+                );
+              })}
+            />
+          );
+        },
       )}
       slotUserWithRole={role.assignedTo.map((user_id) => {
         const user = members.find((member) => member.user_id === user_id);
@@ -569,3 +436,151 @@ const updateRole = (data: SetRoleAndPermissionAPI['request']) => {
     data,
   );
 };
+
+const rolesOrder = {
+  admin: 0,
+  recruiter: 1,
+  'recruiting coordinator': 2,
+  'hiring manager': 2,
+  sourcer: 3,
+  interview: 4,
+};
+
+const app_modules: {
+  name: string;
+  description: string;
+  permissions: string[];
+}[] = [
+  {
+    name: 'enable disable Apps',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
+    permissions: [
+      'jobs_enabled',
+      'assessment_enabled',
+      'phone_screening_enabled',
+      'integrations_enabled',
+      'sourcing_enabled',
+      'workflow_enabled',
+      'company_setting_enabled',
+      'team_enabled',
+      'scheduler_enabled',
+      'tasks_enabled',
+      'settings_scheduler_enable',
+    ],
+  },
+  {
+    name: 'candidate permissions',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
+    permissions: [
+      'candidates_add',
+      'candidates_read',
+      'candidates_update',
+      'candidates_delete',
+      'candidates_moveStage',
+    ],
+  },
+  {
+    name: 'job permissions',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Jobs module:',
+    permissions: [
+      'jobs_read',
+      'jobs_create',
+      'jobs_update',
+      'jobs_publish',
+      'jobs_unpublish',
+      'jobs_archive',
+      'jobs_restore',
+      'jobs_delete',
+      'jobs_assignHiringManager',
+      'jobs_assignRecruiter',
+      'jobs_assignCoordinator',
+      'jobs_assignSourcer',
+    ],
+  },
+  {
+    name: 'profile score permissions',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
+    permissions: ['profileScore_view', 'profileScore_update'],
+  },
+  {
+    name: 'interview permissions',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
+    permissions: ['interviews_read', 'interviews_update', 'interviews_delete'],
+  },
+  {
+    name: 'report permissions',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
+    permissions: ['reports_view', 'reports_generate', 'reports_export'],
+  },
+  {
+    name: 'settings permissions',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
+    permissions: ['settings_view', 'settings_update'],
+  },
+  {
+    name: 'task permissions',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
+    permissions: ['tasks_create', 'tasks_read', 'tasks_update', 'tasks_delete'],
+  },
+  {
+    name: 'Workflow permissions',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
+    permissions: [
+      'workflow_create',
+      'workflow_read',
+      'workflow_update',
+      'workflow_delete',
+    ],
+  },
+  {
+    name: 'scheduler permissions',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
+    permissions: [
+      'scheduler_read',
+      'scheduler_create',
+      'scheduler_update',
+      'scheduler_delete',
+      'scheduler_request_availability',
+      'scheduler_send_scheduling',
+      'scheduler_interview_types_create',
+      'scheduler_interview_types_read',
+      'scheduler_interview_types_update',
+      'scheduler_interviewer_edit',
+      'settings_scheduler_update',
+    ],
+  },
+  {
+    name: 'User Manage permissions',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
+    permissions: ['team_read', 'team_create', 'team_update', 'team_delete'],
+  },
+  {
+    name: 'Company permissions',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
+    permissions: ['settings_company_enable', 'settings_company_update'],
+  },
+  {
+    name: 'team permissions',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
+    permissions: ['settings_team_enable', 'settings_team_update'],
+  },
+  {
+    name: 'Roles permissions',
+    description:
+      'Here are the permissions enabled for the Recruiting Coordinator role to manage the Tasks module:',
+    permissions: ['settings_roles_enable', 'settings_roles_update'],
+  },
+];
