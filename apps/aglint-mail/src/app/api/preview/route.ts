29fc54084915dad419491c58f081602116235da5
ChandraAglint <chandra@aglinthq.com>
2024-07-10 16:25:04 +0530

    Fix/email preview with dummy data (#1147)
    * email
    
    * fix : variables
    
    * fix :  added values
    
    * final check
    
    * time ts updated
    
    * fix company name dynamic
    
    * UI fix in scheduler
    
    * job preview fixed
    
    ---------
    
    Co-authored-by: Dileep B C <dileep@aglinthq.com>

--- apps/aglint-mail/src/app/api/preview/route.ts
+++ apps/aglint-mail/src/app/api/preview/route.ts
@@ -2,31 +2,60 @@ import { NextResponse } from 'next/server';
 import { createElement } from 'react';
 import { render } from '@react-email/render';
 import { z } from 'zod';
-import {
-  ClientError,
-  MailArgValidationError,
-} from '../../../utils/apiUtils/customErrors';
+import { replaceAll, type allTempvariables } from '@aglint/shared-utils';
+import { ClientError } from '../../../utils/apiUtils/customErrors';
 import { getEmails } from '../../../utils/apiUtils/get-emails';
+import { fetchUtil } from './fetch-utils';
 
 interface ReqPayload {
   mail_type: string;
-  body: string;
+  recruiter_id?: string;
+  job_id?: string;
 }
 
 const ReqPayload = z.object({
   mailType: z.string(),
-  body: z.string(),
+  recruiter_id: z.optional(z.string(), undefined),
+  job_id: z.optional(z.string(), undefined),
 });
 
+const all_possible_dynamic_values: {
+  [K in (typeof allTempvariables)[number]]: string;
+} = {
+  candidateFirstName: 'Emily',
+  candidateLastName: 'Johnson',
+  candidateName: 'Emily Johnson',
+  companyName: 'Aglint',
+  jobRole: 'HR Coordinator',
+  organizerName: 'Jane Smith',
+  organizerFirstName: 'Jane',
+  organizerLastName: 'Smith',
+  OrganizerTimeZone: 'PST',
+  interviewerName: 'Michael Johnson',
+  interviewerFirstName: 'Michael',
+  interviewerLastName: 'Johnson',
+  startDate: 'Fri, May 12, 2024',
+  endDate: 'May 13, 2024',
+  time: '10:30 AM - 11:00 PM',
+};
+
 export async function POST(req: Request) {
-  const { mail_type, body }: ReqPayload = await req.json();
+  const { mail_type, recruiter_id, job_id }: ReqPayload = await req.json();
 
   try {
     if (!mail_type) {
       throw new ClientError('attribute application_id missing', 400);
     }
+    const { body, companyName } = await fetchUtil({
+      recruiter_id,
+      mail_type,
+      job_id,
+    });
+
     const { emails } = await getEmails();
-    const emailBody = body.replace(/\{\{/g, '').replace(/\}\}/g, '');
+    const filledBody = replacePlaceholders(body, companyName);
+
+    const emailBody = filledBody.replace(/\{\{/g, '').replace(/\}\}/g, '');
 
     const emailIdx = emails.findIndex((e) => e === mail_type);
 
@@ -36,36 +65,14 @@ export async function POST(req: Request) {
     });
   } catch (e: any) {
     console.error(e);
-    if (e instanceof ClientError) {
-      return NextResponse.json(
-        {
-          error: `${e.name} : ${e.message}`,
-        },
-        {
-          status: e.status,
-        },
-      );
-    }
-    if (e instanceof MailArgValidationError) {
-      return NextResponse.json(
-        {
-          error: `${e.name}: mail_type:application received,  ${e.message}`,
-        },
-        {
-          status: 400,
-        },
-      );
-    }
-    if (e) {
-      return NextResponse.json(
-        {
-          error: `${e.name}: mail_type:application received,  ${e.message}`,
-        },
-        {
-          status: 500,
-        },
-      );
-    }
+    return NextResponse.json(
+      {
+        error: `${e.name} : ${e.message}`,
+      },
+      {
+        status: e.status,
+      },
+    );
   }
 }
 
@@ -76,8 +83,27 @@ export const renderEmailTemplates = async (
   const { default: Template, dummy } = await import(
     `../../../emails/${filename}`
   );
+
+  dummy.body = emailBody;
   dummy.emailBody = emailBody;
   const element = createElement(Template, dummy);
   const html = render(element);
   return { html };
+};
+
+const replacePlaceholders = (template_body: string, compName: string) => {
+  let updated_temp_body = template_body;
+  const values = {
+    ...all_possible_dynamic_values,
+    companyName: compName || all_possible_dynamic_values.companyName,
+  };
+
+  for (const key of Object.keys(values)) {
+    updated_temp_body = replaceAll(
+      updated_temp_body,
+      `{{${key}}}`,
+      values[String(key)],
+    );
+  }
+  return updated_temp_body;
 };
