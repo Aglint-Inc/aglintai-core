5625cb8678b09bdf8533e3f529f41b2efc6dc28d
viveks14 <129153058+viveks14@users.noreply.github.com>
2024-07-10 21:46:14 +0530

    Fix/role 1235 (#1175)
    * sync: devlink
    
    * fix: issues
    
     - disable admin role edit
     - quick switch
    
    * sync: devlink
    
    * fix: prop type
    
    * feat: roles settings search user and change role

---
+++ apps/aglint-recruiter/src/components/CompanyDetailComp/CompanyInfoComp/RolesAndPermissions/RoleDetails/RoleUserWidget.tsx
@@ -0,0 +1,198 @@
+import { RecruiterUserType } from '@aglint/shared-types';
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemButton,
+  Popover,
+  Stack,
+  Typography,
+} from '@mui/material';
+import React, { useState } from 'react';
+
+import { GlobalBadge } from '@/devlink/GlobalBadge';
+import { GlobalEmptyState } from '@/devlink/GlobalEmptyState';
+import { IconButtonGhost } from '@/devlink/IconButtonGhost';
+import { UserWithRole } from '@/devlink/UserWithRole';
+import SearchField from '@/src/components/Common/SearchField/SearchField';
+import { GetRoleAndPermissionsAPI } from '@/src/pages/api/getRoleAndPermissions/type';
+import { capitalizeFirstLetter } from '@/src/utils/text/textUtils';
+
+export const RoleUserWidget = ({
+  role,
+  members,
+  setEditUser,
+}: {
+  role: Awaited<
+    GetRoleAndPermissionsAPI['response']
+  >['rolesAndPermissions'][string] & {
+    name: string;
+  };
+  members: RecruiterUserType[];
+  // eslint-disable-next-line no-unused-vars
+  setEditUser: (x: RecruiterUserType) => void;
+}) => {
+  return (
+    <>
+      <UserSearch members={members} setEditUser={setEditUser} />
+      {role.assignedTo.length ? (
+        role.assignedTo.map((user_id) => {
+          const user = members.find((member) => member.user_id === user_id);
+          if (!user) return;
+          return (
+            <UserWithRole
+              key={user_id}
+              textName={`${user.first_name || ''} ${user.last_name || ''}`.trim()}
+              textRole={user.position}
+              slotButton={
+                <IconButtonGhost
+                  iconName={'edit'}
+                  color={'neutral'}
+                  onClickButton={{ onClick: () => setEditUser(user) }}
+                />
+              }
+              slotBadge={
+                <GlobalBadge
+                  color={user.is_suspended ? 'error' : 'success'}
+                  textBadge={user.is_suspended ? 'Suspended' : 'Active'}
+                />
+              }
+              slotAvatar={
+                <Avatar
+                  key={user_id}
+                  src={user.profile_image}
+                  variant='rounded'
+                  alt={user.first_name}
+                  sx={{ height: '100%', width: '100%' }}
+                />
+              }
+            />
+          );
+        })
+      ) : (
+        <GlobalEmptyState
+          styleEmpty={{
+            style: {
+              backgroundColor: 'var(--neutral-3)',
+            },
+          }}
+          iconName={'group'}
+          textDesc={'No Users Assigned'}
+        />
+      )}
+    </>
+  );
+};
+
+const UserSearch = ({
+  members,
+  setEditUser,
+}: {
+  members: RecruiterUserType[]; // eslint-disable-next-line no-unused-vars
+  setEditUser: (x: RecruiterUserType) => void;
+}) => {
+  const [txt, setTxt] = useState('');
+  const [anchorEl, setAnchorEl] = useState<
+    HTMLInputElement | HTMLTextAreaElement
+  >(null);
+  const open = Boolean(anchorEl);
+  const id = open ? 'role-setting-search' : undefined;
+  function handleClose() {
+    setAnchorEl(null);
+  }
+  const filteredMembers = txt?.trim().length
+    ? members.filter(
+        (member) =>
+          `${member.first_name || ''} ${member.last_name || ''}`
+            .toLowerCase()
+            .includes(txt?.toLowerCase()) ||
+          member.role.toLowerCase().includes(txt?.toLowerCase()),
+      )
+    : null;
+
+  return (
+    <>
+      <SearchField
+        key={'search-role'}
+        value={txt}
+        isFullWidth
+        placeholder='Search users to Add'
+        onClear={() => {
+          setTxt(null);
+        }}
+        onChange={({ target, currentTarget }) => {
+          setAnchorEl(currentTarget);
+          setTxt(target.value);
+        }}
+      />
+      {Boolean(filteredMembers) && (
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          disableAutoFocus
+          disableEnforceFocus
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          transformOrigin={{ vertical: -10, horizontal: 0 }}
+          sx={{
+            '& .MuiPopover-paper': {
+              borderRadius: 'var(--radius-2)',
+              borderColor: 'var(--neutral-6)',
+              width: (anchorEl?.clientWidth || 200) + 20 + 'px',
+            },
+          }}
+        >
+          <List>
+            {filteredMembers.length ? (
+              filteredMembers.map((item) => {
+                return (
+                  <ListItemButton
+                    key={item.role}
+                    onClick={() => {
+                      setEditUser(item);
+                      handleClose();
+                    }}
+                  >
+                    <Stack direction={'row'} alignItems={'center'} spacing={2}>
+                      <Avatar variant='rounded' src={item.profile_image} />
+                      <Stack>
+                        <Typography
+                          sx={{
+                            fontSize: '14px',
+                            fontWeight: 600,
+                          }}
+                        >
+                          {capitalizeFirstLetter(
+                            `${item.first_name || ''} ${item.last_name || ''}`,
+                          )}
+                        </Typography>
+                        <Typography>
+                          {capitalizeFirstLetter(item.role)}
+                        </Typography>
+                      </Stack>
+                    </Stack>
+                  </ListItemButton>
+                );
+              })
+            ) : (
+              <ListItem key={'1o1'}>
+                <Typography
+                  sx={{
+                    fontSize: '14px',
+                    fontWeight: 600,
+                  }}
+                >
+                  No Match Found
+                </Typography>
+              </ListItem>
+            )}
+          </List>
+        </Popover>
+      )}
+    </>
+  );
+};
