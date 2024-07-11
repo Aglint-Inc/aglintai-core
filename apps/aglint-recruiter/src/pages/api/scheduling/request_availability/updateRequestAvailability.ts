574dfeebfcde240876fe48980870a62cc4e763b6
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-07-11 15:22:22 +0530

    fix: save candidate timezone (#1184)

--- apps/aglint-recruiter/src/pages/api/scheduling/request_availability/updateRequestAvailability.ts
+++ apps/aglint-recruiter/src/pages/api/scheduling/request_availability/updateRequestAvailability.ts
@@ -4,6 +4,8 @@ import { createClient } from '@supabase/supabase-js';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { fetchAllActivities } from '@/src/components/Scheduling/CandidateDetails/hooks';
+import { userTzDayjs } from '@/src/services/CandidateScheduleV2/utils/userTzDayjs';
+import { supabaseAdmin } from '@/src/utils/supabase/supabaseAdmin';
 
 const supabase = createClient<DB>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -37,6 +39,11 @@ export default async function handler(
             '*,request_session_relation( interview_session(*) ), applications ( candidate_id, candidates ( * ), public_jobs ( logo,company ) )',
           )
           .single();
+        await supabaseAdmin
+          .from('candidates')
+          .update({ timezone: userTzDayjs.tz.guess() })
+          .eq('id', candidateAvailability?.applications?.candidates?.id)
+          .throwOnError();
         if (!error) {
           return res.send(candidateAvailability);
         }
