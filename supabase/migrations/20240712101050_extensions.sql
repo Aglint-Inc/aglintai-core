8cd7e51b7d95e60814bc071cb917a45324f03d47
Chinmai <chinmai@aglinthq.com>
2024-07-12 19:11:59 +0530

    mig fix

--- supabase/migrations/20240712101050_extensions.sql
+++ supabase/migrations/20240712101050_extensions.sql
@@ -4,5 +4,7 @@ CREATE EXTENSION IF NOT EXISTS "vector" WITH SCHEMA "extensions";
 CREATE EXTENSION IF NOT EXISTS "wrappers" WITH SCHEMA "extensions";
 CREATE EXTENSION IF NOT EXISTS "pg_cron" WITH SCHEMA "extensions";
 CREATE EXTENSION IF NOT EXISTS "citext" WITH SCHEMA "extensions";
-CREATE EXTENSION IF NOT EXISTS "postgis" WITH SCHEMA "extensions";
+SET pgaudit.log = 'none';
+create extension if not exists "postgis" with schema "extensions";
+SET pgaudit.log = 'ddl';
 
