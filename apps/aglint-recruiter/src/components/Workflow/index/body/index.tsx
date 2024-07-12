954d85d50c1a4438d0b99e96d09227c627997c70
chinmaicraglint <125248276+chinmaicraglint@users.noreply.github.com>
2024-07-12 23:17:00 +0530

    dev-to-staging (#1224)
    * mig fix
    
    * delete confirmation
    
    * candidate scoring badge (#1221)
    
    ---------
    
    Co-authored-by: Punith G <punithg@aglinthq.com>
    Co-authored-by: Aglint Inc <127595354+aglintai@users.noreply.github.com>
    Co-authored-by: Punith G <91651608+MlNl-PEKKA@users.noreply.github.com>

--- apps/aglint-recruiter/src/components/Workflow/index/body/index.tsx
+++ apps/aglint-recruiter/src/components/Workflow/index/body/index.tsx
@@ -1,10 +1,5 @@
-import { Dialog } from '@mui/material';
-
-import { DeletePopup as DeletePopupDev } from '@/devlink3/DeletePopup';
 import { WorkflowLanding } from '@/devlink3/WorkflowLanding';
 import Seo from '@/src/components/Common/Seo';
-import { useWorkflows } from '@/src/context/Workflows';
-import { useWorkflowStore } from '@/src/context/Workflows/store';
 
 import Content from './content';
 import Filters from './filters';
@@ -17,40 +12,8 @@ const Body = () => {
         slotSearchAndFilter={<Filters />}
         slotWorkflowCard={<Content />}
       />
-      <DeletePopup />
     </>
   );
 };
 
 export default Body;
-
-const DeletePopup = () => {
-  const { handleDeleteWorkflow } = useWorkflows();
-  const { deletion, closeDeletion } = useWorkflowStore(
-    ({ deletion, closeDeletion }) => ({
-      deletion,
-      closeDeletion,
-    }),
-  );
-  const count = deletion?.workflow?.jobs?.length ?? 0;
-  return (
-    <Dialog open={deletion.open}>
-      <DeletePopupDev
-        buttonText={count === 0 ? 'Delete' : 'Unlink and Delete'}
-        textDescription={
-          count === 0
-            ? ' Are you sure you want to delete this workflow?'
-            : `This workflow is being used in ${count} job${count === 1 ? '' : 's'}. Are you sure you want to unlink and Delete this workflow?`
-        }
-        onClickCancel={{ onClick: () => closeDeletion() }}
-        onClickDelete={{
-          onClick: () => {
-            handleDeleteWorkflow({ id: deletion.workflow?.id });
-            closeDeletion();
-          },
-        }}
-        textTitle={'Delete workflow'}
-      />
-    </Dialog>
-  );
-};
