a6e5cfe7c4c4d535a27f3fca9b5fff14c7c303a7
Chinmai <chinmai@aglinthq.com>
2024-07-13 16:14:36 +0530

    migration was failing got fixed

--- supabase/migrations/20240712104103_trigger_disqua_to_new.sql
+++ supabase/migrations/20240712104103_trigger_disqua_to_new.sql
@@ -1,6 +1,4 @@
 -- Create the trigger
-DROP TRIGGER IF EXISTS trigger_delete_interview_schedule ON public.applications;
-
 CREATE TRIGGER trigger_delete_interview_schedule
 AFTER UPDATE OF status ON public.applications
 FOR EACH ROW
@@ -8,7 +6,6 @@ WHEN (OLD.status = 'disqualified' AND NEW.status = 'new')
 EXECUTE FUNCTION delete_interview_schedule_on_status_update();
 
 
-DROP FUNCTION IF EXISTS delete_interview_schedule_on_status_update();
 -- Create the trigger function
 CREATE OR REPLACE FUNCTION delete_interview_schedule_on_status_update()
 RETURNS TRIGGER AS $$
@@ -27,4 +24,3 @@ DROP CONSTRAINT IF EXISTS unique_user_module;
 
 ALTER TABLE public.interview_module_relation
 ADD CONSTRAINT unique_user_module UNIQUE (user_id, module_id);
-
