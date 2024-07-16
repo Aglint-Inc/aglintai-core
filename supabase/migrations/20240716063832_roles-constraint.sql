5a83e1825847f5adcdbd42377ad9b46d77937f73
Punith G <91651608+MlNl-PEKKA@users.noreply.github.com>
2024-07-16 12:11:48 +0530

    roles unique constraint (#1338)

---
+++ supabase/migrations/20240716063832_roles-constraint.sql
@@ -0,0 +1,5 @@
+CREATE UNIQUE INDEX roles_recruiter_id_name_unique ON public.roles USING btree (recruiter_id, name);
+
+alter table "public"."roles" add constraint "roles_recruiter_id_name_unique" UNIQUE using index "roles_recruiter_id_name_unique";
+
+
