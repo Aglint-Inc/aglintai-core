1e70711e93c9254cf5c7dcc7c0ccb2825e22bb6b
Punith G <punithg@aglinthq.com>
2024-07-17 12:35:46 +0530

    tour create memoization

--- apps/aglint-recruiter/src/context/TourContext/hooks.tsx
+++ apps/aglint-recruiter/src/context/TourContext/hooks.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
+import { useCallback } from 'react';
 
 import { tourQuery, useCreateTourLog } from '@/src/queries/tour';
 
@@ -7,7 +8,8 @@ import { useAuthDetails } from '../AuthContext/AuthContext';
 export const useTourContext = () => {
   const { recruiter_id } = useAuthDetails();
   const tour = useQuery(tourQuery.tours(recruiter_id));
-  const { mutate: handleCreateTourLog } = useCreateTourLog();
+  const { mutate } = useCreateTourLog();
+  const handleCreateTourLog = useCallback(mutate, []);
   return {
     tour,
     handleCreateTourLog,
