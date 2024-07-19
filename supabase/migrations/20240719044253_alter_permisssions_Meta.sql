02bd6cc71de1bd7d626543728ab55f1b5b685ae0
VivekS <vivek@aglinthq.com>
2024-07-19 15:04:28 +0530

    fix: migration

--- supabase/migrations/20240719044253_alter_permisssions_Meta.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."permissions" drop column "dependency_tree";
-
-alter table "public"."permissions" drop column "updated_at";
-
-alter table "public"."permissions" add column "meta" jsonb default '{"module": true, "description": "", "dependency_tree": {"child": [], "parent": null, "sibling": null}}'::jsonb;
