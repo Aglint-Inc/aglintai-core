a3e6c3d701e3c2a012873b25b56370cea4cd6df2
Dileep B C <dileep@aglinthq.com>
2024-07-22 21:02:01 +0530

    fix : lint

--- apps/aglint-recruiter/src/components/Jobs/Job/Workflow/list.tsx
+++ apps/aglint-recruiter/src/components/Jobs/Job/Workflow/list.tsx
@@ -13,6 +13,7 @@ import { WorkflowCard } from '@/devlink3/WorkflowCard';
 import { WorkflowEmpty } from '@/devlink3/WorkflowEmpty';
 import Loader from '@/src/components/Common/Loader';
 import OptimisticWrapper from '@/src/components/NewAssessment/Common/wrapper/loadingWapper';
+import { getTriggerOption } from '@/src/components/Workflow/[id]/body/constants';
 import { JobIcon } from '@/src/components/Workflow/index/body/icons';
 import { useAuthDetails } from '@/src/context/AuthContext/AuthContext';
 import { useJobDashboard } from '@/src/context/JobDashboard';
@@ -31,7 +32,6 @@ import { useWorkflowQuery } from '@/src/queries/workflow';
 import { Workflow } from '@/src/types/workflow.types';
 import ROUTES from '@/src/utils/routing/routes';
 import toast from '@/src/utils/toast';
-import { getTriggerOption } from '@/src/components/Workflow/[id]/body/constants';
 
 const JobWorkflowComp = () => {
   return (
