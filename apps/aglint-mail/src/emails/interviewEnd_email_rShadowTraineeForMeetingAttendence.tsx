dd5c970b17a4222f22e0e2f326bb8ae3a1ff4f0a
ChandraAglint <chandra@aglinthq.com>
2024-07-22 21:09:03 +0530

    fix / email endpoint updates (#1581)
    endpoint updates
    
    Co-authored-by: Dileep B C <dileep@aglinthq.com>

--- apps/aglint-mail/src/emails/interviewEnd_email_rShadowTraineeForMeetingAttendence.tsx
+++ apps/aglint-mail/src/emails/interviewEnd_email_rShadowTraineeForMeetingAttendence.tsx
@@ -3,7 +3,8 @@ import type { EmailTemplateAPi } from '@aglint/shared-types';
 import { companyLogoDummy } from '../utils/assets/common';
 import { EmailContainer } from '../components/template/Container';
 
-type EmailType = EmailTemplateAPi<'onRShadowComplete_email_trainee'>;
+type EmailType =
+  EmailTemplateAPi<'interviewEnd_email_rShadowTraineeForMeetingAttendence'>;
 
 export const dummy: EmailType['react_email_placeholders'] = {
   emailBody: `<p>Hi <span class="temp-variable" data-type="temp-variable" data-id="traineeName">{{traineeName}}</span> ,</p><p></p><p>Could you please confirm if you've completed the <span class="temp-variable" data-type="temp-variable" data-id="reverseShadowCount">{{reverseShadowCount}}</span> reverse shadow session for <span class="temp-variable" data-type="temp-variable" data-id="interviewType">{{interviewType}}</span> ? You were scheduled as a shadow interviewer in the <span class="temp-variable" data-type="temp-variable" data-id="sessionName">{{sessionName}}</span> for <span class="temp-variable" data-type="temp-variable" data-id="jobRole">{{jobRole}}</span> with <span class="temp-variable" data-type="temp-variable" data-id="candidateName">{{candidateName}}</span> .</p><p></p><p>Please click the link below to confirm: <span class="temp-variable" data-type="temp-variable" data-id="reverseShadowConfirmLink">{{reverseShadowConfirmLink}}</span></p><p>From,</p><p><span class="temp-variable" data-type="temp-variable" data-id="organizerName">{{organizerName}}</span></p>`,
