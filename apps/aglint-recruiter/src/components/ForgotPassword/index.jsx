fdc74dd6cf51bf1b8a128a61e178a6706e21f915
ChandraAglint <chandra@aglinthq.com>
2024-07-22 16:28:17 +0530

    reset password validation

--- apps/aglint-recruiter/src/components/ForgotPassword/index.jsx
+++ apps/aglint-recruiter/src/components/ForgotPassword/index.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
-import { Box, Container, Stack, TextField } from '@mui/material';
+import { Box, Container, Stack } from '@mui/material';
 import { createClient } from '@supabase/supabase-js';
-import { errorMessages } from '@utils/errorMessages';
 import posthog from 'posthog-js';
 import { useRef, useState } from 'react';
 
@@ -11,11 +10,13 @@ let tempEmail = '';
 
 import { useRouter } from 'next/router';
 
+import { ButtonGhost } from '@/devlink/ButtonGhost';
+import { ButtonSolid } from '@/devlink/ButtonSolid';
 import { YTransform } from '@/src/utils/framer-motions/Animation';
 import ROUTES from '@/src/utils/routing/routes';
-import toast from '@/src/utils/toast';
 
 import Footer from '../Common/Footer';
+import UITextField from '../Common/UITextField';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
@@ -34,24 +35,25 @@ export default function ForgotPasswordComponent() {
   let email = useRef(null);
   const handleEmail = () => {
     const emailRef = email.current.value;
-    const emailRegex =
-      /^([a-zA-Z0-9]+)([_.+\-{1}])?([a-zA-Z0-9]+)@([a-zA-Z0-9]+)([.])([a-zA-Z]+)$/g;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/g;
     if (emailRef === '') {
       setError({
         ...error,
         valid: {
           error: true,
-          msg: errorMessages.emailRequired,
+          msg: 'Email cannot be empty',
         },
       });
+      return false;
     } else if (!emailRegex.test(emailRef)) {
       setError({
         ...error,
         valid: {
           error: true,
-          msg: errorMessages.emailRequired,
+          msg: 'Enter a valid email address',
         },
       });
+      return false;
     } else {
       setError({
         ...error,
@@ -60,9 +62,10 @@ export default function ForgotPasswordComponent() {
         },
       });
     }
+    return true;
   };
   const handleSubmit = async (event) => {
-    if (!loading && email.current?.value.length !== 0) {
+    if (handleEmail() && !loading && email.current?.value.length !== 0) {
       event.preventDefault();
       setLoading(true);
       const currentEmail = email.current?.value
@@ -83,15 +86,13 @@ export default function ForgotPasswordComponent() {
       } else {
         setLoading(false);
       }
-    } else {
-      toast.error('Please enter email.');
     }
   };
 
   const router = useRouter();
   return (
     <Container
-      maxWidth="false"
+      maxWidth='false'
       sx={{
         height: '100vh',
         display: 'flex',
@@ -99,7 +100,8 @@ export default function ForgotPasswordComponent() {
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'var(--neutral-2)',
-      }}>
+      }}
+    >
       <Box
         sx={{
           flex: 1,
@@ -107,68 +109,79 @@ export default function ForgotPasswordComponent() {
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
-        }}>
-      {changetext == 'Send' && (
-        <YTransform uniqueKey={changetext}>
-          <PwResetForm
-            slotForm={
-              <Box component='form' onSubmit={handleSubmit}>
-                <Stack spacing={2}>
-                  <TextField
-                    inputRef={email}
-                    fullWidth
-                    onFocus={() =>
-                      setError({
-                        ...error,
-                        valid: {
-                          error: false,
-                        },
-                      })
-                    }
-                    onBlur={handleEmail}
-                    error={error.valid.error}
-                    helperText={error.valid.error ? error.valid.msg : ''}
-                    placeholder='Enter email'
-                    id='Email'
-                    required
+        }}
+      >
+        {changetext == 'Send' && (
+          <YTransform uniqueKey={changetext}>
+            <PwResetForm
+              slotButton={
+                <Stack spacing={'var(--space-2)'}>
+                  <ButtonSolid
+                    textButton='Reset'
+                    size={2}
+                    onClickButton={{ onClick: handleSubmit }}
+                  />
+                  <ButtonGhost
+                    textButton='Back to login'
+                    size={2}
+                    color={'neutral'}
+                    onClickButton={{
+                      onClick: () => {
+                        router.push(ROUTES['/login']());
+                      },
+                    }}
                   />
                 </Stack>
-              </Box>
-            }
-            onclickBack={{
-              onClick: () => {
-                router.push(ROUTES['/login']());
-              },
-            }}
-            onclickReset={{
-              onClick: handleSubmit,
-            }}
-            contactLink={{
-              href: 'mailto:admin@aglinthq.com',
-            }}
-          />
-        </YTransform>
-      )}
-      {changetext === 'Sent' && (
-        <YTransform uniqueKey={changetext}>
-          <PwResetConfirm
-            onclickBack={{
-              onClick: () => {
-                router.push(ROUTES['/login']());
-              },
-            }}
-            contactLink={{
-              href: 'mailto:admin@aglinthq.com',
-            }}
-            onclickReset={{
-              onClick: (e) => {
-                handleSubmit(e);
-              },
-            }}
-          />
-        </YTransform>
-      )}
-    </Box>
+              }
+              slotForm={
+                <Box component='form' onSubmit={handleSubmit}>
+                  <Stack spacing={2}>
+                    <UITextField
+                      ref={email}
+                      fullWidth
+                      onFocus={() =>
+                        setError({
+                          ...error,
+                          valid: {
+                            error: false,
+                          },
+                        })
+                      }
+                      error={error.valid.error}
+                      helperText={error.valid.error ? error.valid.msg : ''}
+                      placeholder='Enter a email'
+                      id='Email'
+                      required
+                    />
+                  </Stack>
+                </Box>
+              }
+              contactLink={{
+                href: 'mailto:admin@aglinthq.com',
+              }}
+            />
+          </YTransform>
+        )}
+        {changetext === 'Sent' && (
+          <YTransform uniqueKey={changetext}>
+            <PwResetConfirm
+              onclickBack={{
+                onClick: () => {
+                  router.push(ROUTES['/login']());
+                },
+              }}
+              contactLink={{
+                href: 'mailto:admin@aglinthq.com',
+              }}
+              onclickReset={{
+                onClick: (e) => {
+                  handleSubmit(e);
+                },
+              }}
+            />
+          </YTransform>
+        )}
+      </Box>
       <Box
         sx={{
           width: '100%',
@@ -177,6 +190,6 @@ export default function ForgotPasswordComponent() {
       >
         <Footer />
       </Box>
-      </Container>
+    </Container>
   );
 }
