ab5a05a30249605c06fa778186079aae9568f1f4
Ravi K <ravi@aglinthq.com>
2024-07-22 18:56:35 +0530

    Devlink Sync

---
+++ apps/aglint-recruiter/devlink3/KeywordsHelper.d.ts
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as Types from "./types";
+
+declare function KeywordsHelper(props: {
+  as?: React.ElementType;
+  onClickArrow?: Types.Devlink.RuntimeProps;
+  styleWidth?: Types.Devlink.RuntimeProps;
+}): React.JSX.Element;
