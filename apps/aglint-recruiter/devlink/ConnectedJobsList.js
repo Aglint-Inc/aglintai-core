12369d535c7ff1a11e2e83e43a3d396666d4a652
Punith G <punithg@aglinthq.com>
2024-07-23 19:43:57 +0530

    fix: Minor UI Issues on workflow

--- apps/aglint-recruiter/devlink/ConnectedJobsList.js
+++ apps/aglint-recruiter/devlink/ConnectedJobsList.js
@@ -8,10 +8,11 @@ import _styles from "./ConnectedJobsList.module.css";
 export function ConnectedJobsList({
   as: _Component = _Builtin.Block,
   slotTextWithIcon,
+  textTitle = "Connected Jobs",
 }) {
   return (
     <_Component className={_utils.cx(_styles, "connected_jobs")} tag="div">
-      <Text content="Connected Jobs" color="neutral" />
+      <Text content={textTitle} color="neutral" />
       <_Builtin.Block tag="div">{slotTextWithIcon}</_Builtin.Block>
     </_Component>
   );
