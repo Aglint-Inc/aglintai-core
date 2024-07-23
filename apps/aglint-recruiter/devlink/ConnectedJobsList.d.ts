4bd6c63b45c3fef157f5b6154beeeedf276a0f51
Punith G <91651608+MlNl-PEKKA@users.noreply.github.com>
2024-07-23 19:44:58 +0530

    fix: Minor UI Issues on workflow (#1622)

--- apps/aglint-recruiter/devlink/ConnectedJobsList.d.ts
+++ apps/aglint-recruiter/devlink/ConnectedJobsList.d.ts
@@ -4,4 +4,5 @@ import * as Types from "./types";
 declare function ConnectedJobsList(props: {
   as?: React.ElementType;
   slotTextWithIcon?: Types.Devlink.Slot;
+  textTitle?: React.ReactNode;
 }): React.JSX.Element;
