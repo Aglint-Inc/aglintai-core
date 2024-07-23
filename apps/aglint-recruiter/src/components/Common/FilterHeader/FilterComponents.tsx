715d84eed9899b88bb4b19514cec22979debb696
ChandraAglint <chandra@aglinthq.com>
2024-07-23 19:41:04 +0530

    sche - schl :  dyn icon update

--- apps/aglint-recruiter/src/components/Common/FilterHeader/FilterComponents.tsx
+++ apps/aglint-recruiter/src/components/Common/FilterHeader/FilterComponents.tsx
@@ -247,19 +247,9 @@ export function FilterComponent({
         textLabel={title}
         slotRightIcon={
           <Stack>
-            <GlobalIcon iconName='keyboard_arrow_down' />
-            {/* <svg
-              width='15'
-              height='16'
-              viewBox='0 0 15 16'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path
-                d='M7.75781 11.2578C7.58594 11.4141 7.41406 11.4141 7.24219 11.2578L2.74219 6.75781C2.58594 6.58594 2.58594 6.41406 2.74219 6.24219C2.91406 6.08594 3.08594 6.08594 3.25781 6.24219L7.5 10.4609L11.7422 6.24219C11.9141 6.08594 12.0859 6.08594 12.2578 6.24219C12.4141 6.41406 12.4141 6.58594 12.2578 6.75781L7.75781 11.2578Z'
-                fill='#0F3554'
-              />
-            </svg> */}
+            <GlobalIcon
+              iconName={anchorEl ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}
+            />
           </Stack>
         }
       />
@@ -383,7 +373,9 @@ function MultiSectionFilterComponent({
         textLabel={title}
         slotRightIcon={
           <Stack>
-            <GlobalIcon iconName='keyboard_arrow_down' />
+            <GlobalIcon
+              iconName={anchorEl ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}
+            />
           </Stack>
         }
       />
@@ -539,19 +531,9 @@ function NestedFilterComponent({
         textLabel={title}
         slotRightIcon={
           <Stack>
-            <GlobalIcon iconName='keyboard_arrow_down' />
-            {/* <svg
-              width='15'
-              height='16'
-              viewBox='0 0 15 16'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path
-                d='M7.75781 11.2578C7.58594 11.4141 7.41406 11.4141 7.24219 11.2578L2.74219 6.75781C2.58594 6.58594 2.58594 6.41406 2.74219 6.24219C2.91406 6.08594 3.08594 6.08594 3.25781 6.24219L7.5 10.4609L11.7422 6.24219C11.9141 6.08594 12.0859 6.08594 12.2578 6.24219C12.4141 6.41406 12.4141 6.58594 12.2578 6.75781L7.75781 11.2578Z'
-                fill='#0F3554'
-              />
-            </svg> */}
+            <GlobalIcon
+              iconName={anchorEl ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}
+            />
           </Stack>
         }
       />
