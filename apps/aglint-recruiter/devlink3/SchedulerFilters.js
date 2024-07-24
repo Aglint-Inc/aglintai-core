60cfd971cb3e00825b1683865781208dbc3870ac
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-07-24 20:51:20 +0530

    fix: availability card for self schedule

--- apps/aglint-recruiter/devlink3/SchedulerFilters.js
+++ apps/aglint-recruiter/devlink3/SchedulerFilters.js
@@ -3,6 +3,7 @@ import React from "react";
 import * as _Builtin from "./_Builtin";
 import { Text } from "./Text";
 import { GlobalIcon } from "./GlobalIcon";
+import { SchedulerConflictCard } from "./SchedulerConflictCard";
 import { SlotComp } from "./SlotComp";
 import { TimeRangeSelector } from "./TimeRangeSelector";
 import * as _utils from "./utils";
@@ -10,27 +11,12 @@ import _styles from "./SchedulerFilters.module.css";
 
 export function SchedulerFilters({
   as: _Component = _Builtin.Block,
-  textNumberNoConflicts = "0",
-  textNumberSoftConflicts = "0",
-  textNumberHardConflicts = "0",
-  textNumberOutsideWorkHours = (
-    <>
-      {"0"}
-      <br />
-    </>
-  ),
   slotPreferedInterviewersSearch,
   slotTimeRangeSelector,
   slotCheckbox,
   textDateRange = "April 04 - April 05",
-  slotSoftConflictToggle,
   slotNoConflictToggle,
-  slotHardConflictToggle,
-  slotOutsideToggle,
-  isNoConflictActive = false,
-  isSoftConflictActive = false,
-  isHardConflictActive = false,
-  isOutsideActive = false,
+  slotSchedulerConflictCard,
 }) {
   return (
     <_Component
@@ -60,162 +46,9 @@ export function SchedulerFilters({
         className={_utils.cx(_styles, "conflict_block_wrapper")}
         tag="div"
       >
-        <_Builtin.Block
-          className={_utils.cx(_styles, "conflict_block")}
-          tag="div"
-        >
-          <_Builtin.Block
-            className={_utils.cx(_styles, "conflict_block_head")}
-            tag="div"
-          >
-            <_Builtin.Block
-              className={_utils.cx(_styles, "conflict-number-wrap")}
-              tag="div"
-            >
-              <Text content={textNumberNoConflicts} size="6" color="success" />
-              <Text content="No Conflicts" weight="medium" color="success" />
-            </_Builtin.Block>
-            <_Builtin.Block
-              className={_utils.cx(_styles, "nc-icomn-wrap")}
-              tag="div"
-            >
-              <GlobalIcon iconName="check_circle" />
-            </_Builtin.Block>
-          </_Builtin.Block>
-          <_Builtin.Block
-            className={_utils.cx(_styles, "relative-1")}
-            tag="div"
-          >
-            {slotNoConflictToggle}
-          </_Builtin.Block>
-          {isNoConflictActive ? (
-            <_Builtin.Block
-              className={_utils.cx(_styles, "conflict_block_active")}
-              tag="div"
-            />
-          ) : null}
-        </_Builtin.Block>
-        <_Builtin.Block
-          className={_utils.cx(_styles, "conflict_block")}
-          tag="div"
-        >
-          <_Builtin.Block
-            className={_utils.cx(_styles, "conflict_block_head")}
-            tag="div"
-          >
-            <_Builtin.Block
-              className={_utils.cx(_styles, "conflict-number-wrap")}
-              tag="div"
-            >
-              <Text
-                content={textNumberSoftConflicts}
-                size="6"
-                color="warning"
-              />
-              <Text content="Soft Conflicts" weight="medium" color="warning" />
-            </_Builtin.Block>
-            <_Builtin.Block
-              className={_utils.cx(_styles, "sc-icon-wrap")}
-              tag="div"
-            >
-              <GlobalIcon iconName="info" />
-            </_Builtin.Block>
-          </_Builtin.Block>
-          <_Builtin.Block
-            className={_utils.cx(_styles, "relative-1")}
-            tag="div"
-          >
-            {slotSoftConflictToggle}
-          </_Builtin.Block>
-          {isSoftConflictActive ? (
-            <_Builtin.Block
-              className={_utils.cx(_styles, "conflict_block_active")}
-              tag="div"
-            />
-          ) : null}
-        </_Builtin.Block>
-        <_Builtin.Block
-          className={_utils.cx(_styles, "conflict_block")}
-          id={_utils.cx(
-            _styles,
-            "w-node-_0e66376c-6f8f-5928-fc1e-939cce85cb18-04bedc83"
-          )}
-          tag="div"
-        >
-          <_Builtin.Block
-            className={_utils.cx(_styles, "conflict_block_head")}
-            tag="div"
-          >
-            <_Builtin.Block
-              className={_utils.cx(_styles, "conflict-number-wrap")}
-              tag="div"
-            >
-              <Text content={textNumberHardConflicts} size="6" color="error" />
-              <Text content="Hard Conflicts" weight="medium" color="error" />
-            </_Builtin.Block>
-            <_Builtin.Block
-              className={_utils.cx(_styles, "hc-icon-wrap")}
-              tag="div"
-            >
-              <GlobalIcon iconName="warning" />
-            </_Builtin.Block>
-          </_Builtin.Block>
-          <_Builtin.Block
-            className={_utils.cx(_styles, "relative-1")}
-            tag="div"
-          >
-            {slotHardConflictToggle}
-          </_Builtin.Block>
-          {isHardConflictActive ? (
-            <_Builtin.Block
-              className={_utils.cx(_styles, "conflict_block_active")}
-              tag="div"
-            />
-          ) : null}
-        </_Builtin.Block>
-        <_Builtin.Block
-          className={_utils.cx(_styles, "conflict_block")}
-          id={_utils.cx(
-            _styles,
-            "w-node-_12a6b914-407e-f355-78ff-89a41a97aba0-04bedc83"
-          )}
-          tag="div"
-        >
-          <_Builtin.Block
-            className={_utils.cx(_styles, "conflict_block_head")}
-            tag="div"
-          >
-            <_Builtin.Block
-              className={_utils.cx(_styles, "conflict-number-wrap")}
-              tag="div"
-            >
-              <Text
-                content={textNumberOutsideWorkHours}
-                size="6"
-                color="info"
-              />
-              <Text content="Outside Work Hours" weight="medium" color="info" />
-            </_Builtin.Block>
-            <_Builtin.Block
-              className={_utils.cx(_styles, "owh-icon-wrap")}
-              tag="div"
-            >
-              <GlobalIcon iconName="bedtime" />
-            </_Builtin.Block>
-          </_Builtin.Block>
-          <_Builtin.Block
-            className={_utils.cx(_styles, "relative-1")}
-            tag="div"
-          >
-            {slotOutsideToggle}
-          </_Builtin.Block>
-          {isOutsideActive ? (
-            <_Builtin.Block
-              className={_utils.cx(_styles, "conflict_block_active")}
-              tag="div"
-            />
-          ) : null}
-        </_Builtin.Block>
+        {slotSchedulerConflictCard ?? (
+          <SchedulerConflictCard slotNoConflictToggle={slotNoConflictToggle} />
+        )}
       </_Builtin.Block>
       <_Builtin.Block
         className={_utils.cx(_styles, "sub_session_with_heading")}
