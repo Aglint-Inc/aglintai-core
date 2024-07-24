c307d22698038b937139c62f0fe222796e4a28de
Chinmai <chinmai@aglinthq.com>
2024-07-24 13:53:33 +0530

    refactor edit drawer component

---
+++ apps/aglint-recruiter/src/components/Scheduling/CandidateDetails/FullSchedule/EditDrawer/ButtonGroup.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { ButtonSoft } from '@/devlink/ButtonSoft';
+import { ButtonSolid } from '@/devlink/ButtonSolid';
+
+import { useEditSession } from './hooks';
+import { useEditSessionDrawerStore } from './store';
+
+function ButtonGroup() {
+  const { saving, errorValidation } = useEditSessionDrawerStore((state) => ({
+    saving: state.saving,
+    errorValidation: state.errorValidation,
+  }));
+
+  const { handleClose, handleSave } = useEditSession();
+
+  return (
+    <>
+      <ButtonSoft
+        textButton='Cancel'
+        color={'neutral'}
+        size={2}
+        onClickButton={{ onClick: () => handleClose() }}
+      />
+      <ButtonSolid
+        isDisabled={errorValidation.some((err) => err.error)}
+        textButton='Save'
+        size={2}
+        isLoading={saving}
+        onClickButton={{
+          onClick: () => {
+            if (!saving) {
+              handleSave();
+            }
+          },
+        }}
+      />
+    </>
+  );
+}
+
+export default ButtonGroup;
---
+++ apps/aglint-recruiter/src/components/Scheduling/CandidateDetails/FullSchedule/EditDrawer/InterviewMode/SelectSessionType.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { InterviewModePill } from '@/devlink2/InterviewModePill';
+
+import IconSessionType from '../../../RightPanel/IconSessionType';
+import { setEditSession, useEditSessionDrawerStore } from '../store';
+
+function SelectSessionType() {
+  const { editSession } = useEditSessionDrawerStore((state) => ({
+    editSession: state.editSession,
+  }));
+
+  return (
+    <>
+      <InterviewModePill
+        isActive={editSession.interview_session.session_type === 'panel'}
+        textModeName={'Panel'}
+        slotModeIcon={<IconSessionType type='panel' />}
+        onClickPill={{
+          onClick: () => {
+            setEditSession({
+              interview_session: {
+                ...editSession.interview_session,
+                session_type: 'panel',
+              },
+            });
+          },
+        }}
+      />
+      <InterviewModePill
+        isActive={editSession.interview_session.session_type === 'individual'}
+        textModeName={'Individual'}
+        slotModeIcon={<IconSessionType type='individual' />}
+        onClickPill={{
+          onClick: () => {
+            setEditSession({
+              interview_session: {
+                ...editSession.interview_session,
+                session_type: 'individual',
+              },
+            });
+          },
+        }}
+      />
+    </>
+  );
+}
+
+export default SelectSessionType;
