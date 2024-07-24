0fd532c844f2b89d9c54d2ebf412523972f911fe
Punith G <91651608+MlNl-PEKKA@users.noreply.github.com>
2024-07-24 16:11:43 +0530

    schedule card warnings (#1652)

--- apps/aglint-recruiter/src/components/Scheduling/CandidateDetails/FullSchedule/ScheduleIndividual/Collapse/index.tsx
+++ apps/aglint-recruiter/src/components/Scheduling/CandidateDetails/FullSchedule/ScheduleIndividual/Collapse/index.tsx
@@ -1,6 +1,7 @@
 import { Collapse, Stack } from '@mui/material';
 
 import { ButtonSoft } from '@/devlink/ButtonSoft';
+import { GlobalBanner } from '@/devlink2/GlobalBanner';
 import { GlobalUserDetail } from '@/devlink3/GlobalUserDetail';
 import { Text } from '@/devlink3/Text';
 import { TextWithIcon } from '@/devlink3/TextWithIcon';
@@ -55,6 +56,8 @@ function CollapseContent({
 
   const cancelReasons = currentSession?.cancel_reasons;
 
+  const count = users?.length ?? 0;
+
   return (
     <Collapse in={collapsed}>
       {!!currentSession && (
@@ -94,70 +97,87 @@ function CollapseContent({
             )}
 
             <Stack spacing={'var(--space-2)'}>
-              <Text
-                content={
-                  interview_meeting?.status === 'confirmed' ||
-                  interview_meeting?.status === 'completed'
-                    ? 'Interviewer(s)'
-                    : interview_session.session_type === 'panel'
-                      ? `${numberToText(
-                          interview_session.interviewer_cnt,
-                        )} of the member below will be picked as interviewer`
-                      : interview_session.session_type === 'individual'
-                        ? `One of the member below will be picked as interviewer`
-                        : 'Interviewer(s)'
-                }
-                size={1}
-                color={'neutral'}
-                weight={'regular'}
-              />
-              {users.map((user) => {
-                const item = user.user_details;
-                const pause_json = user.interview_module_relation?.pause_json;
-                const isPaused = !!pause_json; //null check needed because debrief doesnt have module relation
-                const isCalendarConnected =
-                  (!!recruiter.service_json &&
-                    recruiter.email.split('@')[1] ===
-                      user.user_details.email.split('@')[1]) ||
-                  !!(user.user_details.schedule_auth as any)?.access_token;
+              {count !== 0 && (
+                <Text
+                  content={
+                    interview_meeting?.status === 'confirmed' ||
+                    interview_meeting?.status === 'completed'
+                      ? 'Interviewer(s)'
+                      : interview_session.session_type === 'panel'
+                        ? `${numberToText(
+                            interview_session.interviewer_cnt,
+                          )} of the member below will be picked as interviewer`
+                        : interview_session.session_type === 'individual'
+                          ? `One of the member below will be picked as interviewer`
+                          : 'Interviewer(s)'
+                  }
+                  size={1}
+                  color={'neutral'}
+                  weight={'regular'}
+                />
+              )}
 
-                const cancelReason = cancelReasons?.find(
-                  (can) =>
-                    can.interview_session_cancel.session_relation_id ===
-                    user.interview_session_relation.id,
-                )?.interview_session_cancel;
+              {count === 0 ? (
+                <GlobalBanner
+                  color={'error'}
+                  iconName={'warning'}
+                  textTitle={'No interviewers assigned to this stage'}
+                  textDescription={
+                    'Please add interviewers to proceed with scheduling this stage'
+                  }
+                  slotButtons={<></>}
+                />
+              ) : (
+                users.map((user) => {
+                  const item = user.user_details;
+                  const pause_json = user.interview_module_relation?.pause_json;
+                  const isPaused = !!pause_json; //null check needed because debrief doesnt have module relation
+                  const isCalendarConnected =
+                    (!!recruiter.service_json &&
+                      recruiter.email.split('@')[1] ===
+                        user.user_details.email.split('@')[1]) ||
+                    !!(user.user_details.schedule_auth as any)?.access_token;
 
-                return (
-                  <InterviewerUserDetail
-                    key={item.user_id}
-                    accepted_status={
-                      user.interview_session_relation.accepted_status
-                    }
-                    cancelReason={cancelReason}
-                    interview_meeting={{
-                      end_time: interview_meeting?.end_time,
-                      start_time: interview_meeting?.start_time,
-                      status: interview_meeting?.status,
-                    }}
-                    interviewerTimeZone={
-                      item.scheduling_settings?.timeZone.tzCode
-                    }
-                    isCalendarConnected={isCalendarConnected}
-                    isPaused={isPaused}
-                    pause_json={pause_json}
-                    userDetails={{
-                      first_name: item.first_name,
-                      last_name: item.last_name,
-                      position: item.position,
-                      profile_image: item.profile_image,
-                    }}
-                    trainingType={user.interview_session_relation.training_type}
-                    interviewerType={
-                      user.interview_session_relation.interviewer_type
-                    }
-                  />
-                );
-              })}
+                  const cancelReason = cancelReasons?.find(
+                    (can) =>
+                      can.interview_session_cancel.session_relation_id ===
+                      user.interview_session_relation.id,
+                  )?.interview_session_cancel;
+
+                  return (
+                    <InterviewerUserDetail
+                      key={item.user_id}
+                      accepted_status={
+                        user.interview_session_relation.accepted_status
+                      }
+                      cancelReason={cancelReason}
+                      interview_meeting={{
+                        end_time: interview_meeting?.end_time,
+                        start_time: interview_meeting?.start_time,
+                        status: interview_meeting?.status,
+                      }}
+                      interviewerTimeZone={
+                        item.scheduling_settings?.timeZone.tzCode
+                      }
+                      isCalendarConnected={isCalendarConnected}
+                      isPaused={isPaused}
+                      pause_json={pause_json}
+                      userDetails={{
+                        first_name: item.first_name,
+                        last_name: item.last_name,
+                        position: item.position,
+                        profile_image: item.profile_image,
+                      }}
+                      trainingType={
+                        user.interview_session_relation.training_type
+                      }
+                      interviewerType={
+                        user.interview_session_relation.interviewer_type
+                      }
+                    />
+                  );
+                })
+              )}
             </Stack>
           </>
           <>
