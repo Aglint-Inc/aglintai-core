8ad73e68bcf731d340e7cad62ca2ae0f65c27c7f
Dileep B C <135205237+dileepbc3141@users.noreply.github.com>
2024-07-24 21:33:19 +0530

    Workflow triggers (#1667)
    * wip
    
    * WIp
    
    * fix : workflow
    
    * fix : plan
    
    * fix : updated trigger function
    
    * fix : seed plan
    
    * feat : func_workflow_log_on_update_interview_module_relation
    
    * workflow_log_on_update_interview_training_progress
    
    * delete functions
    
    * fix : sql
    
    * fix : workflow func
    
    * fix : seed workflows
    
    * fix : shadows email
    
    * fix : workflow function
    
    * fix: payloads
    
    * fix : triggers
    
    * fix : lint
    
    * seed.sql

--- apps/aglint-mail/src/app/api/interviewEnd_email_rShadowTraineeForMeetingAttendence/fetch-util.ts
+++ apps/aglint-mail/src/app/api/interviewEnd_email_rShadowTraineeForMeetingAttendence/fetch-util.ts
@@ -7,81 +7,93 @@ import { numberToOrdinal } from '../../../utils/email/common/functions';
 export async function fetchUtil(
   req_body: EmailTemplateAPi<'interviewEnd_email_rShadowTraineeForMeetingAttendence'>['api_payload'],
 ) {
-  const [data] = supabaseWrap(
+  const training_ints = supabaseWrap(
     await supabaseAdmin
-      .from('interview_module_relation')
-      .select('user_id,recruiter_user(*),interview_module(*)')
-      .eq('id', req_body.interview_module_relation_id),
+      .from('meeting_interviewers')
+      .select()
+      .eq('training_type', 'reverse_shadow')
+      .eq('session_id', req_body.session_id),
+    false,
   );
-  const [interviewMeeting] = supabaseWrap(
-    await supabaseAdmin
-      .from('interview_meeting')
-      .select(
-        'recruiter_user(first_name,last_name,scheduling_settings),interview_schedule(applications(candidates(first_name,last_name),public_jobs(id,company,job_title,logo,recruiter_id)))',
-      )
-      .eq('id', req_body.interview_meeting_id),
-  );
-  const [session] = supabaseWrap(
+
+  if (training_ints.length === 0) {
+    return [];
+  }
+  const [meeting_detail] = supabaseWrap(
     await supabaseAdmin
       .from('interview_session')
-      .select('name')
+      .select(
+        '*,interview_module(*), interview_meeting(*, recruiter_user(*), interview_schedule(*, applications(*, public_jobs(*),candidates(*))))',
+      )
       .eq('id', req_body.session_id),
   );
 
-  const [shadow] = supabaseWrap(
+  const module_relations = supabaseWrap(
     await supabaseAdmin
       .from('module_relations_view')
-      .select('reverse_shadow_meeting_count')
-      .eq('user_id', data.user_id),
+      .select()
+      .in(
+        'user_id',
+        training_ints.map((int) => int.user_id),
+      ),
+    false,
   );
 
-  const reverseShadowCount = shadow.reverse_shadow_meeting_count;
-
-  const { interview_module, recruiter_user: trainee } = data;
-
-  const organizer = interviewMeeting.recruiter_user;
-  const candidate = interviewMeeting.interview_schedule.applications.candidates;
-  const job = interviewMeeting.interview_schedule.applications.public_jobs;
-
   const comp_email_temp = await fetchCompEmailTemp(
-    job.recruiter_id,
+    meeting_detail.interview_meeting.interview_schedule.recruiter_id,
     'interviewEnd_email_rShadowTraineeForMeetingAttendence',
   );
 
-  const comp_email_placeholder: EmailTemplateAPi<'interviewEnd_email_rShadowTraineeForMeetingAttendence'>['comp_email_placeholders'] =
-    {
-      candidateFirstName: candidate.first_name,
-      candidateLastName: candidate.last_name,
-      candidateName: getFullName(candidate.first_name, candidate.last_name),
-      traineeFirstName: trainee.first_name,
-      traineeLastName: trainee.last_name,
-      traineeName: getFullName(trainee.first_name, trainee.last_name),
-      organizerFirstName: organizer.first_name,
-      organizerLastName: organizer.last_name,
-      organizerName: getFullName(trainee.first_name, trainee.last_name),
-      OrganizerTimeZone: organizer.scheduling_settings.timeZone.tzCode,
-      sessionName: session.name,
-      interviewType: interview_module.name,
-      companyName: job.company,
-      jobRole: job.job_title,
-      reverseShadowCount: numberToOrdinal(reverseShadowCount),
-      reverseShadowConfirmLink: `<a href="#' target="_blank">here</a>`,
-    };
+  const candidate =
+    meeting_detail.interview_meeting.interview_schedule.applications.candidates;
+  const organizer = meeting_detail.interview_meeting.recruiter_user;
 
-  const filled_comp_template = fillCompEmailTemplate(
-    comp_email_placeholder,
-    comp_email_temp,
-  );
-  const react_email_placeholders: EmailTemplateAPi<'interviewEnd_email_rShadowTraineeForMeetingAttendence'>['react_email_placeholders'] =
-    {
-      companyLogo: job.logo,
-      emailBody: filled_comp_template.body,
-      subject: filled_comp_template.subject,
-    };
+  const job =
+    meeting_detail.interview_meeting.interview_schedule.applications
+      .public_jobs;
 
-  return {
-    filled_comp_template,
-    react_email_placeholders,
-    recipient_email: trainee.email,
-  };
+  const mail_details = training_ints.map((trainee) => {
+    const shadowCount = module_relations.find(
+      (s) => s.user_id === trainee.user_id,
+    ).number_of_reverse_shadow;
+    const comp_email_placeholder: EmailTemplateAPi<'interviewEnd_email_rShadowTraineeForMeetingAttendence'>['comp_email_placeholders'] =
+      {
+        candidateFirstName: candidate.first_name,
+        candidateLastName: candidate.last_name,
+        candidateName: getFullName(candidate.first_name, candidate.last_name),
+        traineeFirstName: trainee.first_name,
+        traineeLastName: trainee.last_name,
+        traineeName: getFullName(trainee.first_name, trainee.last_name),
+        organizerFirstName: organizer.first_name,
+        organizerLastName: organizer.last_name,
+        organizerName: getFullName(trainee.first_name, trainee.last_name),
+        OrganizerTimeZone: organizer.scheduling_settings.timeZone.tzCode,
+        sessionName: meeting_detail.name,
+        interviewType: meeting_detail.interview_module.name,
+        companyName: job.company,
+        jobRole: job.job_title,
+        reverseShadowCount: numberToOrdinal(shadowCount),
+        reverseShadowConfirmLink: `<a href="#' target="_blank">here</a>`,
+      };
+
+    const filled_comp_template = fillCompEmailTemplate(
+      comp_email_placeholder,
+      comp_email_temp,
+    );
+    const react_email_placeholders: EmailTemplateAPi<'interviewEnd_email_rShadowTraineeForMeetingAttendence'>['react_email_placeholders'] =
+      {
+        companyLogo: job.logo,
+        emailBody: filled_comp_template.body,
+        subject: filled_comp_template.subject,
+      };
+
+    return {
+      comp_email_placeholder,
+      filled_comp_template,
+      react_email_placeholders,
+      recipient_email: trainee.email,
+    };
+  });
+
+  return mail_details;
 }
