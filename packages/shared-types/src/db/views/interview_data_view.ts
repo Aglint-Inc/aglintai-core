131dd4fb0cc2980c79edd1204c7c6ddc7234d688
Chinmai <chinmai@aglinthq.com>
2024-07-31 15:23:01 +0530

    converted sql to view and added infinite scroll

---
+++ packages/shared-types/src/db/views/interview_data_view.ts
@@ -0,0 +1,26 @@
+import { DatabaseTable, DatabaseView, schedulingSettingType } from "../..";
+import type { ViewType } from "./index.types";
+
+export type CustomInterviewDataView = ViewType<
+  "interview_data_view",
+  {
+    applications: DatabaseTable["applications"];
+    candidates: {
+      id: string;
+      first_name: string;
+      last_name: string;
+      current_job_title: string;
+    };
+    schedule: DatabaseTable["interview_schedule"];
+    public_jobs: {
+      id: string;
+      job_title: string;
+      recruiting_coordinator: string;
+      department_id: number;
+    };
+    interview_session_meetings: {
+      interview_meeting: null | DatabaseTable["interview_meeting"];
+      interview_session: DatabaseTable["interview_session"];
+    }[];
+  }
+>;
