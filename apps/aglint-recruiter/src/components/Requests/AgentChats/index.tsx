a8f6e3074f7b35adab1a27c151711f9f3833dfb3
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-08-02 21:08:48 +0530

    fix: dummy data for progress

--- apps/aglint-recruiter/src/components/Requests/AgentChats/index.tsx
+++ apps/aglint-recruiter/src/components/Requests/AgentChats/index.tsx
@@ -33,6 +33,7 @@ import {
 
 function AgentChats() {
   const { recruiterUser, recruiter_id } = useAuthDetails();
+
   const [textToObject, setTextToObject] = useState<ApplicantInfo | null>(null);
   const [loading, setLoading] = useState(false);
   const { requests } = useRequests();
@@ -91,7 +92,9 @@ function AgentChats() {
             cancel_id: null,
           }) as DatabaseTableInsert['request_relation'],
       );
-      createRequestSessionRelations(sessionsRelations);
+      createRequestSessionRelations(sessionsRelations).then((res) => {
+        requests.refetch();
+      });
     });
   }
   return (
