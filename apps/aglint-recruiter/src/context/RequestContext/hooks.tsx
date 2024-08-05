f81dae4a6189442addefbbe6de80957c2bc63c1e
Punith G <punithg@aglinthq.com>
2024-08-05 15:46:14 +0530

    update, create , delete hooks - request/s contexts

--- apps/aglint-recruiter/src/context/RequestContext/hooks.tsx
+++ apps/aglint-recruiter/src/context/RequestContext/hooks.tsx
@@ -1,14 +1,67 @@
 import { useQuery } from '@tanstack/react-query';
+import { useCallback, useMemo } from 'react';
 
 import { requestQueries } from '@/src/queries/requests';
+
+import { useRequests } from '../RequestsContext';
 
 type RequestParams = Parameters<(typeof requestQueries)['request_progress']>[0];
 
 export const useRequestActions = ({ request_id }: RequestParams) => {
+  const {
+    handleUpdateRequest: updateRequest,
+    handleAsyncUpdateRequest: asyncUpdateRequest,
+    handleDeleteRequest: deleteRequest,
+    handleAsyncDeleteRequest: asyncDeleteRequest,
+    mutationQueue,
+  } = useRequests();
+
   const request_progress = useQuery(
     requestQueries.request_progress({ request_id }),
   );
+
+  const handleUpdateRequest = useCallback(
+    (payload: Parameters<typeof updateRequest>[0]['payload']) =>
+      updateRequest({
+        id: request_id,
+        payload,
+      }),
+    [request_id, updateRequest],
+  );
+  const handleAsyncUpdateRequest = useCallback(
+    async (payload: Parameters<typeof asyncUpdateRequest>[0]['payload']) =>
+      await asyncUpdateRequest({
+        id: request_id,
+        payload,
+      }),
+    [request_id, asyncUpdateRequest],
+  );
+  const handleDeleteRequest = useCallback(
+    () =>
+      deleteRequest({
+        id: request_id,
+      }),
+    [request_id, deleteRequest],
+  );
+  const handleAsyncDeleteRequest = useCallback(
+    async () =>
+      await asyncDeleteRequest({
+        id: request_id,
+      }),
+    [request_id, asyncUpdateRequest],
+  );
+
+  const isMutating = useMemo(
+    () => mutationQueue.includes(request_id),
+    [mutationQueue, request_id],
+  );
+
   return {
     request_progress,
+    handleDeleteRequest,
+    handleAsyncDeleteRequest,
+    handleUpdateRequest,
+    handleAsyncUpdateRequest,
+    isMutating,
   };
 };
