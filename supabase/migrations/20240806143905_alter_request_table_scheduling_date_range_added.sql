e27e23cdf5c9b3de1874d9f20fbbfe89fb5d85a0
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-08-06 20:20:54 +0530

    fix: updated default value now() to null

--- supabase/migrations/20240806143905_alter_request_table_scheduling_date_range_added.sql
+++ supabase/migrations/20240806143905_alter_request_table_scheduling_date_range_added.sql
@@ -1,5 +1,5 @@
-alter table "public"."request" add column "schedule_end_date" timestamp with time zone default now();
+alter table "public"."request" add column "schedule_end_date" timestamp with time zone default null;
 
-alter table "public"."request" add column "schedule_start_date" timestamp with time zone default now();
+alter table "public"."request" add column "schedule_start_date" timestamp with time zone default null;
 
 
