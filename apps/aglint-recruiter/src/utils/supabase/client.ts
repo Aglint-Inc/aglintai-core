79219f19d01f0b791aca408b248f82447e59ec2b
ChandraAglint <chandra@aglinthq.com>
2024-08-12 21:27:55 +0530

    lint fix

--- apps/aglint-recruiter/src/utils/supabase/client.ts
+++ apps/aglint-recruiter/src/utils/supabase/client.ts
@@ -6,16 +6,19 @@ import { createBrowserClient } from '@supabase/ssr';
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-export const supabase = createBrowserClient<DB>(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createBrowserClient<DB>(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY,
+);
 
-declare global {
-  interface Window {
-    supabase?: ReturnType<typeof createBrowserClient>;
-  }
-}
+// declare global {
+//   interface Window {
+//     supabase?: ReturnType<typeof createBrowserClient>;
+//   }
+// }
 
-if (SUPABASE_URL && SUPABASE_URL.includes('.supabase.co')) {
-  if (typeof window !== 'undefined') {
-    window.supabase = supabase;
-  }
-}
+// if (SUPABASE_URL && SUPABASE_URL.includes('.supabase.co')) {
+//   if (typeof window !== 'undefined') {
+//     window.supabase = supabase;
+//   }
+// }
