bdab85a135e2f2076fb7b509ff25b8c8160a4f8d
Dileep BC <dileep@aglinthq.com>
2024-08-16 23:26:52 +0530

    fix : delete trigger

---
+++ supabase/migrations/20240816175117_delete_trigger.sql
@@ -0,0 +1,7 @@
+drop trigger if exists "event_trigger_request_delete" on "public"."request";
+
+drop trigger if exists "event_trigger_request_insert" on "public"."request";
+
+drop trigger if exists "event_trigger_request_update" on "public"."request";
+
+
