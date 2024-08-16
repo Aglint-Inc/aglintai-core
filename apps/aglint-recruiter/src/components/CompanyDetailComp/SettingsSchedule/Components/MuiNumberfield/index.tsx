ce18e381058c99637ab5d77063843ca807dcc97d
ChandraAglint <chandra@aglinthq.com>
2024-08-16 13:38:53 +0530

    restore number field

--- apps/aglint-recruiter/src/components/CompanyDetailComp/SettingsSchedule/Components/MuiNumberfield/index.tsx
+++ apps/aglint-recruiter/src/components/CompanyDetailComp/SettingsSchedule/Components/MuiNumberfield/index.tsx
@@ -6,6 +6,7 @@ function MuiNumberfield({
   value,
   handleSelect,
   width = '160px',
+  height = '36px',
   isDebounceEnable = true,
   isDisable = false,
   isMarginTop = true,
@@ -46,25 +47,13 @@ function MuiNumberfield({
   );
 
   return (
-    <FormControl
-      sx={{
-        marginTop: isMarginTop ? '12px !important' : '',
-      }}
-    >
+    <FormControl sx={{ marginTop: isMarginTop ? '12px !important' : '' }}>
       <TextField
         disabled={isDisable}
-        inputProps={{ type: 'number' }}
         sx={{
           width: width,
           '& .MuiInputBase-root': {
-            // height: height, // Customize the height here
-          },
-          '& .MuiInputBase-input': {
-            // bgcolor: 'yellow',
-            paddingRight: 0,
-            width: width,
-            border: 'none',
-            margin: 'none !important',
+            height: height, // Customize the height here
           },
         }}
         value={Number(tempValue).toString()}
@@ -76,3 +65,81 @@ function MuiNumberfield({
 }
 
 export default MuiNumberfield;
+// import { FormControl, TextField } from '@mui/material';
+// import { debounce } from 'lodash';
+// import { useCallback, useEffect, useState } from 'react';
+
+// function MuiNumberfield({
+//   value,
+//   handleSelect,
+//   width = '160px',
+//   isDebounceEnable = true,
+//   isDisable = false,
+//   isMarginTop = true,
+//   max = 100,
+// }: {
+//   value: number | string;
+//   width?: string;
+//   isDebounceEnable?: boolean;
+//   height?: string;
+//   isMarginTop?: boolean;
+//   isDisable?: boolean;
+//   handleSelect: any;
+//   max?: number;
+// }) {
+//   const [tempValue, setTempValue] = useState(value);
+//   useEffect(() => {
+//     setTempValue(value);
+//   }, [value]);
+
+//   function handlerMinMax(e) {
+//     let newValue = +e.target.value;
+//     if (!(newValue >= 0)) newValue = 0;
+//     if (newValue > max) newValue = max;
+//     setTempValue(newValue);
+//     if (tempValue == newValue) return;
+//     if (isDebounceEnable) {
+//       debouncedChange(newValue);
+//     } else {
+//       handleSelect(newValue);
+//     }
+//   }
+
+//   const debouncedChange = useCallback(
+//     debounce((newValue) => {
+//       handleSelect(newValue);
+//     }, 500),
+//     [],
+//   );
+
+//   return (
+//     <FormControl
+//       sx={{
+//         marginTop: isMarginTop ? '12px !important' : '',
+//       }}
+//     >
+//       <TextField
+//         disabled={isDisable}
+//         inputProps={{ type: 'number' }}
+//         sx={{
+//           width: width,
+//           '& .MuiInputBase-root': {
+//             // height: height, // Customize the height here
+//           },
+//           '& .MuiInputBase-input': {
+//             // bgcolor: 'yellow',
+//             paddingRight: 0,
+//             width: width,
+//             border: 'none',
+//             margin: 'none !important',
+//           },
+//         }}
+//         value={Number(tempValue).toString()}
+//         onChange={handlerMinMax}
+//         type='number'
+//       />
+//     </FormControl>
+//   );
+// }
+
+// export default MuiNumberfield;
