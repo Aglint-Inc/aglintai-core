3a8f6f5fc5a37ba5ffc158592791d53e4836dc5b
Dileep B C <135205237+dileepbc3141@users.noreply.github.com>
2024-08-17 23:19:24 +0530

    Feat/db emit events (#1967)
    * feat : fire and forget
    
    * feat: js triggers
    
    * fix : trigger_func
    
    * feat: schedule_reminder
    
    * drop funcs
    
    * fix : delete trigger
    
    * fix: candidate_request_table
    
    * feat: update filter json
    
    * feat : js_triggers
    
    * fix : removed request_id int_meeting
    
    * fix : api fix move-to-interview
    
    * updated seed.sql
    
    * remove meeting_id
    
    * fix : fix:lint
    
    * fix : onUpdate request
    
    * fix : trigger
    
    * fix : cron run every 5 minute
    
    * fix: vault schema
    
    * fix : remove notes.txt
    
    * fix : sendgrid
    
    * fix : throw on Err

---
+++ apps/aglint-recruiter/src/services/event-triggers/trigger-funcs/onUpdateRequest.ts
@@ -0,0 +1,74 @@
+import { DatabaseTable } from '@aglint/shared-types';
+import { supabaseWrap } from '@aglint/shared-utils';
+import { dayjsLocal } from '@aglint/shared-utils/src/scheduling/dayjsLocal';
+
+import { supabaseAdmin } from '@/src/utils/supabase/supabaseAdmin';
+
+import { getWActions } from '../utils/w_actions';
+
+export const onUpdateRequest = async ({
+  new_data,
+  old_data,
+}: {
+  old_data: DatabaseTable['request'];
+  new_data: DatabaseTable['request'];
+}) => {
+  if (old_data.status === 'to_do' && new_data.status === 'in_progress') {
+    await triggerActions(new_data);
+  }
+};
+
+const triggerActions = async (new_data: DatabaseTable['request']) => {
+  try {
+    const [applications] = supabaseWrap(
+      await supabaseAdmin
+        .from('applications')
+        .select()
+        .eq('id', new_data.application_id),
+    );
+    const req_relns = supabaseWrap(
+      await supabaseAdmin
+        .from('request_relation')
+        .select()
+        .eq('request_id', new_data.id),
+    );
+
+    const { job_level_actions } = await getWActions(applications.job_id);
+
+    const promises = job_level_actions
+      .filter(
+        (j_l_a) =>
+          (new_data.type === 'schedule_request' &&
+            (j_l_a.workflow.trigger === 'onAvailReqAgent' ||
+              j_l_a.workflow.trigger === 'onSelfScheduleReqAgent')) ||
+          (new_data.type === 'reschedule_request' &&
+            j_l_a.workflow.trigger === 'onRequestReschedule') ||
+          (new_data.type === 'cancel_schedule_request' &&
+            j_l_a.workflow.trigger === 'onRequestCancel'),
+      )
+      .map(async (j_l_a) => {
+        supabaseWrap(
+          await supabaseAdmin.rpc('create_new_workflow_action_log', {
+            triggered_table: 'request',
+            base_time: dayjsLocal().toISOString(),
+            workflow_action_id: j_l_a.id,
+            workflow_id: j_l_a.workflow_id,
+            triggered_table_pkey: new_data.id,
+            interval_minutes: j_l_a.workflow.interval,
+            meta: {
+              target_api: j_l_a.target_api,
+              payload: j_l_a.payload,
+              request_id: new_data.id,
+              session_ids: req_relns.map((reln) => reln.session_id),
+              recruiter_id: j_l_a.workflow.recruiter_id,
+              application_id: new_data.application_id,
+            },
+            phase: j_l_a.workflow.phase,
+          }),
+        );
+      });
+    await Promise.allSettled(promises);
+  } catch (err) {
+    console.error(`Failed update request status event`, err.message);
+  }
+};
