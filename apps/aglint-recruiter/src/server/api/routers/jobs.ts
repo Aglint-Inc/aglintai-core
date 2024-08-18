c9a254a0df8281e57aed9c0ff9f4f617b5ae690a
Punith G <punithg@aglinthq.com>
2024-08-18 20:58:31 +0530

    integrated trpc

---
+++ apps/aglint-recruiter/src/server/api/routers/jobs.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+import { createTRPCRouter, publicProcedure } from '../trpc';
+
+export const jobsRouter = createTRPCRouter({
+  read: publicProcedure
+    .input(z.object({ recruiter_id: z.string().uuid() }))
+    .query(async ({ ctx: { db }, input: { recruiter_id } }) => {
+      return (
+        await db
+          .from('public_jobs')
+          .select('*')
+          .eq('recruiter_id', recruiter_id)
+          .throwOnError()
+      ).data;
+    }),
+});
