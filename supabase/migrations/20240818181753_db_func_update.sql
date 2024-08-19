1d44b59a3153e46b90aa36bf373bd90d03c0d7c0
Dileep BC <dileep@aglinthq.com>
2024-08-19 17:45:13 +0530

    fix : added col

--- supabase/migrations/20240818181753_db_func_update.sql
+++ supabase/migrations/20240818181753_db_func_update.sql
@@ -104,3 +104,48 @@ END;$function$
 ;
 
 
+CREATE TRIGGER event_trigger_interview_session_cancel_insert AFTER INSERT ON public.interview_session_cancel FOR EACH ROW EXECUTE FUNCTION call_webhook_on_change();
+
+
+DROP TRIGGER IF EXISTS workflow_action_deletion ON "public"."workflow";
+drop view if exists "public"."workflow_view";
+
+alter type "public"."workflow_trigger" rename to "workflow_trigger__old_version_to_be_dropped";
+
+create type "public"."workflow_trigger" as enum ('selfScheduleReminder', 'interviewStart', 'sendAvailReqReminder', 'interviewerConfirmation', 'interviewEnd', 'meetingDeclined', 'meetingAccepted', 'candidateBook', 'onQualified', 'onTrainingComplete', 'onAvailReqAgent', 'onReceivingAvailReq', 'onSelfScheduleReqAgent', 'onRequestCancel', 'onRequestReschedule', 'onRequestInterviewerDecline');
+
+alter table "public"."workflow" alter column trigger type "public"."workflow_trigger" using trigger::text::"public"."workflow_trigger";
+
+drop type "public"."workflow_trigger__old_version_to_be_dropped";
+
+create or replace view "public"."workflow_view" as  SELECT workflow.id,
+    workflow.created_at,
+    workflow.trigger,
+    workflow.phase,
+    workflow."interval",
+    workflow.title,
+    workflow.recruiter_id,
+    workflow.auto_connect,
+    workflow.description,
+    COALESCE(workflow_jobs.jobs, '[]'::json) AS jobs,
+    workflow.workflow_type,
+    workflow.is_paused
+   FROM (workflow
+     LEFT JOIN ( SELECT workflow_job_relation.workflow_id,
+            json_agg(json_build_object('id', workflow_job_relation.job_id, 'job_title', public_jobs.job_title, 'department', departments.name, 'location', row_to_json(office_locations.*), 'status', public_jobs.status)) AS jobs
+           FROM (((workflow_job_relation
+             LEFT JOIN public_jobs ON ((workflow_job_relation.job_id = public_jobs.id)))
+             LEFT JOIN departments ON ((departments.id = public_jobs.department_id)))
+             LEFT JOIN office_locations ON ((office_locations.id = public_jobs.location_id)))
+          GROUP BY workflow_job_relation.workflow_id) workflow_jobs ON ((workflow_jobs.workflow_id = workflow.id)));
+
+
+CREATE TRIGGER workflow_action_deletion AFTER UPDATE OF trigger ON public.workflow FOR EACH ROW EXECUTE FUNCTION trigger_workflow_action_deletion();
+
+alter table "public"."interview_session_cancel" add column "request_id" uuid;
+
+alter table "public"."interview_session_cancel" add constraint "interview_session_cancel_request_id_fkey" FOREIGN KEY (request_id) REFERENCES request(id) ON UPDATE CASCADE ON DELETE SET NULL not valid;
+
+alter table "public"."interview_session_cancel" validate constraint "interview_session_cancel_request_id_fkey";
+
+
