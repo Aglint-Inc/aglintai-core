ec36a4e047f31b3580ab5fdf230d9cfb22890099
ChandraAglint <chandra@aglinthq.com>
2024-08-20 13:06:01 +0530

    extension api added for reset workflow and template speratly

--- apps/aglint-recruiter/src/utils/paths/allowed.ts
+++ apps/aglint-recruiter/src/utils/paths/allowed.ts
@@ -99,6 +99,7 @@ export const allowedPaths = new Set<t>([
   '/api/automation/cancel_request',
   '/api/automation/get_users_not_in_constantEmails',
   '/api/automation/schedule_auth_update',
+  '/api/automation/seed_default_data',
   '/api/emails/sendSelfScheduleRequest_email_applicant',
   '/api/emails/interviewCancel_email_applicant',
   '/api/emails/selfScheduleReminder_email_applicant',
