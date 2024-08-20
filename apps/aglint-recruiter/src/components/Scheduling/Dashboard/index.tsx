3662b0ceee54d0f264be8b1a729e71df7edb117e
Punith G <punithg@aglinthq.com>
2024-08-20 20:53:32 +0530

    delete training progress

--- apps/aglint-recruiter/src/components/Scheduling/Dashboard/index.tsx
+++ apps/aglint-recruiter/src/components/Scheduling/Dashboard/index.tsx
@@ -1,18 +1,16 @@
-import { Stack } from '@mui/material';
-
 import { SchedulingDashboard as SchedulingDashboardDev } from '@/devlink3/SchedulingDashboard';
+import { useSchedulingAnalytics } from '@/src/context/SchedulingAnalytics';
 
+import Loader from '../../Common/Loader';
 import CancelReasons from './CancelReasons';
 import CompletedInterviewBarChart from './CompletedInterview';
 import InterviewersAnalyticCards from './InterviewersAnalyticCards';
 import InterviewMeetingStatus from './InterviewMeetingStatus';
-import RecentRescheduleCancel from './RecentRescheduleCancel';
-import TrainingProgress from './TrainingProgress';
-import InterviewTrainingStatus from './TrainingStatus';
-import { useSchedulingAnalytics } from '@/src/context/SchedulingAnalytics';
-import Loader from '../../Common/Loader';
-import { Tabs } from './tabs';
 import { Leaderboard } from './leaderboard';
+import RecentRescheduleCancel from './RecentRescheduleCancel';
+import { Tabs } from './tabs';
+// import TrainingProgress from './TrainingProgress';
+import InterviewTrainingStatus from './TrainingStatus';
 
 const SchedulingDashboard = () => {
   const { enabled } = useSchedulingAnalytics();
@@ -31,7 +29,7 @@ const SchedulingDashboard = () => {
           <InterviewTrainingStatus />
         </>
       }
-      slotTrainingProgress={<TrainingProgress />}
+      slotTrainingProgress={<></>}
       slotScheduleCount={<Tabs />}
       slotRecentReschedule={<RecentRescheduleCancel />}
       slotCompletedInterview={
