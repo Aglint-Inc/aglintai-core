7d1e921f07d7a7fb5d6cbda86b5bdc4d0c78eb78
Punith G <punithg@aglinthq.com>
2024-08-21 16:11:21 +0530

    revert deletes

---
+++ apps/aglint-recruiter/src/components/Scheduling/Dashboard/LeaderBoardWidget.tsx
@@ -0,0 +1,90 @@
+import { Avatar, Stack } from '@mui/material';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+
+import { Skeleton } from '@/devlink2/Skeleton';
+import { LeaderBoard } from '@/devlink3/LeaderBoard';
+import { LeaderBoardCard } from '@/devlink3/LeaderBoardCard';
+import { LeaderBoardLoader } from '@/devlink3/LeaderBoardLoader';
+import { NoData } from '@/devlink3/NoData';
+import { useInterviewLeaderboard } from '@/src/queries/scheduling-dashboard';
+import { getFullName } from '@/src/utils/jsonResume';
+import { capitalizeAll } from '@/src/utils/text/textUtils';
+
+import SchedulingDropdown from './SchedulingDropdown';
+
+const LeaderBoardWidget = () => {
+  const [type, setType] = useState<LeaderBoardWidgetRowsProps['type']>('month');
+  return (
+    <LeaderBoard
+      slotDropdownButton={<SchedulingDropdown type={type} setType={setType} />}
+      slotLeaderboardCard={<LeaderBoardWidgetRows type={type} />}
+    />
+  );
+};
+
+type LeaderBoardWidgetRowsProps = {
+  type: Parameters<typeof useInterviewLeaderboard>[0];
+};
+
+const LeaderBoardWidgetRows = ({ type }: LeaderBoardWidgetRowsProps) => {
+  const { data, status } = useInterviewLeaderboard(type);
+
+  if (status === 'pending')
+    return [...new Array(Math.trunc(Math.random() * 9) + 1)].map((_, i) => (
+      <LeaderBoardLoader key={i} slotSkeleton={<Skeleton />} />
+    ));
+
+  if (!(!!data && !!Array.isArray(data) && data.length !== 0))
+    return <NoData />;
+
+  return <LeaderBoardWidgetComponent interviewLeaderboard={data} />;
+};
+
+export default LeaderBoardWidget;
+
+type InterviewLeaderboardProps = {
+  interviewLeaderboard: ReturnType<typeof useInterviewLeaderboard>['data'];
+};
+const LeaderBoardWidgetComponent = ({
+  interviewLeaderboard,
+}: InterviewLeaderboardProps) => {
+  const router = useRouter();
+  return (
+    <>
+      {interviewLeaderboard.map((item, index) => (
+        <Stack
+          key={item.user_id}
+          onClick={() => {
+            router.push(`scheduling/interviewer/${item.user_id}?tab=overview`);
+          }}
+          sx={{
+            borderRadius: '4px',
+          }}
+        >
+          <Link href={`/user/profile/${item.user_id}`}>
+            <LeaderBoardCard
+              textCountNo={index + 1}
+              textName={
+                <Link href={`/user/profile/${item.user_id}`}>
+                  {capitalizeAll(getFullName(item.first_name, item.last_name))}
+                </Link>
+              }
+              textRole={item.user_position}
+              slotImage={
+                <Avatar
+                  src={item.profile_image}
+                  alt={getFullName(item.first_name, item.last_name)}
+                  variant='rounded-medium'
+                />
+              }
+              noInterview={item.interviews}
+              noHours={(item.duration / 60).toFixed(1)}
+            />
+          </Link>
+        </Stack>
+      ))}
+    </>
+  );
+};
---
+++ apps/aglint-recruiter/src/components/Scheduling/Dashboard/RecentRescheduleCancel.tsx
@@ -0,0 +1,295 @@
+/* eslint-disable security/detect-object-injection */
+import { Avatar, Skeleton } from '@mui/material';
+import dayjs from 'dayjs';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+
+import { NoData } from '@/devlink3/NoData';
+import { RecentDeclineList } from '@/devlink3/RecentDeclineList';
+import { RecentDeclines } from '@/devlink3/RecentDeclines';
+import { RecentReschedule } from '@/devlink3/RecentReschedule';
+import { RecentRescheduleList } from '@/devlink3/RecentRescheduleList';
+import {
+  useCancelRescheduleReasons,
+  useCancelRescheduleReasonsUsers,
+  useScheduleSessionsAnalytics,
+} from '@/src/queries/scheduling-dashboard';
+
+import { FilterDropDownDash } from './FilterDropDownDash';
+
+const RecentRescheduleCancel = () => {
+  const { data: analyticsData } = useScheduleSessionsAnalytics();
+  const { data: CancelReasonsData } = useCancelRescheduleReasons();
+  const {
+    data: userDetails,
+    isPending,
+    parentFetching,
+    disabled,
+  } = useCancelRescheduleReasonsUsers();
+
+  const [type, setType] = useState<'interviewer' | 'candidate'>('candidate');
+
+  const itemList = [
+    { label: 'Interview', value: 'interviewer' },
+    { label: 'Candidate', value: 'candidate' },
+  ];
+  const processedRescheduleData = (CancelReasonsData || [])
+    .filter((item) => item.type == 'reschedule')
+    .reduce(
+      (acc, curr) => {
+        if (curr.session_relation_id == null) {
+          const temp = acc['candidate'] || [];
+          const tempAD = analyticsData.find(
+            (item) => item.interview_session.id == curr.session_id,
+          );
+          const detail_id = tempAD?.interview_schedule.application_id;
+          const meet_id = tempAD?.interview_meeting.id;
+          const temp_user = userDetails?.[type];
+          const user = temp_user?.[detail_id];
+          if (user) {
+            const tempItem = {
+              id: user.id,
+              meet_id: meet_id,
+              name: user.name,
+              image: user.profile_image,
+              time: curr.created_at,
+              desc: curr.other_details?.note || curr.reason,
+            };
+
+            acc['candidate'] = [...temp, tempItem];
+          }
+        } else {
+          const temp = acc['interviewer'] || [];
+          const detail_id = curr.session_relation_id;
+          const meet_id = analyticsData.find(
+            (item) => item.interview_session.id == curr.session_id,
+          )?.interview_meeting.id;
+          const temp_user = userDetails?.[type];
+          const user = temp_user?.[detail_id];
+          if (user) {
+            const tempItem = {
+              id: user.id,
+              meet_id: meet_id,
+              name: user.name,
+              image: user.profile_image,
+              time: curr.created_at,
+              desc: curr.other_details?.note || curr.reason,
+            };
+            acc['interviewer'] = [...temp, tempItem];
+          }
+        }
+        return acc;
+      },
+      { interviewer: [], candidate: [] } as {
+        // eslint-disable-next-line no-unused-vars
+        interviewer: {
+          id: any;
+          meet_id: string;
+          name: any;
+          image: any;
+          time: string;
+          desc: string;
+        }[];
+        candidate: {
+          id: any;
+          meet_id: string;
+          name: any;
+          image: any;
+          time: string;
+          desc: string;
+        }[];
+      },
+    );
+  const processedCancelData = (CancelReasonsData || [])
+    .map((item) => {
+      const temp_user = {
+        ...userDetails?.['candidate'],
+        ...userDetails?.['interviewer'],
+      };
+      const detail_id = analyticsData.find(
+        (itemX) => itemX.interview_session.id == item.session_id,
+      )?.interview_schedule.application_id;
+      const user = temp_user?.[detail_id];
+      return (
+        user && {
+          id: user.id,
+          name: user.name,
+          image: user.profile_image,
+          time: item.created_at,
+          desc: item.other_details?.note || item.reason,
+        }
+      );
+    })
+    .filter((item) => item);
+  const loading = parentFetching || (!disabled && isPending);
+
+  return (
+    <>
+      <RecentReschedule
+        slotRecentRescheduleList={
+          loading ? (
+            <RecentRescheduleListItem loading />
+          ) : processedRescheduleData?.[type].length ? (
+            processedRescheduleData?.[type]
+              .filter((item) => item)
+              .map((user) => {
+                return (
+                  <RecentRescheduleListItem
+                    key={user.id}
+                    detail={{
+                      meet_id: user.meet_id,
+                      name: user.name,
+                      image: user.image,
+                      time: user.time,
+                      desc: user.desc,
+                    }}
+                  />
+                );
+              })
+          ) : (
+            <NoData />
+          )
+        }
+        slotDropdown={
+          <FilterDropDownDash
+            itemList={itemList}
+            onChange={setType}
+            value={type}
+          />
+        }
+      />
+      <RecentDeclines
+        slotRecentDeclineList={
+          <>
+            {loading ? (
+              <RecentDeclineListItem loading />
+            ) : processedCancelData?.length ? (
+              processedCancelData.map((user) => {
+                return (
+                  <RecentDeclineListItem
+                    key={user.id}
+                    detail={{
+                      name: user.name,
+                      image: user.image,
+                      time: user.time,
+                      desc: user.desc,
+                    }}
+                  />
+                );
+              })
+            ) : (
+              <NoData />
+            )}
+          </>
+        }
+      />
+    </>
+  );
+};
+
+export default RecentRescheduleCancel;
+
+const RecentRescheduleListItem = ({
+  loading = false,
+  detail = null,
+}: {
+  loading?: boolean;
+  detail?: {
+    meet_id: string;
+    name: string;
+    image: string;
+    time: string;
+    desc: string;
+  };
+}) => {
+  const router = useRouter();
+  if (loading)
+    return (
+      <RecentRescheduleList
+        slotImage={
+          <Skeleton variant='circular' width={'100%'} height={'100%'} />
+        }
+        textName={
+          <Skeleton variant='text' width={'100px'} height={'var(--space-6)'} />
+        }
+        textTime={
+          <Skeleton variant='text' width={'50px'} height={'var(--space-6)'} />
+        }
+        textDesc={
+          <Skeleton variant='text' width={'200px'} height={'var(--space-6)'} />
+        }
+      />
+    );
+  return (
+    <RecentRescheduleList
+      slotImage={
+        <Avatar
+          src={detail.image ? detail.image : undefined}
+          alt={detail.name}
+          variant='rounded-medium'
+        />
+      }
+      onClickCandidate={{
+        onClick: () => {
+          if (detail?.meet_id)
+            router.push(
+              `/scheduling/view?meeting_id=${detail?.meet_id}&tab=candidate_details`,
+            );
+        },
+      }}
+      textName={detail.name}
+      textTime={dayjs(detail.time).fromNow()}
+      textDesc={detail.desc}
+      onClickView={{
+        onClick: () => {
+          router.push(`/scheduling/view?meeting_id=${detail.meet_id}`);
+        },
+      }}
+    />
+  );
+};
+
+const RecentDeclineListItem = ({
+  loading = false,
+  detail = null,
+}: {
+  loading?: boolean;
+  detail?: {
+    name: string;
+    image: string;
+    time: string;
+    desc: string;
+  };
+}) => {
+  if (loading)
+    return (
+      <RecentDeclineList
+        slotImage={
+          <Skeleton variant='circular' width={'100%'} height={'100%'} />
+        }
+        textName={
+          <Skeleton variant='text' width={'100px'} height={'var(--space-6)'} />
+        }
+        textTime={
+          <Skeleton variant='text' width={'50px'} height={'var(--space-6)'} />
+        }
+        textDesc={
+          <Skeleton variant='text' width={'200px'} height={'var(--space-6)'} />
+        }
+      />
+    );
+  return (
+    <RecentDeclineList
+      slotImage={
+        <Avatar
+          src={detail.image ? detail.image : undefined}
+          alt={detail.name}
+          variant='rounded-medium'
+        />
+      }
+      textName={detail.name}
+      textTime={dayjs(detail.time).fromNow()}
+      textDesc={detail.desc}
+    />
+  );
+};
