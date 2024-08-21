2c24e0486592327ef91f42ee0aafe76a12b7f0e6
ChandraAglint <chandra@aglinthq.com>
2024-08-21 21:37:37 +0530

    interview load adding

--- apps/aglint-recruiter/src/components/Interviewers/InterviewLoad.tsx
+++ apps/aglint-recruiter/src/components/Interviewers/InterviewLoad.tsx
@@ -3,23 +3,22 @@ import { useEffect } from 'react';
 import { useAuthDetails } from '@/src/context/AuthContext/AuthContext';
 import { supabase } from '@/src/utils/supabase/client';
 function InterviewLoad() {
-  //   const { recruiter } = useAuthDetails();
+  const { recruiter } = useAuthDetails();
 
-  //   const fetchData = async () => {
-  //     const { data: all_interviewers } = await supabase
-  //       .from('all_interviewers')
-  //       .select('user_id,position,profile_image,first_name,last_name')
-  //       .eq('recruiter_id', recruiter.id);
+  const fetchData = async () => {
+    const { data: all_interviewers } = await supabase
+      .from('all_interviewers')
+      .select(
+        'position,profile_image,first_name,last_name,completed_meeting_last_month',
+      )
+      .eq('recruiter_id', recruiter.id);
 
-  //     console.log(all_interviewers);
-  //     // const interviewers_ids = all_interviewers.map(
-  //     //   (interviewer) => interviewer.user_id,
-  //     // );
-  //   };
+    console.log(all_interviewers);
+  };
 
-  //   useEffect(() => {
-  //     fetchData();
-  //   }, [recruiter.id]);
+  useEffect(() => {
+    fetchData();
+  }, [recruiter.id]);
   return <>InterviewLoad</>;
 }
 
