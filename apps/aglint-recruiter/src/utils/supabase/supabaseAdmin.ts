d343f36e12f1908bba3e0e574c3bc31f0bcfcfeb
Ravi K <ravi@aglinthq.com>
2024-08-22 11:58:37 +0530

    Added logs for supbasekey error tracing

--- apps/aglint-recruiter/src/utils/supabase/supabaseAdmin.ts
+++ apps/aglint-recruiter/src/utils/supabase/supabaseAdmin.ts
@@ -1,7 +1,13 @@
 import { DB } from '@aglint/shared-types';
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseAdmin = createClient<DB>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY,
-);
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const key = process.env.SUPABASE_SERVICE_KEY;
+const env = process.env.NEXT_PUBLIC_HOST_NAME!;
+if (!url) {
+  throw new Error(`Missing SUPABASE_URL on ${env}`);
+}
+if (!key) {
+  throw new Error(`Missing SUPABASE_SERVICE_KEY on ${env}`);
+}
+export const supabaseAdmin = createClient<DB>(url, key);
