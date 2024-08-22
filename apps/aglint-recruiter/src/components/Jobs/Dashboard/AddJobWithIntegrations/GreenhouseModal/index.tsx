f8da22d422db49b4be6ddf0b7f082a43d8569384
Punith G <punithg@aglinthq.com>
2024-08-22 18:26:44 +0530

    fix greenhouse modal title

--- apps/aglint-recruiter/src/components/Jobs/Dashboard/AddJobWithIntegrations/GreenhouseModal/index.tsx
+++ apps/aglint-recruiter/src/components/Jobs/Dashboard/AddJobWithIntegrations/GreenhouseModal/index.tsx
@@ -105,6 +105,7 @@ export function GreenhouseModal() {
       }}
     >
       <SideDrawerLarge
+        textDrawertitle={'Import from Greenhouse'}
         drawerSize={'small'}
         slotButtons={
           <>
