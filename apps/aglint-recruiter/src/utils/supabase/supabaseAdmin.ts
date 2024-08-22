2bb5a260fb7e49a8fd32fe7540492838f94663f1
Chinmai <chinmai@aglinthq.com>
2024-08-22 13:28:03 +0530

    fix build

--- apps/aglint-recruiter/src/utils/supabase/supabaseAdmin.ts
+++ apps/aglint-recruiter/src/utils/supabase/supabaseAdmin.ts
@@ -2,12 +2,15 @@ import { DB } from '@aglint/shared-types';
 import { createClient } from '@supabase/supabase-js';
 
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const key = process.env.SUPABASE_SERVICE_KEY;
+const key =
+  process.env.SUPABASE_SERVICE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const env = process.env.NEXT_PUBLIC_HOST_NAME!;
 if (!url) {
   throw new Error(`Missing SUPABASE_URL on ${env}`);
 }
 if (!key) {
-  throw new Error(`Missing SUPABASE_SERVICE_KEY on ${env}, ${JSON.stringify(process.env)}`);
+  throw new Error(
+    `Missing SUPABASE_SERVICE_KEY on ${env}, ${JSON.stringify(process.env)}`,
+  );
 }
 export const supabaseAdmin = createClient<DB>(url, key);
