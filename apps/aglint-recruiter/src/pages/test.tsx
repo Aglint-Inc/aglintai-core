db4600cc3b6b85d108c6f4ce4cc0a4b9726e00eb
Chinmai <chinmai@aglinthq.com>
2024-08-26 16:15:06 +0530

    fix lint

--- apps/aglint-recruiter/src/pages/test.tsx
+++ apps/aglint-recruiter/src/pages/test.tsx
@@ -1,13 +1,7 @@
-import { useEffect } from 'react';
+
 import { ChatApp } from '../components/ChatApp/ChatApp';
-import { supabaseAdmin } from '../utils/supabase/supabaseAdmin';
-import axios from 'axios';
-import { supabase } from '../utils/supabase/client';
 
 function Page() {
-  useEffect(() => {
-    fetchUsers();
-  }, []);
   return (
     <>
       <ChatApp />
@@ -20,23 +14,3 @@ Page.publicProvider = (page) => {
 };
 
 export default Page;
-
-const fetchUsers = async (user_id: string) => {
-  const users = (
-    await supabase
-      .from('recruiter_user')
-      .select('*,calendar_sync')
-      .is('is_calendar_connected', true)
-      .throwOnError()
-  ).data;
-
-  await Promise.all(
-    users.map((user) => {
-      axios.post('/api/google-calender/resync', {
-        user_id: user.user_id,
-      });
-    }),
-  );
-
-  return true;
-};
