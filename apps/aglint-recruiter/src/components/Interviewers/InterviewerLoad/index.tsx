ade491a71019ef8f177e48b2705e2a3c58420bfc
ChandraAglint <chandra@aglinthq.com>
2024-08-26 16:47:39 +0530

    Interview load

--- apps/aglint-recruiter/src/components/Interviewers/InterviewerLoad/index.tsx
+++ apps/aglint-recruiter/src/components/Interviewers/InterviewerLoad/index.tsx
@@ -1,21 +1,95 @@
 import { getFullName } from '@aglint/shared-utils';
 import { Avatar, Stack } from '@mui/material';
+import { useState } from 'react';
 
 import { InterviewerWorkload } from '@/devlink3/InterviewerWorkload';
 import { InterviewWorkloadList } from '@/devlink3/InterviewWorkloadList';
 import { useAuthDetails } from '@/src/context/AuthContext/AuthContext';
+import { useJobs } from '@/src/context/JobsContext';
+import { useAllDepartments } from '@/src/queries/departments';
+import { useAllOfficeLocations } from '@/src/queries/officeLocations';
 import dayjs from '@/src/utils/dayjs';
 
 import Loader from '../../Common/Loader';
+import { Filter } from '../components/Filter';
 import { useAllInterviewer, useAllInterviewerType } from '../Hook';
 import LineGraph from './LineGraph';
 
 function InterviewerLoad() {
   const { recruiter } = useAuthDetails();
-
   const { data: interviewers, isLoading } = useAllInterviewer(recruiter.id);
+  const { data: departments } = useAllDepartments();
+  const { data: locations } = useAllOfficeLocations();
+  const [selectedInterviewTypes, setInterviewTypes] = useState<string[]>([]);
+  const [selectedJobs, setJobs] = useState<string[]>([]);
+  const [selectedDepartments, setDepartments] = useState<string[]>([]);
+  const [selectedLocations, setLocations] = useState<string[]>([]);
 
-  // to calculate a maximum number for count per day on over all | default is 5 meeting per day
+  const {
+    jobs: { data: Jobs },
+  } = useJobs();
+
+  //Location filter List
+  const locationList = locations?.length
+    ? locations.map((loc) => ({
+        name: loc.city + ', ' + loc.region + ', ' + loc.country,
+        value: loc.id,
+      }))
+    : [];
+
+  //Department filter list
+  const departmentList = departments?.length
+    ? departments.map((dep) => ({ name: dep.name, value: dep.id }))
+    : [];
+
+  //Job filter List
+  const JobsList = Jobs?.length
+    ? Jobs.map((job) => ({
+        name: job.job_title,
+        value: job.id,
+      }))
+    : [];
+
+  // Interview Type filter list
+  const allInterviewTypes = interviewers?.length
+    ? [
+        ...new Set(
+          interviewers
+            .map((interviewer) => [
+              ...interviewer.qualified_module_names,
+              ...interviewer.training_module_names,
+            ])
+            .flat()
+            .filter((interviewType) => interviewType),
+        ),
+      ]
+    : [];
+
+  const InterviewTypeOptions = allInterviewTypes.map((type) => ({
+    name: type,
+    value: type.replace(/\s+/g, '').toLowerCase(),
+  }));
+
+  // filtering the interviewers
+  const filteredInters = interviewers?.filter((interviewer) => {
+    const qualifedType = interviewer.qualified_module_names.some((interType) =>
+      selectedInterviewTypes.includes(
+        interType?.replace(/\s+/g, '').toLowerCase(),
+      ),
+    );
+    const trainingType = interviewer.training_module_names.some((interType) =>
+      selectedInterviewTypes.includes(
+        interType?.replace(/\s+/g, '').toLowerCase(),
+      ),
+    );
+
+    return qualifedType || trainingType;
+  });
+
+  const filteredInterviewers =
+    filteredInters?.length === 0 ? interviewers : filteredInters;
+
+  // calculate max count for height of the graph
   let maxCount = 5;
 
   if (interviewers?.length)
@@ -40,11 +114,42 @@ function InterviewerLoad() {
         <Loader />
       </Stack>
     );
+
   return (
     <>
       <InterviewerWorkload
         textDateRange={`${dayjs().subtract(1, 'month').format('DD MMM YYYY')} - ${dayjs().format('DD MMM YYYY')}`}
-        slotInterviewWorkloadList={interviewers.map((interviewer) => (
+        slotFilter={
+          <Stack direction={'row'} gap={1}>
+            <Filter
+              itemList={
+                InterviewTypeOptions?.length ? InterviewTypeOptions : []
+              }
+              title='Interview Types'
+              setSelectedItems={setInterviewTypes}
+              selectedItems={selectedInterviewTypes}
+            />
+            <Filter
+              itemList={JobsList?.length ? JobsList : []}
+              title='Jobs'
+              setSelectedItems={setJobs}
+              selectedItems={selectedJobs}
+            />
+            <Filter
+              itemList={departmentList?.length ? departmentList : []}
+              title='Departments'
+              setSelectedItems={setDepartments}
+              selectedItems={selectedDepartments}
+            />
+            <Filter
+              itemList={locationList?.length ? locationList : []}
+              title='Locations'
+              setSelectedItems={setLocations}
+              selectedItems={selectedLocations}
+            />
+          </Stack>
+        }
+        slotInterviewWorkloadList={filteredInterviewers.map((interviewer) => (
           <InterviewerCard
             key={interviewer.user_id}
             interviewer={interviewer}
--- apps/aglint-recruiter/src/components/Interviewers/Metrics/index.tsx
+++ apps/aglint-recruiter/src/components/Interviewers/Metrics/index.tsx
@@ -1,20 +1,14 @@
 import { DatabaseTable } from '@aglint/shared-types';
-import { Avatar, Checkbox, Popover, Stack, Typography } from '@mui/material';
+import { Avatar, Stack } from '@mui/material';
 import React, { useState } from 'react';
 
 import { GlobalEmptyState } from '@/devlink/GlobalEmptyState';
-import { GlobalIcon } from '@/devlink/GlobalIcon';
-import { ButtonFilter } from '@/devlink2/ButtonFilter';
-import { FilterDropdown } from '@/devlink2/FilterDropdown';
 import { InterviewerMetricList } from '@/devlink3/InterviewerMetricList';
 import { InterviewerMetrics } from '@/devlink3/InterviewerMetrics';
 import { useAllDepartments } from '@/src/queries/departments';
-import {
-  capitalizeAll,
-  capitalizeFirstLetter,
-} from '@/src/utils/text/textUtils';
 
 import Loader from '../../Common/Loader';
+import { Filter } from '../components/Filter';
 import { useLeaderBoard } from '../Hook';
 import { LeaderAnalyticsFilterType } from '../types';
 
@@ -121,128 +115,3 @@ function Metrics() {
 }
 
 export default Metrics;
-
-const Filter = ({
-  selectedItems,
-  setSelectedItems,
-  itemList,
-  title,
-  isSingle = false,
-  nameIsTitle = false,
-}: {
-  title?: string;
-  itemList: { name: string; value: any }[];
-  selectedItems: any[] | any;
-  setSelectedItems: any;
-  isSingle?: boolean;
-  nameIsTitle?: boolean;
-}) => {
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
-    null,
-  );
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'jobs-filter' : undefined;
-  function handleClose() {
-    setAnchorEl(null);
-  }
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  return (
-    <>
-      <Stack minWidth={'100px'}>
-        <ButtonFilter
-          isActive={Boolean(selectedItems.length)}
-          isDotVisible={isSingle ? false : Boolean(selectedItems.length)}
-          onClickStatus={{
-            onClick: handleClick,
-          }}
-          textLabel={
-            nameIsTitle
-              ? itemList.find((item) => item.value === selectedItems).name
-              : title
-          }
-          slotRightIcon={
-            <Stack>
-              <GlobalIcon
-                iconName={
-                  anchorEl ? 'keyboard_arrow_up' : 'keyboard_arrow_down'
-                }
-              />
-            </Stack>
-          }
-        />
-      </Stack>
-
-      <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{ vertical: -10, horizontal: 0 }}
-      >
-        <FilterDropdown
-          isRemoveVisible={false}
-          isResetVisible={isSingle ? false : itemList.length !== 0}
-          slotOption={
-            itemList.length ? (
-              itemList?.map((item, i) => {
-                return (
-                  <Stack
-                    key={i}
-                    direction={'row'}
-                    sx={{
-                      alignItems: 'center',
-                      userSelect: 'none',
-                      backgroundColor: isSingle
-                        ? selectedItems === item.value
-                          ? 'var(--neutral-2)'
-                          : ''
-                        : '',
-                      ':hover': { bgcolor: 'var(--neutral-2)' },
-                      borderRadius: 'var(--radius-2)',
-                      cursor: 'pointer',
-                      minWidth: '120px',
-                    }}
-                    spacing={1}
-                    padding={'var(--space-2) var(--space-2)'}
-                    onClick={() => {
-                      if (isSingle) setSelectedItems(item.value);
-                      else
-                        setSelectedItems(() =>
-                          selectedItems.includes(item.value)
-                            ? selectedItems.filter((pre) => pre !== item.value)
-                            : [...selectedItems, item.value],
-                        );
-                    }}
-                  >
-                    {!isSingle && (
-                      <Checkbox checked={selectedItems.includes(item.value)} />
-                    )}
-                    <Typography>{capitalizeFirstLetter(item.name)}</Typography>
-                  </Stack>
-                );
-              })
-            ) : (
-              <GlobalEmptyState
-                textDesc={`No ${capitalizeAll(title)}`}
-                iconName={'add'}
-              />
-            )
-          }
-          onClickReset={{
-            onClick: () => {
-              setSelectedItems(() => []);
-            },
-          }}
-        />
-      </Popover>
-    </>
-  );
-};
