91f00f567fcd232c08621b63f7e98488a2066c38
Dileep BC <dileep@aglinthq.com>
2024-08-26 16:47:38 +0530

    fix

--- apps/aglint-recruiter/src/components/Requests/RequestSections/Section/Request/RequestDetails/RequestProgress/NewScheduleEvents/CandidateAvailReceive.tsx
+++ apps/aglint-recruiter/src/components/Requests/RequestSections/Section/Request/RequestDetails/RequestProgress/NewScheduleEvents/CandidateAvailReceive.tsx
@@ -19,41 +19,41 @@ const CandidateAvailReceive = ({
 }) => {
   const { request_progress } = useRequest();
   let lastEvent: DatabaseTable['request_progress'];
-  const { availRecivedProgress, reqProgresMp } = useMemo(() => {
-    let progres: DatabaseTable['request_progress'][] = [];
-    let mp: RequestProgressMapType = {};
+  const { availRecivedProgress: availReceivedProgress, reqProgresMp } =
+    useMemo(() => {
+      let progres: DatabaseTable['request_progress'][] = [];
+      let mp: RequestProgressMapType = {};
 
-    if (request_progress.data.length === 0) {
+      if (request_progress.data.length === 0) {
+        return { availRecivedProgress: progres, reqProgresMp: mp };
+      }
+      request_progress.data.forEach((prog) => {
+        if (prog.event_type === 'CAND_AVAIL_REC') {
+          progres.push({
+            ...prog,
+          });
+        } else if (
+          progres.length > 0 &&
+          prog.event_type !== 'CAND_CONFIRM_SLOT'
+        ) {
+          progres.push({
+            ...prog,
+          });
+        }
+      });
+
+      progres.forEach((row) => {
+        if (!mp[row.event_type]) {
+          mp[row.event_type] = [];
+        }
+        mp[row.event_type].push({ ...row });
+      });
       return { availRecivedProgress: progres, reqProgresMp: mp };
-    }
-    request_progress.data.forEach((prog) => {
-      if (prog.event_type === 'CAND_AVAIL_REC') {
-        progres.push({
-          ...prog,
-        });
-      } else if (
-        progres.length > 0 &&
-        prog.event_type !== 'CAND_CONFIRM_SLOT'
-      ) {
-        progres.push({
-          ...prog,
-        });
-      }
-    });
-
-    progres.forEach((row) => {
-      if (!mp[row.event_type]) {
-        mp[row.event_type] = [];
-      }
-      mp[row.event_type].push({ ...row });
-    });
-    return { availRecivedProgress: progres, reqProgresMp: mp };
-  }, [request_progress.data]);
-  if (availRecivedProgress.length > 0) {
-    lastEvent = availRecivedProgress[availRecivedProgress.length - 1];
+    }, [request_progress.data]);
+  if (availReceivedProgress.length > 0) {
+    lastEvent = availReceivedProgress[availReceivedProgress.length - 1];
   }
 
-  console.log(eventTargetMap['onReceivingAvailReq']);
   return (
     <Stack rowGap={1.5}>
       <TextWithIcon
