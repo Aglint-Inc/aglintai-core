ccc30bdd7234c080f0a259de5d7d62a5b44ba09d
Punith G <punithg@aglinthq.com>
2024-08-26 21:25:56 +0530

    hide closed jobs

---
+++ supabase/migrations/20240826155323_workflow_view.sql
@@ -0,0 +1,25 @@
+create or replace view "public"."workflow_view" as  SELECT workflow.id,
+    workflow.created_at,
+    workflow.trigger,
+    workflow.phase,
+    workflow."interval",
+    workflow.title,
+    workflow.recruiter_id,
+    workflow.auto_connect,
+    workflow.description,
+    COALESCE(workflow_jobs.jobs, '[]'::json) AS jobs,
+    workflow.workflow_type,
+    workflow.is_paused,
+    workflow.is_request_workflow
+   FROM (workflow
+     LEFT JOIN ( SELECT workflow_job_relation.workflow_id,
+            json_agg(json_build_object('id', workflow_job_relation.job_id, 'job_title', public_jobs.job_title, 'department', departments.name, 'location', row_to_json(office_locations.*), 'status', public_jobs.status)) AS jobs
+           FROM (((workflow_job_relation
+             LEFT JOIN public_jobs ON ((workflow_job_relation.job_id = public_jobs.id)))
+             LEFT JOIN departments ON ((departments.id = public_jobs.department_id)))
+             LEFT JOIN office_locations ON ((office_locations.id = public_jobs.location_id)))
+          WHERE (public_jobs.status <> 'closed'::public_job_status)
+          GROUP BY workflow_job_relation.workflow_id) workflow_jobs ON ((workflow_jobs.workflow_id = workflow.id)));
+
+
+
