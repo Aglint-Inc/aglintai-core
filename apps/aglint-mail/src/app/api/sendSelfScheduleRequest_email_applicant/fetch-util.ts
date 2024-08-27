3cea5b273eb7622ab040bdee96968b25a22fc2bf
Dileep BC <dileep@aglinthq.com>
2024-08-27 08:52:58 +0530

    mail: lint

--- apps/aglint-mail/src/app/api/sendSelfScheduleRequest_email_applicant/fetch-util.ts
+++ apps/aglint-mail/src/app/api/sendSelfScheduleRequest_email_applicant/fetch-util.ts
@@ -57,12 +57,9 @@ export async function dbUtil(
     },
   } = filterJson;
 
-  const task_id = req_body.task_id;
   let scheduleLink = '';
   if (filterJson.applications.id && req_body.filter_json_id) {
-    scheduleLink = task_id
-      ? `${process.env.NEXT_PUBLIC_APP_URL}/scheduling/invite/${filterJson.applications.id}?filter_id=${req_body.filter_json_id}&task_id=${task_id}`
-      : `${process.env.NEXT_PUBLIC_APP_URL}/scheduling/invite/${filterJson.applications.id}?filter_id=${req_body.filter_json_id}`;
+    scheduleLink = `${process.env.NEXT_PUBLIC_APP_URL}/scheduling/invite/${filterJson.applications.id}?filter_id=${req_body.filter_json_id}`;
   }
   const comp_email_placeholder: EmailTemplateAPi<'sendSelfScheduleRequest_email_applicant'>['comp_email_placeholders'] =
     {
