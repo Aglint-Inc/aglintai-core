ae42e0b8df3a1b65319ddb458f5b4b904960eae5
Dileep BC <dileep@aglinthq.com>
2024-08-27 14:45:37 +0530

    fix : cand avail recive

--- apps/aglint-recruiter/src/components/Requests/RequestSections/Section/Request/RequestDetails/RequestProgress/NewScheduleEvents/CandidateAvailReceive.tsx
+++ apps/aglint-recruiter/src/components/Requests/RequestSections/Section/Request/RequestDetails/RequestProgress/NewScheduleEvents/CandidateAvailReceive.tsx
@@ -28,37 +28,48 @@ const CandidateAvailReceive = ({
   const requestId = query.id as string;
   const { request_progress } = useRequest();
   let lastEvent: DatabaseTable['request_progress'];
-  const { availRecivedProgress: availReceivedProgress, reqProgresMp } =
-    useMemo(() => {
-      let progres: DatabaseTable['request_progress'][] = [];
-      let mp: RequestProgressMapType = {};
+  const {
+    availRecivedProgress: availReceivedProgress,
+    reqProgresMp,
+    isSlotConfirmed,
+  } = useMemo(() => {
+    let progres: DatabaseTable['request_progress'][] = [];
+    let mp: RequestProgressMapType = {};
+    let isSlotConfirmed = false;
 
-      if (request_progress.data.length === 0) {
-        return { availRecivedProgress: progres, reqProgresMp: mp };
-      }
-      request_progress.data.forEach((prog) => {
-        if (prog.event_type === 'CAND_AVAIL_REC') {
-          progres.push({
-            ...prog,
-          });
-        } else if (
-          progres.length > 0 &&
-          prog.event_type !== 'CAND_CONFIRM_SLOT'
-        ) {
-          progres.push({
-            ...prog,
-          });
-        }
-      });
-
-      progres.forEach((row) => {
-        if (!mp[row.event_type]) {
-          mp[row.event_type] = [];
-        }
-        mp[row.event_type].push({ ...row });
-      });
+    if (request_progress.data.length === 0) {
       return { availRecivedProgress: progres, reqProgresMp: mp };
-    }, [request_progress.data]);
+    }
+    request_progress.data.forEach((prog) => {
+      if (prog.event_type === 'CAND_AVAIL_REC') {
+        progres.push({
+          ...prog,
+        });
+      } else if (
+        progres.length > 0 &&
+        prog.event_type !== 'CAND_CONFIRM_SLOT'
+      ) {
+        progres.push({
+          ...prog,
+        });
+      }
+      if (prog.event_type == 'CAND_CONFIRM_SLOT') {
+        isSlotConfirmed = true;
+      }
+    });
+
+    progres.forEach((row) => {
+      if (!mp[row.event_type]) {
+        mp[row.event_type] = [];
+      }
+      mp[row.event_type].push({ ...row });
+    });
+    return {
+      availRecivedProgress: progres,
+      reqProgresMp: mp,
+      isSlotConfirmed,
+    };
+  }, [request_progress.data]);
   if (availReceivedProgress.length > 0) {
     lastEvent = availReceivedProgress[availReceivedProgress.length - 1];
   }
@@ -84,6 +95,7 @@ const CandidateAvailReceive = ({
   ) {
     isManual = false;
   }
+
   return (
     <Stack rowGap={1.5}>
       <TextWithIcon
@@ -108,6 +120,7 @@ const CandidateAvailReceive = ({
         <ShowCode.When
           isTrue={
             lastEvent &&
+            !isSlotConfirmed &&
             lastEvent.event_type === 'CAND_AVAIL_REC' &&
             Boolean(!eventTargetMap['onReceivingAvailReq'])
           }
