c3f88609518a96ecd51f98cc7390fdfd877a83ca
Dileep BC <dileep@aglinthq.com>
2024-08-28 14:40:53 +0530

    fix : link sent

--- apps/aglint-recruiter/src/components/Requests/RequestSections/Section/Request/RequestDetails/RequestProgress/Actions/SelfScheduleLinkSent.tsx
+++ apps/aglint-recruiter/src/components/Requests/RequestSections/Section/Request/RequestDetails/RequestProgress/Actions/SelfScheduleLinkSent.tsx
@@ -1,13 +1,22 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import { DatabaseTable } from '@aglint/shared-types';
 import { Stack } from '@mui/material';
+import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 
 import { ButtonGhost } from '@/devlink/ButtonGhost';
 import { GlobalIcon } from '@/devlink2/GlobalIcon';
 import { Text } from '@/devlink2/Text';
+import { useRequests } from '@/src/context/RequestsContext';
 
 const SelfScheduleLinkSent = (args: DatabaseTable['request_progress']) => {
+  const { query } = useRouter();
+  const {
+    requests: { data: requestList },
+  } = useRequests();
+  const selectedRequest = Object.values(requestList)
+    .flat()
+    .find((request) => request?.id === query?.id);
   const [isCopied, setIsCopied] = useState(false);
   return (
     <>
@@ -23,7 +32,7 @@ const SelfScheduleLinkSent = (args: DatabaseTable['request_progress']) => {
             onClick: () => {
               setIsCopied(true);
               navigator.clipboard.writeText(
-                `${process.env.NEXT_PUBLIC_HOST_NAME}/scheduling/invite/${'application_id'}?filter_id=${args.meta.filter_json_id}`,
+                `${process.env.NEXT_PUBLIC_HOST_NAME}/scheduling/invite/${selectedRequest.application_id}?filter_id=${args.meta.filter_json_id}`,
               );
               setTimeout(() => {
                 setIsCopied(false);
--- apps/aglint-recruiter/src/components/Requests/RequestSections/Section/Request/RequestDetails/RequestProgress/utils/ProgressActionMap.ts
+++ apps/aglint-recruiter/src/components/Requests/RequestSections/Section/Request/RequestDetails/RequestProgress/utils/ProgressActionMap.ts
@@ -1,7 +1,7 @@
 import { DatabaseTable } from '@aglint/shared-types';
 
+import FirstFollowUp from '../Actions/FirstFollowUp';
 import ScheduleFlows from '../Actions/Schedule';
-import SelfScheduleFollowUp from '../Actions/SelfScheduleFollowUp';
 import SelfScheduleLinkSent from '../Actions/SelfScheduleLinkSent';
 
 export const progressActionMap: Partial<
@@ -11,7 +11,7 @@ export const progressActionMap: Partial<
   >
 > = {
   CHOOSE_SCHEDULE_FLOW_not_started: ScheduleFlows,
-  SCHEDULE_FIRST_FOLLOWUP_SELF_SCHEDULE_completed: SelfScheduleFollowUp,
-  SCHEDULE_FIRST_FOLLOWUP_AVAILABILITY_LINK_completed: SelfScheduleFollowUp,
+  SCHEDULE_FIRST_FOLLOWUP_SELF_SCHEDULE_completed: FirstFollowUp,
+  SCHEDULE_FIRST_FOLLOWUP_AVAILABILITY_LINK_completed: FirstFollowUp,
   SELF_SCHEDULE_LINK_completed: SelfScheduleLinkSent,
 };
