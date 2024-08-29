cf0e433b25bceeb55e64e869f580d55ac064763e
Dileep BC <dileep@aglinthq.com>
2024-08-29 12:54:11 +0530

    fix: migrations

--- supabase/migrations/20240828132638_request_progress_updated_at.sql
+++
@@ -1,3 +0,0 @@
-alter table "public"."request_progress" add column "updated_at" timestamp with time zone not null default now();
-
-
--- supabase/migrations/20240829072221_progress_db_update.sql
+++ supabase/migrations/20240829072221_progress_db_update.sql
@@ -1,3 +1,6 @@
 CREATE TRIGGER event_trigger_request_insert AFTER INSERT ON public.request FOR EACH ROW EXECUTE FUNCTION call_webhook_on_change();
 
 
+alter table "public"."request_progress" add column "updated_at" timestamp with time zone not null default now();
+
+
