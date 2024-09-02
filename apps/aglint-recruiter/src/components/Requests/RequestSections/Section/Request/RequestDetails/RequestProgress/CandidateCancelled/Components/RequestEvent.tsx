e92b3d3b9d38db283e02e21e799694a2d55ddc2d
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-09-02 10:52:58 +0530

    refactor meeting cancel workflow

---
+++ apps/aglint-recruiter/src/components/Requests/RequestSections/Section/Request/RequestDetails/RequestProgress/CandidateCancelled/Components/RequestEvent.tsx
@@ -0,0 +1,63 @@
+import { CircularProgress } from '@mui/material';
+
+import { ButtonSolid } from '@/devlink2/ButtonSolid';
+import { ScheduleProgress } from '@/devlink2/ScheduleProgress';
+
+import { SlackIcon } from '../../../Components/SlackIcon';
+import { workflowCopy } from '../../utils/copy';
+import { EventProgressItem } from '../types';
+import { getWorkflowText } from '../utils';
+
+function RequestEvent({
+  eventType,
+  api,
+  requestProgress,
+  isManualFlow,
+  handleClick,
+}: EventProgressItem) {
+  const workflow = workflowCopy[eventType];
+  return (
+    <ScheduleProgress
+      slotAiText={null} // Placeholder for AI instructions if needed
+      isAiTextVisible={false}
+      slotLoader={
+        requestProgress?.status === 'in_progress' ? (
+          <CircularProgress size={20} />
+        ) : null
+      }
+      slotRequestIcon={
+        api === 'onRequestCancel_slack_interviewersOrganizer' ? (
+          <SlackIcon />
+        ) : (
+          ''
+        )
+      }
+      slotHoverIcon={''}
+      status={requestProgress?.status ?? null}
+      textProgress={getWorkflowText({
+        workflow,
+        status: requestProgress?.status,
+      })}
+      slotButton={
+        isManualFlow &&
+        requestProgress?.status !== 'completed' && (
+          <ButtonSolid
+            textButton={
+              api === 'onRequestCancel_agent_cancelEvents'
+                ? 'Cancel'
+                : api === 'onRequestCancel_slack_interviewersOrganizer'
+                  ? 'Send slack'
+                  : ''
+            }
+            onClickButton={{
+              onClick: handleClick,
+            }}
+            size={1}
+          />
+        )
+      }
+    />
+  );
+}
+
+export default RequestEvent;
---
+++ apps/aglint-recruiter/src/components/Requests/RequestSections/Section/Request/RequestDetails/RequestProgress/CandidateCancelled/Components/RequestProgressWrapper.tsx
@@ -0,0 +1,38 @@
+import { RequestProgress } from '@/devlink2/RequestProgress';
+
+import { EventProgressItem, ProgressDataItem } from '../types';
+import RequestEvent from './RequestEvent';
+
+type EventProgress = EventProgressItem[];
+
+// Type for the entire progressData array
+type ProgressData = ProgressDataItem[];
+
+function RequestProgressWrapper({
+  progressData,
+  EventProgress,
+}: {
+  progressData: ProgressData;
+  EventProgress: EventProgress;
+}) {
+  return progressData.map((progress, index) => (
+    <RequestProgress
+      key={index}
+      isDividerVisible={progress.isDividerVisible}
+      indicator={progress.indicator}
+      circleIndicator={progress.circleIndicator}
+      slotIndicator={progress.slotIndicator}
+      textRequestProgress={progress.textRequestProgress}
+      slotProgress={
+        <>
+          {EventProgress.map((event, i) => (
+            <RequestEvent key={i} {...event} />
+          ))}
+          {progress.addActionButton}
+        </>
+      }
+    />
+  ));
+}
+
+export default RequestProgressWrapper;
