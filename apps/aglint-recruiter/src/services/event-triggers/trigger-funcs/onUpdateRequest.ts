7ed22d97908998d157e0580710ceedce800c44e0
Dileep BC <dileep@aglinthq.com>
2024-09-02 18:14:27 +0530

    fix : event triggers

--- apps/aglint-recruiter/src/services/event-triggers/trigger-funcs/onUpdateRequest.ts
+++
@@ -1,9 +0,0 @@
-import { DatabaseTable } from '@aglint/shared-types';
-
-export const onUpdateRequest = async ({
-  new_data,
-  old_data,
-}: {
-  old_data: DatabaseTable['request'];
-  new_data: DatabaseTable['request'];
-}) => {};
