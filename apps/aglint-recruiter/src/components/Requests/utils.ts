a0e67d403add4a7893e29f9374822715ae1eb452
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-09-03 14:18:56 +0530

    fix: request list and dashboard page

--- apps/aglint-recruiter/src/components/Requests/utils.ts
+++ apps/aglint-recruiter/src/components/Requests/utils.ts
@@ -1,17 +1,5 @@
 import { Request } from '@/src/queries/requests/types';
 
-export function formatSessionsName(sessions: string[]) {
-  if (sessions.length === 0) {
-    return '';
-  } else if (sessions.length === 1) {
-    return sessions[0];
-  } else {
-    return (
-      sessions.slice(0, -1).join(', ') + ' and ' + sessions[sessions.length - 1]
-    );
-  }
-}
-
 export function getStatusColor({ status }: { status: Request['status'] }) {
   return status === 'to_do'
     ? 'purple'
@@ -22,4 +10,16 @@ export function getStatusColor({ status }: { status: Request['status'] }) {
         : status === 'completed'
           ? 'success'
           : 'neutral';
+}
+
+export function formatSessionsName(sessions: string[]) {
+  if (sessions.length === 0) {
+    return '';
+  } else if (sessions.length === 1) {
+    return sessions[0];
+  } else {
+    return (
+      sessions.slice(0, -1).join(', ') + ' and ' + sessions[sessions.length - 1]
+    );
+  }
 }
