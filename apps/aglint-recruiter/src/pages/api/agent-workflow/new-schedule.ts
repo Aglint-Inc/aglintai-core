4fd7e4b219c137511150ddf0944d05544af82ff8
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-09-09 11:18:09 +0530

    fix: removed request section component

--- apps/aglint-recruiter/src/pages/api/agent-workflow/new-schedule.ts
+++ apps/aglint-recruiter/src/pages/api/agent-workflow/new-schedule.ts
@@ -11,7 +11,7 @@ import { dayjsLocal } from '@aglint/shared-utils/src/scheduling/dayjsLocal';
 import { type NextApiRequest, type NextApiResponse } from 'next';
 import * as v from 'valibot';
 
-import { apiTargetToEvents } from '@/components/Requests/RequestSections/Section/Request/RequestDetails/RequestProgress/utils/progressMaps';
+import { apiTargetToEvents } from '@/components/Requests/RequestSections/RequestCard/RequestDetails/RequestProgress/utils/progressMaps';
 import { candidateAvailRequest } from '@/services/api-schedulings/candidateAvailRequest';
 import { candidateAvailReRequest } from '@/services/api-schedulings/candidateAvailReRequest';
 import { candidateSelfSchedule } from '@/services/api-schedulings/candidateSelfSchedule';
