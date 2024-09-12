daf6e8cad50d7dfea6879aae7fd39afd5256adf1
ChandraAglint <chandra@aglinthq.com>
2024-09-12 15:12:50 +0530

    CandidateBasicInfo

--- apps/aglint-recruiter/src/app/(authenticated)/_jobs/[job]/_common/components/CandidateDrawer/Meta.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/_jobs/[job]/_common/components/CandidateDrawer/Meta.tsx
@@ -1,5 +1,4 @@
-import { CandidateBasicInfo } from '@devlink/CandidateBasicInfo';
-
+import UITypography from '@/components/Common/UITypography';
 import { useApplication } from '@/context/ApplicationContext';
 
 const Meta = () => {
@@ -8,12 +7,37 @@ const Meta = () => {
   } = useApplication();
   if (status === 'pending') return <></>;
   return (
-    <CandidateBasicInfo
-      textLocation={data.city ?? '---'}
-      textMail={data.email ?? '---'}
-      textPhone={data.phone ?? '---'}
-      textRole={data.current_job_title ?? '---'}
-    />
+    <div className='grid h-16 grid-cols-2 gap-1'>
+      {/* Tailwind grid styles */}
+      <div className='flex items-center gap-1'>
+        <div>
+          <UITypography variant='p' type='small'>
+            {data.current_job_title ?? '---'}
+          </UITypography>
+        </div>
+      </div>
+      <div className='flex items-center gap-1'>
+        <div>
+          <UITypography variant='p' type='small'>
+            {data.city ?? '---'}
+          </UITypography>
+        </div>
+      </div>
+      <div className='flex items-center gap-1'>
+        <div>
+          <UITypography variant='p' type='small'>
+            {data.email ?? '---'}
+          </UITypography>
+        </div>
+      </div>
+      <div className='flex items-center gap-1'>
+        <div>
+          <UITypography variant='p' type='small'>
+            {data.phone ?? '---'}
+          </UITypography>
+        </div>
+      </div>
+    </div>
   );
 };
 
