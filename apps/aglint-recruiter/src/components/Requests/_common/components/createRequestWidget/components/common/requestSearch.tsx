716fcadf66e7ac4917fcb86394af9a8ce77da0f5
Punith G <punithg@aglinthq.com>
2024-09-13 21:17:32 +0530

    disable focus

--- apps/aglint-recruiter/src/components/Requests/_common/components/createRequestWidget/components/common/requestSearch.tsx
+++ apps/aglint-recruiter/src/components/Requests/_common/components/createRequestWidget/components/common/requestSearch.tsx
@@ -32,7 +32,7 @@ const Component = <T extends string>(
   return (
     <div className='flex items-center border-b px-3'>
       <SearchIcon className='mr-2 h-4 w-4 shrink-0 opacity-50' />
-      <Input
+      <input
         ref={ref}
         className='flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50'
         placeholder={placeholder}
