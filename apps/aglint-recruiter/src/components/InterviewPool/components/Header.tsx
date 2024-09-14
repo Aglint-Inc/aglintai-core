8124112da66161c6cefdd18dfe61b75ff033bde1
ChandraAglint <chandra@aglinthq.com>
2024-09-15 00:58:10 +0530

    added module added

--- apps/aglint-recruiter/src/components/InterviewPool/components/Header.tsx
+++ apps/aglint-recruiter/src/components/InterviewPool/components/Header.tsx
@@ -1,8 +1,12 @@
+import { Plus } from 'lucide-react';
 import { type Dispatch, type SetStateAction } from 'react';
 
 import FilterHeader from '@/components/Common/FilterHeader';
 import { UIButton } from '@/components/Common/UIButton';
 import UITextField from '@/components/Common/UITextField';
+import CreateModuleDialog from '@/components/Scheduling/InterviewTypes/CreateModuleDialog';
+import { setIsCreateDialogOpen } from '@/components/Scheduling/InterviewTypes/store';
+import { useRolesAndPermissions } from '@/context/RolesAndPermissions/RolesAndPermissionsContext';
 import { useAllDepartments } from '@/queries/departments';
 
 export const Header = ({
@@ -13,6 +17,7 @@ export const Header = ({
   setDepartments,
 }: HeaderProps) => {
   const { data: departments } = useAllDepartments();
+  const { checkPermissions } = useRolesAndPermissions();
 
   const departmentList = departments?.length
     ? departments.map((dep) => ({ label: dep.name, id: dep.id.toString() }))
@@ -24,38 +29,53 @@ export const Header = ({
   };
 
   return (
-    <div className='mb-6 flex justify-between items-center'>
-      <UITextField
-        placeholder='Search interviewers...'
-        fieldSize='medium'
-        value={searchText}
-        onChange={(e) => setSearchText(e.target.value)}
-        className='w-[250px]'
-      />
-      <div className='flex items-center gap-2'>
-        {isFilterApplied ? (
-          <UIButton size='sm' onClick={resetAllFilter}>
-            Reset all
-          </UIButton>
-        ) : (
-          <></>
-        )}
-        <FilterHeader
-          filters={[
-            {
-              type: 'filter',
-              name: 'Department',
-              value: selectedDepartments,
-              options: departmentList,
-              multiSelect: true,
-              setValue: (value) => {
-                setDepartments(value);
-              },
-            },
-          ]}
+    <>
+      <CreateModuleDialog />
+      <div className='mb-6 flex justify-between items-center'>
+        <UITextField
+          placeholder='Search interviewers...'
+          fieldSize='medium'
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          className='w-[250px]'
         />
+        <div className='flex items-center gap-2'>
+          {isFilterApplied ? (
+            <UIButton size='sm' onClick={resetAllFilter}>
+              Reset all
+            </UIButton>
+          ) : (
+            <></>
+          )}
+          <FilterHeader
+            filters={[
+              {
+                type: 'filter',
+                name: 'Department',
+                value: selectedDepartments,
+                options: departmentList,
+                multiSelect: true,
+                setValue: (value) => {
+                  setDepartments(value);
+                },
+              },
+            ]}
+          />
+          {checkPermissions(['interview_types']) && (
+            <UIButton
+              variant='default'
+              size='sm'
+              onClick={() => {
+                setIsCreateDialogOpen(true);
+              }}
+              leftIcon={<Plus />}
+            >
+              Interview Type
+            </UIButton>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
