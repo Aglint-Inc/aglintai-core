aa3006005b2d9a05b9a131706c008b064ba8e9bd
ChandraAglint <chandra@aglinthq.com>
2024-09-17 22:03:35 +0530

    company role fix

--- apps/aglint-recruiter/src/components/CompanyDetailComp/SideNav/utils.ts
+++ apps/aglint-recruiter/src/components/CompanyDetailComp/SideNav/utils.ts
@@ -1,5 +1,3 @@
-import { type permissionsEnum } from '@aglint/shared-types/src/db/tables/permissions/type';
-
 export const hoursList = Array.from({ length: 40 }, (_, index) => index + 1);
 
 export const settingSubNavItem = {
@@ -17,69 +15,3 @@ export const settingSubNavItem = {
   AGENTTEMPLATE: 'agentTemplate',
   PORTAL_SETTINGS: 'portalSetttings',
 };
-
-export const settingsItems = (show: boolean) =>
-  [
-    {
-      label: 'Company Details',
-      value: settingSubNavItem['COMPANYINFO'],
-      icon: 'Building',
-      show: true,
-    },
-    {
-      label: 'Working Hours',
-      value: settingSubNavItem['WORKINGHOURS'],
-      icon: 'Clock',
-      show: true,
-    },
-    {
-      label: 'Holidays',
-      value: settingSubNavItem['HOLIDAYS'],
-      icon: 'Calendar',
-      show: true,
-    },
-    {
-      label: 'User',
-      value: settingSubNavItem['USERS'],
-      permission: 'view_users',
-      icon: 'Users',
-      show: true,
-    },
-    {
-      label: 'Roles',
-      value: settingSubNavItem['ROLES'],
-      permission: 'view_roles',
-      icon: 'Shield',
-      show: show,
-    },
-    {
-      label: 'Templates',
-      value: settingSubNavItem['EMAILTEMPLATE'],
-      icon: 'FileText',
-      show: show,
-    },
-    {
-      label: 'Scheduling',
-      value: settingSubNavItem['SCHEDULING'],
-      icon: 'CalendarDays',
-      show: show,
-    },
-    {
-      label: 'Reasons',
-      value: settingSubNavItem['SCHEDULING_REASONS'],
-      icon: 'List',
-      show: show,
-    },
-    {
-      label: 'Candidate Portal',
-      value: settingSubNavItem['PORTAL_SETTINGS'],
-      icon: 'Globe',
-      show: show,
-    },
-  ] as {
-    label: string;
-    value: string;
-    permission?: permissionsEnum | 'authorized';
-    icon: string;
-    show: boolean;
-  }[];
