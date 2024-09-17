e4203701b1dd7521fe1dfb571456aeb53211e4ed
chinmaicraglint <125248276+chinmaicraglint@users.noreply.github.com>
2024-09-17 11:58:41 +0530

    Fix/logout prod (#2286)
    * dev merge to main (#2284)
    
    * accordion fix
    
    * tab fix
    
    * default image fix
    
    * Table fix
    
    * script: fix Json type
    
    * img removed
    
    * fix: company logo uploading issue
    
    * Added griper and fixed lint
    
    * fix: issue fixed in isShowFeature flag
    
    * company setting fix
    
    * fix: nested filter (wong op checked, search placeholder )
    
    * fix: nested filter (wong op checked, search placeholder )
    
    * remove script js files
    
    * Recruiter preferences ats (#2273)
    
    recruiter_preferences-ats
    
    * Minor ui fixes
    
    * fix
    
    * fix: hide create request from candidate list
    
    * Refactor/move slack (#2275)
    
    * fix : slack routes
    
    * fix : interaction
    
    * fix : interviewStart_slack_interviewers
    
    * WIP
    
    * fix : interviewEnd_slack_shadowTraineeForMeetingAttendence
    
    * WIP
    
    * fix: candidateBook_slack_interviewerForConfirmation
    
    * WIP
    
    * fix : WIP
    
    * fix : schema
    
    * fix
    
    * fix : route
    
    * fix : candidateBook_slack_interviewerForConfirmation
    
    * candidateBook_slack_interviewerForConfirmation
    
    * fix : slack
    
    * fix : slack
    
    * fix: remove files
    
    * fix :
    
    * api fix invite user
    
    * fix (#2276)
    
    * fix: adding dayoff issue
    
    * fix: toast issue
    
    * fix: move to interview text change
    
    * fix
    
    * fixes
    
    * fix: updated upload csv drag area
    
    * fix: updated upload csv drag area
    
    * Added top bar nav fix to auth context
    
    * fix: sort
    
    * fix: conditions
    
    * Added conditioanl hiding of the profile and create job design fix
    
    * ats job sync
    
    * fix lever candidate sync api
    
    * job sync visibility
    
    * Application Detail UI fix
    
    * Added min height for all screens
    
    * fail_pro_app
    
    ---------
    
    Co-authored-by: ChandraAglint <chandra@aglinthq.com>
    Co-authored-by: Ravi K <ravi@aglinthq.com>
    Co-authored-by: VivekS <vivek@aglinthq.com>
    Co-authored-by: Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
    Co-authored-by: Punith G <91651608+MlNl-PEKKA@users.noreply.github.com>
    Co-authored-by: Dileep B C <135205237+dileepbc3141@users.noreply.github.com>
    Co-authored-by: Punith G <punithg@aglinthq.com>
    
    * fix logout and user image
    
    ---------
    
    Co-authored-by: ChandraAglint <chandra@aglinthq.com>
    Co-authored-by: Ravi K <ravi@aglinthq.com>
    Co-authored-by: VivekS <vivek@aglinthq.com>
    Co-authored-by: Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
    Co-authored-by: Punith G <91651608+MlNl-PEKKA@users.noreply.github.com>
    Co-authored-by: Dileep B C <135205237+dileepbc3141@users.noreply.github.com>
    Co-authored-by: Punith G <punithg@aglinthq.com>

--- apps/aglint-recruiter/src/components/UserDetail/index.tsx
+++ apps/aglint-recruiter/src/components/UserDetail/index.tsx
@@ -63,7 +63,7 @@ export default function InterviewerDetailsPage() {
 
   if (isLoading)
     return (
-      <div className='flex items-center justify-center w-full h-full'>
+      <div className='flex items-center justify-center w-full min-h-screen'>
         <Loader />
       </div>
     );
