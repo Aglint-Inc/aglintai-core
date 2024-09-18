8c28dfba484404ed2d435e0f6e46f18fffdf36d2
Ravi K <ravi@aglinthq.com>
2024-09-18 10:32:19 +0530

    Updaed layout

--- apps/aglint-recruiter/src/app/(authenticated)/_jobs/[job]/hiring-team/_common/components/index.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/_jobs/[job]/hiring-team/_common/components/index.tsx
@@ -111,10 +111,10 @@ const JobEdit = () => {
       </div>
 
       <div className='mb-6 flex gap-6'>
-        <div className='w-1/4'>
+        <div className='w-3/12'>
           <JobsSideNavV2 />
         </div>
-        <div className='w-3/4'>
+        <div className='w-9/12'>
           <div className='flex flex-row justify-between'>
             <div className='flex flex-col gap-2'>
               <h2 className='mb-2 text-xl font-bold'>Hiring Team</h2>
--- apps/aglint-recruiter/src/app/(authenticated)/_jobs/[job]/interview-plan/_common/components/index.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/_jobs/[job]/interview-plan/_common/components/index.tsx
@@ -129,10 +129,10 @@ const InterviewPlanPage = () => {
         </div>
 
         <div className='mb-6 flex gap-6'>
-          <div className='w-1/4'>
+          <div className='w-2/12'>
             <JobsSideNavV2 />
           </div>
-          <div className='w-3/4'>
+          <div className='w-9/12'>
             <div className='flex flex-row justify-between'>
               <div className='flex flex-col gap-2'>
                 <h2 className='mb-2 text-xl font-bold'>Interview Plan</h2>
