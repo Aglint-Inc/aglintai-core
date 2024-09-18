98d3e494f5d39eb6ff9430e4928eebc1ed75f9b1
Punith G <punithg@aglinthq.com>
2024-09-18 14:34:27 +0530

    applicationStoreContext setup

--- apps/aglint-recruiter/src/app/(authenticated)/_jobs/[job]/_common/contexts/applicationsStoreContext.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/_jobs/[job]/_common/contexts/applicationsStoreContext.tsx
@@ -5,7 +5,7 @@
  * @link https://tkdodo.eu/blog/zustand-and-react-context
  *
  */
-import type { DatabaseTable } from '@aglint/shared-types';
+import type { DatabaseView } from '@aglint/shared-types';
 import { createContext, memo, type PropsWithChildren, useState } from 'react';
 import { createStore } from 'zustand';
 
@@ -15,33 +15,80 @@ import {
   getContextStoreInitial,
 } from '@/utils/zustandContextHelpers';
 
-type Checklist = DatabaseTable['applications']['id'][];
+type Application = DatabaseView['application_view'];
 
 type Locations = ReturnType<typeof nestedObjectToArray>;
 
 type Stages = ReturnType<typeof nestedObjectToArray>;
 
-type ActionPopup = DatabaseTable['applications']['status'];
+type Checklist = Application['id'][];
 
-type States = {
-  checklist: Checklist;
-  importPopup: boolean;
-  actionPopup: ActionPopup;
+type ActionPopup = Application['status'];
+
+type Filters = {
+  badges: (keyof Application['badges'])[];
+  bookmarked: boolean;
   locations: Locations;
+  resumeMatch: Application['application_match'][];
+  search: Application['name'];
+  section: Application['status'];
   stages: Stages;
 };
 
-const initial: States = Object.freeze({
-  actionPopup: null,
+const initialFilter: Filters = Object.freeze({
+  badges: [],
+  bookmarked: false,
+  locations: [],
+  resumeMatch: [],
+  search: '',
+  section: 'new',
+  stages: [],
+});
+
+type Sort = {
+  order: 'asc' | 'desc';
+  type:
+    | keyof Pick<Application, 'applied_at' | 'name' | 'latest_activity'>
+    | 'location'
+    | 'resume_match';
+};
+
+const initialSort: Sort = Object.freeze({
+  order: 'desc',
+  type: 'latest_activity',
+});
+
+type Misc = {
+  actionPopup: ActionPopup;
+  checklist: Checklist;
+  importPopup: boolean;
+};
+
+const initialMisc: Misc = Object.freeze({
+  actionPopup: 'new',
   checklist: [],
   importPopup: false,
-  locations: [],
-  stages: [],
+});
+
+type States = Filters & Sort & Misc;
+
+const initial: States = Object.freeze({
+  ...initialFilter,
+  ...initialSort,
+  ...initialMisc,
 });
 
 const get = getContextStoreInitial(initial);
 
-type Store = CreateContextStore<States>;
+type Actions = {
+  handleBadge: (badge: Filters['badges'][number]) => void;
+  handleBookmarked: () => void;
+  handleResumeMatch: (resumeMatch: Filters['resumeMatch'][number]) => void;
+  handleChecklist: (id: Misc['checklist'][number]) => void;
+  handleImportPopup: () => void;
+};
+
+type Store = CreateContextStore<States, Actions>;
 
 const useApplicationsStoreContext = () => {
   const [store] = useState(
@@ -49,18 +96,67 @@ const useApplicationsStoreContext = () => {
       initial,
       ...get(),
       actions: {
+        handleBadge: (badge) =>
+          set((state) => {
+            if (state.badges.includes(badge))
+              return {
+                badges: state.badges.filter((b) => b !== badge),
+              };
+            return {
+              badges: [...state.badges, badge],
+            };
+          }),
+        setBadges: (badges) => set(() => ({ badges })),
+        resetBadges: () => set(() => ({ badges: get('badges') })),
+        handleBookmarked: () =>
+          set((state) => ({ bookmarked: !state.bookmarked })),
+        setBookmarked: (bookmarked) => set(() => ({ bookmarked })),
+        resetBookmarked: () => set(() => ({ bookmarked: get('bookmarked') })),
+        setLocations: (locations) => set(() => ({ locations })),
+        resetLocations: () => set(() => ({ locations: get('locations') })),
+        handleResumeMatch: (resumeMatch) =>
+          set((state) => {
+            if (state.resumeMatch.includes(resumeMatch))
+              return {
+                resumeMatch: state.resumeMatch.filter((b) => b !== resumeMatch),
+              };
+            return {
+              resumeMatch: [...state.resumeMatch, resumeMatch],
+            };
+          }),
+        setResumeMatch: (resumeMatch) => set(() => ({ resumeMatch })),
+        resetResumeMatch: () =>
+          set(() => ({ resumeMatch: get('resumeMatch') })),
+        setSearch: (search) => set(() => ({ search })),
+        resetSearch: () => set(() => ({ search: get('search') })),
+        setSection: (section) => set(() => ({ section })),
+        resetSection: () => set(() => ({ section: get('section') })),
+        setStages: (stages) => set(() => ({ stages })),
+        resetStages: () => set(() => ({ stages: get('stages') })),
+        setOrder: (order) => set(() => ({ order })),
+        resetOrder: () => set(() => ({ order: get('order') })),
+        setType: (type) => set(() => ({ type })),
+        resetType: () => set(() => ({ type: get('type') })),
         setActionPopup: (actionPopup) => set(() => ({ actionPopup })),
         resetActionPopup: () =>
           set(() => ({ actionPopup: get('actionPopup') })),
+        handleChecklist: (id) =>
+          set((state) => {
+            if (state.checklist.includes(id))
+              return {
+                checklist: state.checklist.filter((b) => b !== id),
+              };
+            return {
+              checklist: [...state.checklist, id],
+            };
+          }),
         setChecklist: (checklist) => set(() => ({ checklist })),
         resetChecklist: () => set(() => ({ checklist: get('checklist') })),
+        handleImportPopup: () =>
+          set((state) => ({ importPopup: !state.importPopup })),
         setImportPopup: (importPopup) => set(() => ({ importPopup })),
         resetImportPopup: () =>
           set(() => ({ importPopup: get('importPopup') })),
-        setLocations: (locations) => set(() => ({ locations })),
-        resetLocations: () => set(() => ({ locations: get('locations') })),
-        setStages: (stages) => set(() => ({ stages })),
-        resetStages: () => set(() => ({ stages: get('stages') })),
       },
     })),
   );
