f679c42cc32084c7cb723e93b38fe6ef1ab4c226
Ravi K <ravi@aglinthq.com>
2024-09-19 08:24:02 +0530

    Added progress indicator

--- apps/aglint-recruiter/src/components/Scheduling/Common/StageProgress/index.tsx
+++ apps/aglint-recruiter/src/components/Scheduling/Common/StageProgress/index.tsx
@@ -4,36 +4,33 @@ import { type Application } from '@/types/applications.types';
 
 import StageProgressPill, { getPlanPillProps } from './stageProgressPill';
 
-export type StageProgressProps = Pick<Application, 'interview_plans'>;
+export type StageProgressProps = Pick<Application, 'interview_plans'> & {
+  currentStep: number;
+};
 
 const StageProgress = forwardRef(
   (props: StageProgressProps, ref: Ref<HTMLDivElement>) => {
-    const count = props.interview_plans.length;
-    const pills = getPlanPillProps(props.interview_plans).map(
-      (pillProps, index) => (
-        <StageProgressPill
-          key={index}
-          position={
-            count === 1
-              ? 'lone'
-              : index === 0
-                ? 'starting'
-                : index === count - 1
-                  ? 'ending'
-                  : 'middle'
-          }
-          pillProps={pillProps}
-        />
-      ),
-    );
+    const { interview_plans = [], currentStep } = props;
+    const count = interview_plans.length;
+
+    const pills = getPlanPillProps(interview_plans).map((pillProps, index) => (
+      <StageProgressPill
+        key={index}
+        pillProps={pillProps}
+        isLast={index === count - 1}
+        isActive={index + 1 === currentStep}
+      />
+    ));
+
     return (
-      <div ref={ref} className='flex flex-col'>
-        <div className={'flex flex-row flex-nowrap gap-2'}>
-          {pills.length ? pills : '---'}
+      <div ref={ref} className='flex flex-col space-y-2'>
+        <div className='flex flex-row items-center overflow-hidden rounded-md shadow-sm'>
+          {pills.length ? pills : <div className='bg-gray-100 p-2'>---</div>}
         </div>
       </div>
     );
   },
 );
+
 StageProgress.displayName = 'StageProgress';
 export default StageProgress;
