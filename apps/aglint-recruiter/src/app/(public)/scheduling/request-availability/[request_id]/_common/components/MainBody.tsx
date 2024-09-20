0e5c718869b12a6f458fb31f3ef2233a327a5d17
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-09-20 11:49:48 +0530

    fix: error in availability page

--- apps/aglint-recruiter/src/app/(public)/scheduling/request-availability/[request_id]/_common/components/MainBody.tsx
+++ apps/aglint-recruiter/src/app/(public)/scheduling/request-availability/[request_id]/_common/components/MainBody.tsx
@@ -8,7 +8,6 @@ import { ConfirmedInvitePage } from 'src/app/(public)/scheduling/invite/[id]/_co
 import Footer from '@/components/Common/Footer';
 import { Loader } from '@/components/Common/Loader';
 import MuiAvatar from '@/components/Common/MuiAvatar';
-import { useRouterPro } from '@/hooks/useRouterPro';
 import timeZones from '@/utils/timeZone';
 
 import { useRequestAvailabilityContext } from '../contexts/RequestAvailabilityContext';
@@ -17,16 +16,11 @@ import SingleDaySessions from './SingleDaySessions';
 import SlotsSubmitted from './SlotsSubmitted';
 
 function CandidateAvailability() {
-  const router = useRouterPro();
   const {
     multiDaySessions,
     candidateRequestAvailability,
     loading,
     isSubmitted,
-    setIsSubmitted,
-    setCandidateRequestAvailability,
-    setDateSlots,
-    setDaySlots,
   } = useRequestAvailabilityContext();
   const [meetingsAndRounds, setMeetingsAndRound] = useState<{
     rounds: any[];
@@ -80,34 +74,15 @@ function CandidateAvailability() {
     return timeZones.find(({ tzCode }) => tzCode === tz);
   }, []);
 
-  const checkAndUpdate = async () => {
-    if (!candidateRequestAvailability.booking_confirmed) {
-      if (candidateRequestAvailability.slots) {
-        setIsSubmitted(true);
-        setDateSlots(candidateRequestAvailability.slots);
-        setDaySlots(candidateRequestAvailability.slots);
-      } else {
-        if (!candidateRequestAvailability.visited) {
-          const { data: requestData } = await axios.post(
-            `/api/scheduling/request_availability/updateRequestAvailability`,
-            {
-              id: String(router.params?.request_id),
-              data: { visited: true },
-            },
-          );
-          setCandidateRequestAvailability(requestData);
-        }
-      }
-    } else {
+  useEffect(() => {
+    if (
+      candidateRequestAvailability &&
+      candidateRequestAvailability.booking_confirmed
+    ) {
       getMeetings(
         interview_sessions.map((ele) => ele.id),
         candidateRequestAvailability.application_id,
       );
-    }
-  };
-  useEffect(() => {
-    if (candidateRequestAvailability?.id) {
-      checkAndUpdate();
     }
   }, [candidateRequestAvailability]);
   if (
