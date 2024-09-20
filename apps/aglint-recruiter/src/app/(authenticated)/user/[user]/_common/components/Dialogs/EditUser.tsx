727e04750552998a57c4715cb8e9e9374cf083a3
Punith G <punithg@aglinthq.com>
2024-09-20 15:53:46 +0530

    user-app-router

---
+++ apps/aglint-recruiter/src/app/(authenticated)/user/[user]/_common/components/Dialogs/EditUser.tsx
@@ -0,0 +1,58 @@
+import { getFullName } from '@aglint/shared-utils';
+
+import { useTeamMembers } from '@/company/components/TeamManagement';
+import EditMember from '@/company/components/TeamManagement/EditMemberDialog';
+import { useAuthDetails } from '@/context/AuthContext/AuthContext';
+import { useMemberList } from '@/hooks/useMemberList';
+import { useRouterPro } from '@/hooks/useRouterPro';
+import ROUTES from '@/utils/routing/routes';
+
+import { useInterviewer } from '../../hooks/useInterviewer';
+import { EditProfileDialog } from './EditProfileDialog';
+
+export const EditUserDialog = ({ isOpen, setIsOpen }) => {
+  const router = useRouterPro();
+  const { recruiterUser } = useAuthDetails();
+  const { activeMembers } = useTeamMembers();
+  const user_id = router.params.user as string;
+  const { refetch: interviewerDetailsRefetch } = useInterviewer();
+
+  const { data: members, refetch: memberListRefetch } = useMemberList();
+  const details = members?.find((member) => member.user_id === user_id);
+
+  return (
+    <>
+      {recruiterUser.role === 'admin' ? (
+        <EditMember
+          open={Boolean(isOpen)}
+          memberList={activeMembers
+            .map((mem) => ({
+              id: mem.user_id,
+              name: getFullName(mem.first_name, mem?.last_name),
+            }))
+            .filter((mem) => mem.id !== recruiterUser.user_id)}
+          member={details}
+          refetch={async () => {
+            await interviewerDetailsRefetch();
+            await memberListRefetch();
+          }}
+          onClose={() => {
+            setIsOpen(null);
+            //remove query param
+            router.push(
+              ROUTES['/user/[user]']({
+                user_id: user_id,
+              }),
+            );
+          }}
+        />
+      ) : (
+        <EditProfileDialog
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          interviewerDetailsRefetch={interviewerDetailsRefetch}
+        />
+      )}
+    </>
+  );
+};
---
+++ apps/aglint-recruiter/src/app/(authenticated)/user/[user]/_common/hooks/useInterviewer.ts
@@ -0,0 +1,10 @@
+import { useRouterPro } from '@/hooks/useRouterPro';
+import { api } from '@/trpc/client';
+export type InterviewerDetailType = Awaited<
+  ReturnType<typeof useInterviewer>
+>['data'];
+export const useInterviewer = () => {
+  const router = useRouterPro();
+  const user_id = router.params.user as string;
+  return api.interviewers.get_user_details.useQuery({ user_id });
+};
