885f8c5b28794c5941e6f28452a0764ad5dc4596
VivekS <vivek@aglinthq.com>
2024-09-20 12:00:53 +0530

    fix: integrations migrate to app

---
+++ apps/aglint-recruiter/src/pages/_integrations/integrations/index.tsx
@@ -0,0 +1,16 @@
+import Seo from '@/components/Common/Seo';
+import Integrations from '@/components/Integrations';
+
+function IntegrationsPage() {
+  return (
+    <>
+      <Seo
+        title={`Integrations | Aglint AI`}
+        description='AI for People Products'
+      />
+      <Integrations />
+    </>
+  );
+}
+
+export default IntegrationsPage;
