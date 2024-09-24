796b0745657175de03dfa875d79a89d4259f56aa
VivekS <vivek@aglinthq.com>
2024-09-24 17:59:42 +0530

    fix: remove

--- apps/aglint-apis/src/controllers/scoring/config.ts
+++ apps/aglint-apis/src/controllers/scoring/config.ts
@@ -1,18 +1,11 @@
-import {createClient} from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import {supabaseAdmin} from 'src/services/supabase/SupabaseAdmin';
 
 dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error(
-    'SUPABASE_URL and SUPABASE_SERVICE_ROLE_KEY environment variables are required.'
-  );
-}
 if (!OPENAI_API_KEY) {
   throw new Error('OPENAI_API_KEY environment variables are required.');
 }
@@ -21,4 +14,4 @@ export const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
 });
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = supabaseAdmin;
