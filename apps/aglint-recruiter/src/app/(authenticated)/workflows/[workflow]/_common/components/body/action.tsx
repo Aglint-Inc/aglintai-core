a16d4738740ab3ffa6ce3b92f54bccf0ab5ca291
Ravi K <ravi@aglinthq.com>
2024-09-25 17:46:56 +0530

    Recorginize the public assets

--- apps/aglint-recruiter/src/app/(authenticated)/workflows/[workflow]/_common/components/body/action.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/workflows/[workflow]/_common/components/body/action.tsx
@@ -330,7 +330,7 @@ const WorkflowConnector = () => {
   return (
     <div className={'flex w-full items-center justify-center text-neutral-600'}>
       <Image
-        src='/images/workflows/connector.svg'
+        src='/images/workflow/connector.svg'
         alt='Workflow Connector'
         width={16}
         height={45}
