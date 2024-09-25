b5ae6dc98fa7d813f1870bb2585ee596bab12aa7
ChandraAglint <chandra@aglinthq.com>
2024-09-25 17:44:34 +0530

    Fix/request landing progress added (#2432)
    * landing page added on request page
    
    * lint fix

--- apps/aglint-recruiter/src/server/api/routers/requests/index.ts
+++ apps/aglint-recruiter/src/server/api/routers/requests/index.ts
@@ -1,6 +1,8 @@
 import { createTRPCRouter } from '../../trpc';
 import { create } from './create';
+import { setup } from './setup';
 
 export const requests = createTRPCRouter({
   create,
+  setup,
 });
