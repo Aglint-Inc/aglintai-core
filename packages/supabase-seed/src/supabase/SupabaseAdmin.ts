5600dd33b8907bee7a8affb7b62913a6242e88b1
Dileep BC <dileep@aglinthq.com>
2024-09-26 10:40:54 +0530

    fix: remove supabase-seed package

--- packages/supabase-seed/src/supabase/SupabaseAdmin.ts
+++
@@ -1,8 +0,0 @@
-import {DB} from '@aglint/shared-types';
-import {envConfig} from 'src/config';
-import {createClient} from '@supabase/supabase-js';
-const supabase = createClient<DB>(
-  envConfig.SUPABASE_URL,
-  envConfig.SUPABASE_SERVICE_ROLE_KEY
-);
-export const supabaseAdmin = supabase;
