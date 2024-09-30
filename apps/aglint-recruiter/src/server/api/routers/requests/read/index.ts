4a6a5e4435010f7f5d7a6d4e20487cf199cb4cce
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-09-30 12:58:12 +0530

    fix: completed request api

--- apps/aglint-recruiter/src/server/api/routers/requests/read/index.ts
+++ apps/aglint-recruiter/src/server/api/routers/requests/read/index.ts
@@ -1,7 +1,9 @@
 import { createTRPCRouter } from '@/server/api/trpc';
 
 import { applicantRequest } from './applicantRequest';
+import { completedRequest } from './completedRequest';
 
 export const read = createTRPCRouter({
   applicantRequest,
+  completedRequest,
 });
