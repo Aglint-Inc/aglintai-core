83ec9e04784ec0dd0a2a79b3cda07f695008c2be
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-09-30 13:26:12 +0530

    fix:request count api

--- apps/aglint-recruiter/src/server/api/routers/requests/read/index.ts
+++ apps/aglint-recruiter/src/server/api/routers/requests/read/index.ts
@@ -2,8 +2,10 @@ import { createTRPCRouter } from '@/server/api/trpc';
 
 import { applicantRequest } from './applicantRequest';
 import { completedRequest } from './completedRequest';
+import { requestCount } from './requestCount';
 
 export const read = createTRPCRouter({
   applicantRequest,
   completedRequest,
+  requestCount,
 });
