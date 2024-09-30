07878c82982dec71458a3a5e886e60d3b5cb4a71
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-09-30 13:03:08 +0530

    removed un-used hooks

--- apps/aglint-recruiter/src/app/(authenticated)/requests/_common/hooks/index.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/requests/_common/hooks/index.tsx
@@ -2,5 +2,4 @@ export { useCompletedRequests } from './useCompletedRequests';
 export { useMeetingList } from './useMeetingList';
 export { useRequestAvailabilityDetails } from './useRequestAvailabilityDetails';
 export { useRequestCount } from './useRequestCount';
-export { useRequestCounts } from './useRequestCounts';
-export { useRequestWorkflow } from './useRequestWorkflow';
+export { useReadNotes, useUpdateRequestNote } from './useRequestNotes';
