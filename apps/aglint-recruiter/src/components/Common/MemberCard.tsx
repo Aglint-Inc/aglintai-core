6e07bdc3f69a5dc6986d822939c3447bab735dab
Raimon T Simon <raimonrts@gmail.com>
2024-10-03 21:21:35 +0530

    link fix

--- apps/aglint-recruiter/src/components/Common/MemberCard.tsx
+++ apps/aglint-recruiter/src/components/Common/MemberCard.tsx
@@ -2,7 +2,7 @@ import { getFullName } from '@aglint/shared-utils';
 import { Avatar, AvatarFallback, AvatarImage } from '@components/ui/avatar';
 import React from 'react';
 
-import { capitalizeFirstLetter } from '@/utils/text/textUtils';
+// import { capitalizeFirstLetter } from '@/utils/text/textUtils';
 
 function MemberCard({
   selectedMember,
