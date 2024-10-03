43fc6a99483fab7798dc4e48e6a6ec99e1347173
ChandraAglint <chandra@aglinthq.com>
2024-10-03 12:10:17 +0530

    job edit

--- apps/aglint-recruiter/src/app/(authenticated)/jobs/[job]/(job-edit)/candidate-plan/_common/components/index.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/jobs/[job]/(job-edit)/candidate-plan/_common/components/index.tsx
@@ -1,23 +1,29 @@
+import {
+  Page,
+  PageDescription,
+  PageHeader,
+  PageHeaderText,
+  PageTitle,
+} from '@components/layouts/page-header';
 import { useParams } from 'next/navigation';
 
 import ReorderableInterviewPlan from './ReorderableInterviewPlan';
-
 export const CandidatePlan = () => {
   const { job } = useParams();
   const job_id = job as string;
   return (
-    <div className='w-full'>
-      <div className='flex flex-row justify-between'>
-        <div className='flex flex-col gap-2'>
-          <h2 className='mb-2 text-xl font-bold'>Candidate Plan</h2>
-          <p className='mb-4 text-sm text-gray-600'>
+    <Page>
+      <PageHeader>
+        <PageHeaderText>
+          <PageTitle>Candidate Plan</PageTitle>
+          <PageDescription>
             Update the candidate plan here. Changes will be saved automatically.
-          </p>
-        </div>
+          </PageDescription>
+        </PageHeaderText>
+      </PageHeader>
+      <div>
+        <ReorderableInterviewPlan jobId={job_id} applicationId={null} />
       </div>
-      <div className='my-8 mb-10 max-w-2xl space-y-4'>
-        <ReorderableInterviewPlan jobId={job_id} applicationId={null} />;
-      </div>
-    </div>
+    </Page>
   );
 };
