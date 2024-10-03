483fc583be5bf0c7ea1a0323c2e603ab2feb0e6b
ChandraAglint <chandra@aglinthq.com>
2024-10-03 16:26:16 +0530

    lint fix

--- apps/aglint-recruiter/src/app/(authenticated)/providers.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/providers.tsx
@@ -3,11 +3,11 @@
 import { TooltipProvider } from '@radix-ui/react-tooltip';
 import { type PropsWithChildren } from 'react';
 
+import { OnboardingProvider } from '@/components/Navigation/OnboardPending/context/onboarding';
 import { BreadcrumProvider } from '@/context/BreadcrumContext/BreadcrumContext';
 import { RolesAndPermissionsProvider } from '@/context/RolesAndPermissions/RolesAndPermissionsContext';
 import { JobsProvider } from '@/jobs/contexts';
 import { WorkflowsProvider } from '@/workflows/contexts';
-import { OnboardingProvider } from '@/components/Navigation/OnboardPending/context/onboarding';
 
 export const Provider = ({ children }: PropsWithChildren) => {
   return (
