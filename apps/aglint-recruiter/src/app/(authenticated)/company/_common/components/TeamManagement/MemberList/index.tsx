47bccc56ad1cb9266454f7e6c86d5fd93976f101
Raimon T Simon <raimonrts@gmail.com>
2024-10-04 20:23:58 +0530

    roles and permissions ui update

--- apps/aglint-recruiter/src/app/(authenticated)/company/_common/components/TeamManagement/MemberList/index.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/company/_common/components/TeamManagement/MemberList/index.tsx
@@ -64,7 +64,7 @@ const Member = ({
       </TableCell>
       <TableCell>
         <Badge 
-          className={`
+          className={`rounded-sm
             ${member.status === 'active' ? 'bg-green-100 text-green-800 hover:bg-green-100 hover:text-green-800' : ''} 
             ${member.status === 'invited' ? 'bg-blue-100 text-blue-800 hover:bg-blue-100 hover:text-blue-800' : ''} 
             ${member.status === 'suspended' ? 'bg-red-100 text-red-800 hover:bg-red-100 hover:text-red-800' : ''}
