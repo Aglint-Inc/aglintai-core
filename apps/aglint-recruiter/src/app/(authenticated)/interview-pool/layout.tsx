528955adde9c17224e7627e29d1203f0f60fb878
crc <chinmai@aglinthq.com>
2024-10-07 15:24:50 +0530

    added ssr

---
+++ apps/aglint-recruiter/src/app/(authenticated)/interview-pool/layout.tsx
@@ -0,0 +1,15 @@
+import { unstable_noStore as noStore } from 'next/cache';
+import dynamic from 'next/dynamic';
+import { type PropsWithChildren } from 'react';
+
+import { api, HydrateClient } from '@/trpc/server';
+
+const Layout = async ({ children }: PropsWithChildren) => {
+  noStore();
+
+  void api.interview_pool.list.prefetch();
+
+  return <HydrateClient>{children}</HydrateClient>;
+};
+
+export default Layout;
