f706fe22bc589bfcd19ac1551c5e38f71ffc9763
Dileep BC <dileep@aglinthq.com>
2024-10-07 20:51:28 +0530

    fix : workflow build

--- apps/aglint-recruiter/src/app/(authenticated)/workflows/layout.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/workflows/layout.tsx
@@ -1,3 +1,4 @@
+import { unstable_noStore } from 'next/cache';
 import { type PropsWithChildren } from 'react';
 
 import { api, HydrateClient } from '@/trpc/server';
@@ -7,6 +8,7 @@ import {
 } from '@/workflows/contexts';
 
 const Layout = async ({ children }: PropsWithChildren) => {
+  unstable_noStore();
   void api.workflows.read.prefetch();
   return (
     <HydrateClient>
