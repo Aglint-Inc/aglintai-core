269be369b74bd477b29f315cf9407625fade3c06
Punith G <punithg@aglinthq.com>
2024-10-08 12:41:18 +0530

    drop_warning_ignore_2

--- apps/aglint-recruiter/src/app/(authenticated)/jobs/[job]/_common/components/Banners/InterviewPlan.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/jobs/[job]/_common/components/Banners/InterviewPlan.tsx
@@ -3,9 +3,7 @@ import { useJob } from '@/job/hooks';
 export const InterviewPlan = () => {
   const {
     interviewPlans: { data, status },
-    job: { interview_plan_warning_ignore },
   } = useJob();
   if (status !== 'success') return <></>;
-  if (interview_plan_warning_ignore) return <></>;
   if (!data || data.length === 0) return <>Interview plan not set</>;
 };
