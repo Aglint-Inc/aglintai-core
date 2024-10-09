207174db9b3bb488943bfc707db1e3cb6f98f587
Raimon T Simon <raimonrts@gmail.com>
2024-10-09 14:16:55 +0530

    ui changes in application detail

--- apps/aglint-recruiter/components/interviewStageConnector.tsx
+++ apps/aglint-recruiter/components/interviewStageConnector.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
+import { statusColors } from './statusColors';
+
 interface InterviewStageConnectorProps {
   size: number;
-  position: 'bottom' | 'right'; 
+  position: 'bottom' | 'right' | 'top';
+  status: 'completed' | 'confirmed' | 'not_scheduled';
+  isActive: boolean;
 }
 
 const InterviewStageConnector: React.FC<InterviewStageConnectorProps> = ({
   size,
   position,
+  status,
+  isActive,
 }) => {
-  const innerSize = (size / Math.sqrt(2)).toFixed(2); 
+  const innerSize = (size / Math.sqrt(2)).toFixed(2);
   const absoluteStyle = size / 2 - 1;
 
   const parentDivStyle =
@@ -21,22 +27,40 @@ const InterviewStageConnector: React.FC<InterviewStageConnectorProps> = ({
           justifyContent: 'center',
           width: '100%',
         }
-      : {
-          width: `${absoluteStyle}px`,
-          right: `${-(absoluteStyle - 1)}px`,
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-          height: '100%',
-        };
+      : position === 'top'
+        ? {
+            height: `${absoluteStyle}px`,
+            top: `0px`,
+            alignItems: 'flex-end',
+            justifyContent: 'center',
+            width: '100%',
+          }
+        : {
+            width: `${absoluteStyle}px`,
+            right: `${-(absoluteStyle - 1)}px`,
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            height: '100%',
+          };
 
   return (
     <div className='absolute z-20 flex overflow-hidden' style={parentDivStyle}>
       <div
-        className='flex items-center justify-center bg-red-500'
+        className='flex items-center justify-center'
         style={{ width: `${size}px`, height: `${size}px` }}
       >
         <div
-          className='absolute rotate-45 transform border border-blue-500 bg-green-500 '
+          className={`absolute rotate-45 transform rounded-br-sm border ${
+            position === 'bottom'
+              ? isActive
+                ? `${statusColors[status].activeBg} ${statusColors[status].activeBorder}`
+                : `${statusColors[status].bg} ${statusColors[status].border}`
+              : position === 'top'
+                ? isActive
+                  ? `bg-white ${statusColors[status].activeBorder}`
+                  : `bg-white ${statusColors[status].border}`
+                : ''
+          }`}
           style={{ width: `${innerSize}px`, height: `${innerSize}px` }}
         ></div>
       </div>
