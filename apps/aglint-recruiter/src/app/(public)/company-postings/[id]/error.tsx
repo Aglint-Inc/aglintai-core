180dbdae93e1f8282966e754b789af4a4108dbf3
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-10-09 17:13:34 +0530

    9th oct fixes (#2706)
    * fix: candidate availability ui
    
    * fix: candidate availability date update
    
    * fix: lint error
    
    * fix: speling check in public URL
    
    * fix: speling check in public URL
    
    * fix: handle empty cases for location

--- apps/aglint-recruiter/src/app/(public)/company-postings/[id]/error.tsx
+++ apps/aglint-recruiter/src/app/(public)/company-postings/[id]/error.tsx
@@ -5,12 +5,12 @@ import React from 'react';
 
 const JobNotFound = () => {
   return (
-    <div className='rounded-lgbg-muted mx-auto flex max-w-sm flex-col items-center justify-center p-4 text-center shadow-md'>
+    <div className='mx-auto flex max-w-sm flex-col items-center justify-center rounded-lg bg-muted p-4 text-center shadow-md'>
       <AlertCircle className='mb-4 h-24 w-24 text-destructive' />
       <h3 className='mb-2 text-2xl font-semibold'>Job not found</h3>
       <p className='text-gray-600'>
-        Sorry, we couldn&apos;t find the job you&apos;re looking for. Please try
-        searching again or contact support.
+        {`Sorry, we couldn't find the job you're looking for. Please try
+        searching again or contact support.`}
       </p>
     </div>
   );
