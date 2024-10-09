82d02228a8f55c219bcf356497776de8b2cee01a
chinmaicraglint <125248276+chinmaicraglint@users.noreply.github.com>
2024-10-09 21:59:23 +0530

    Fix redesign self schedule (#2717)
    wip

--- apps/aglint-recruiter/src/app/(public)/_common/_components/SelectedSessionStotsCard.tsx
+++ apps/aglint-recruiter/src/app/(public)/_common/_components/SelectedSessionStotsCard.tsx
@@ -1,5 +1,6 @@
 import { type DatabaseTable } from '@aglint/shared-types';
 import { dayjsLocal } from '@aglint/shared-utils';
+import { cn } from '@lib/utils';
 import { CheckCircle, Timer } from 'lucide-react';
 
 import { UIBadge } from '@/common/UIBadge';
@@ -13,6 +14,7 @@ export function SelectedSessionSlotsCard({
   textSelectedSlots,
   selectedDates,
   isSubmitted,
+  isActive,
 }: {
   textDayCount: string;
   isSelected: boolean;
@@ -24,56 +26,63 @@ export function SelectedSessionSlotsCard({
     DatabaseTable['candidate_request_availability']['slots']
   >[number]['dates'];
   isSubmitted: boolean;
+  isActive: boolean;
 }) {
   return (
-    <div className='w-full'>
-      <div className='space-y-4 p-4'>
-        <div className='flex items-start justify-between'>
-          <div>
-            <h1 className='mb-2 text-black'>{textDayCount}</h1>
-            <div className='flex items-center space-x-2 text-sm text-muted-foreground'>
-              <Timer className='h-5 w-5 text-muted-foreground' />
-              <span>Total Duration: {textTotalDuration}</span>
-            </div>
-          </div>
-        </div>
-        <div className='space-y-2'>{slotSessionInfo}</div>
-      </div>
-      {isSelected ? (
-        <>
-          <hr className='border-t border-neutral-200' />
-          <div className='space-y-4 p-4'>
-            <div className='flex items-center justify-between'>
-              <div className='flex items-center space-x-2'>
-                <CheckCircle className='h-4 w-4 text-green-800' />
-                <span className='text-sm text-muted-foreground'>
-                  {textSelectedSlots}
-                </span>
+    <div
+      className={cn('rounded-lg border', {
+        'border-blue-500': isActive,
+      })}
+    >
+      <div className='w-full'>
+        <div className='space-y-4 p-4'>
+          <div className='flex items-start justify-between'>
+            <div>
+              <h1 className='mb-2 text-black'>{textDayCount}</h1>
+              <div className='flex items-center space-x-2 text-sm text-muted-foreground'>
+                <Timer className='h-5 w-5 text-muted-foreground' />
+                <span>Total Duration: {textTotalDuration}</span>
               </div>
-              {slotChangeButton}
-            </div>
-            <div className='flex flex-col'>
-              {selectedDates.map((ele, i) => {
-                return (
-                  <SelectedSlot
-                    key={i}
-                    textDate={dayjsLocal(ele.curr_day).format('DD MMMM YYYY')}
-                    slotBadge={ele.slots.map((slot, i) => {
-                      return (
-                        <UIBadge
-                          color={isSubmitted ? 'success' : 'warning'}
-                          key={i}
-                          textBadge={`${dayjsLocal(slot.startTime).format('hh:mm A')} - ${dayjsLocal(slot.endTime).format('hh:mm A')}`}
-                        />
-                      );
-                    })}
-                  />
-                );
-              })}
             </div>
           </div>
-        </>
-      ) : null}
+          <div className='space-y-2'>{slotSessionInfo}</div>
+        </div>
+        {isSelected ? (
+          <>
+            <hr className='border-t border-neutral-200' />
+            <div className='space-y-4 p-4'>
+              <div className='flex items-center justify-between'>
+                <div className='flex items-center space-x-2'>
+                  <CheckCircle className='h-4 w-4 text-green-800' />
+                  <span className='text-sm text-muted-foreground'>
+                    {textSelectedSlots}
+                  </span>
+                </div>
+                {slotChangeButton}
+              </div>
+              <div className='flex flex-col'>
+                {selectedDates.map((ele, i) => {
+                  return (
+                    <SelectedSlot
+                      key={i}
+                      textDate={dayjsLocal(ele.curr_day).format('DD MMMM YYYY')}
+                      slotBadge={ele.slots.map((slot, i) => {
+                        return (
+                          <UIBadge
+                            color={isSubmitted ? 'success' : 'warning'}
+                            key={i}
+                            textBadge={`${dayjsLocal(slot.startTime).format('hh:mm A')} - ${dayjsLocal(slot.endTime).format('hh:mm A')}`}
+                          />
+                        );
+                      })}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </>
+        ) : null}
+      </div>
     </div>
   );
 }
--- apps/aglint-recruiter/src/server/api/routers/user/update_current_user/index.ts
+++ apps/aglint-recruiter/src/server/api/routers/user/update_current_user/index.ts
@@ -10,9 +10,8 @@ const mutation = async ({
   const db = createPrivateClient();
   await db
     .from('recruiter_user')
-    //@ts-ignore  remove ignore when strict mode is enabled in tsconfig
     .update({ ...input })
-    .eq('id', ctx.user_id)
+    .eq('user_id', ctx.user_id)
     .throwOnError();
 
   return true;
