546e2f1981a2c2e03e0001a320bf0be7b4109f12
VivekS <vivek@aglinthq.com>
2024-10-10 16:27:28 +0530

    feat:scheduling reports

---
+++ apps/aglint-recruiter/src/app/(authenticated)/reports/scheduling/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react';
+
+import Reasons from '../_common/components/scheduling/reasons';
+
+function SchedulingPage() {
+  return <Reasons />;
+}
+
+export default SchedulingPage;
