b81b595e47b5c21bb0e24a3bb2d59bc7d62358e3
Dileep BC <dileep@aglinthq.com>
2024-10-12 17:13:51 +0530

    fix : migration

--- supabase/migrations/20241010151955_cron2.sql
+++ supabase/migrations/20241010151955_cron2.sql
@@ -97,3 +97,10 @@ $function$
 CREATE TRIGGER exec_immidiate_job AFTER INSERT ON public.workflow_action_logs FOR EACH ROW EXECUTE FUNCTION executeimmediatejoboninsert();
 
 
+alter table "public"."public_jobs" alter column "parameter_weights" set default '{"skills": 0, "education": 0, "experience": 0}'::jsonb;
+
+alter table "public"."request_progress" drop column "target_api";
+
+alter table "public"."request_progress" add column "grouped_progress_id" uuid not null;
+
+
