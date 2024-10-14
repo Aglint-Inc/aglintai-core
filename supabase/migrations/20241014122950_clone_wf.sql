c962426978a7d3db57978b5ccf9be6901c482364
Dileep BC <dileep@aglinthq.com>
2024-10-14 18:10:06 +0530

    fix : merge migration file

--- supabase/migrations/20241014122950_clone_wf.sql
+++ supabase/migrations/20241014122950_clone_wf.sql
@@ -1,3 +1,19 @@
+drop trigger if exists "event_trigger_candidate_request_availability_delete" on "public"."candidate_request_availability";
+
+drop trigger if exists "event_trigger_interview_filter_json_delete" on "public"."interview_filter_json";
+
+drop trigger if exists "event_trigger_interview_filter_json_insert" on "public"."interview_filter_json";
+
+drop trigger if exists "event_trigger_interview_meeting_delete" on "public"."interview_meeting";
+
+drop trigger if exists "event_trigger_interview_meeting_insert" on "public"."interview_meeting";
+
+drop trigger if exists "event_trigger_interview_session_cancel_insert" on "public"."interview_session_cancel";
+
+drop trigger if exists "event_trigger_request_update" on "public"."request";
+
+
+
 drop trigger if exists "event_trigger_request_insert" on "public"."request";
 
 set check_function_bodies = off;
