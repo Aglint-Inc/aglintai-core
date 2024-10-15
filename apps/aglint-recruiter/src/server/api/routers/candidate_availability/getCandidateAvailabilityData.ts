5b62a6d156e0b13c530a7eda0f26b5ac64d7444c
Punith G <punithg@aglinthq.com>
2024-10-15 19:40:36 +0530

    getCandidateAvailabilityData

--- apps/aglint-recruiter/src/server/api/routers/candidate_availability/getCandidateAvailabilityData.ts
+++ apps/aglint-recruiter/src/server/api/routers/candidate_availability/getCandidateAvailabilityData.ts
@@ -1,31 +1,35 @@
+import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
-
-import { createPublicClient } from '@/server/db';
 
 import {
   type ProcedureDefinition,
   type PublicProcedure,
   publicProcedure,
-} from '../../trpc';
+} from '@/server/api/trpc';
+import { createPublicClient } from '@/server/db';
 
 const schema = z.object({
   candidate_request_availability_id: z.string().uuid(),
 });
 
 const query = async ({ input }: PublicProcedure<typeof schema>) => {
-  const { candidate_request_availability_id } = input;
   const db = createPublicClient();
-  if (candidate_request_availability_id) {
-    const { data } = await db
+  const data = (
+    await db
       .from('candidate_request_availability')
       .select(
         '*,request_session_relation(interview_session(*)),applications!inner(candidate_id,candidates!inner(*),public_jobs!inner(job_title,job_type,office_locations(city,region,country))),recruiter!inner(id,logo,name)',
       )
-      .eq('id', candidate_request_availability_id)
+      .eq('id', input.candidate_request_availability_id)
       .single()
-      .throwOnError();
-    if (data) return data;
-  }
+      .throwOnError()
+  ).data;
+  if (!data)
+    throw new TRPCError({
+      code: 'NOT_FOUND',
+      message: 'Candidate Request not found',
+    });
+  return data;
 };
 
 export const getCandidateAvailabilityData = publicProcedure
