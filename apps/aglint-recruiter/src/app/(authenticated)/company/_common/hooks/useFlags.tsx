6f8c4b453056bb7a49ea947325700aaf1ed583b6
Punith G <punithg@aglinthq.com>
2024-10-15 19:06:23 +0530

    useFlags

--- apps/aglint-recruiter/src/app/(authenticated)/company/_common/hooks/useFlags.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/company/_common/hooks/useFlags.tsx
@@ -1,5 +1,4 @@
-import { useCallback } from 'react';
-
+import type { Flags } from '@/routers/tenant/flags';
 import { api } from '@/trpc/client';
 
 type Features =
@@ -15,33 +14,40 @@ type Features =
   | 'AGENT'
   | 'THEMES';
 
-export const useFlags = () => {
+type Output = Flags['output'] & ReturnType<typeof useShowFlag>;
+
+export const useFlags = (): Output => {
   const preferences = api.tenant.flags.useSuspenseQuery(undefined, {
     staleTime: Infinity,
   })[0];
 
-  const isShowFeature = useCallback(
-    (feature: Features) => {
-      if (preferences) {
-        const recruiterPref: Record<Features, boolean> = {
-          SCORING: preferences.scoring,
-          INTEGRATIONS: preferences.integrations,
-          ROLES: Boolean(preferences.roles),
-          REQUESTS: preferences.request,
-          WORKFLOW: preferences.workflow,
-          SCHEDULING: preferences.scheduling,
-          ANALYTICS: preferences.analytics,
-          CANDIDATE_PORTAL: preferences.candidate_portal,
-          AGENT: preferences.agent,
-          REPORTS: preferences.reports,
-          THEMES: preferences.themes,
-        };
-        return recruiterPref[feature];
-      }
-      return false;
-    },
-    [preferences],
-  );
+  const { isShowFeature } = useShowFlag(preferences);
 
   return { ...preferences, isShowFeature };
+};
+
+const useShowFlag = (preferences: Flags['output']) => {
+  const isShowFeature = (feature: Features) =>
+    showFeature(feature, preferences);
+  return { isShowFeature };
+};
+
+const showFeature = (feature: Features, preferences: Flags['output']) => {
+  if (preferences) {
+    const recruiterPref: Record<Features, boolean> = {
+      SCORING: preferences.scoring,
+      INTEGRATIONS: preferences.integrations,
+      ROLES: Boolean(preferences.roles),
+      REQUESTS: preferences.request,
+      WORKFLOW: preferences.workflow,
+      SCHEDULING: preferences.scheduling,
+      ANALYTICS: preferences.analytics,
+      CANDIDATE_PORTAL: preferences.candidate_portal,
+      AGENT: preferences.agent,
+      REPORTS: preferences.reports,
+      THEMES: preferences.themes,
+    };
+    return recruiterPref[feature];
+  }
+  return false;
 };
