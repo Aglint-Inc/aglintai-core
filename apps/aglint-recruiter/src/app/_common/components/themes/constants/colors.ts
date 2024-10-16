16e91fdd4e3d4add99152d5fc13ce69b2192dca8
Punith G <91651608+MlNl-PEKKA@users.noreply.github.com>
2024-10-16 20:46:57 +0530

    Themes (#2773)
    themes

---
+++ apps/aglint-recruiter/src/app/_common/components/themes/constants/colors.ts
@@ -0,0 +1,14 @@
+export const COLORS = [
+  'theme-zinc',
+  'theme-slate',
+  'theme-stone',
+  'theme-gray',
+  'theme-neutral',
+  'theme-red',
+  'theme-rose',
+  'theme-orange',
+  'theme-green',
+  'theme-blue',
+  'theme-yellow',
+  'theme-violet',
+] as const;
