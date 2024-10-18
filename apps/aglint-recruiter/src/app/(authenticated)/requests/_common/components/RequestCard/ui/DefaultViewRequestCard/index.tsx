34d969de4dcef47cd5dbcb1aa092a6dc37d3aaff
Raimon T Simon <raimonrts@gmail.com>
2024-10-18 20:58:51 +0530

    request card update

--- apps/aglint-recruiter/src/app/(authenticated)/requests/_common/components/RequestCard/ui/DefaultViewRequestCard/index.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/requests/_common/components/RequestCard/ui/DefaultViewRequestCard/index.tsx
@@ -11,21 +11,12 @@ import {
 } from '@components/ui/tooltip';
 import { cn } from '@lib/utils';
 import { type RequestProps } from '@requests/types';
-import {
-  Calendar,
-  ChevronDown,
-  ChevronUp,
-  CircleDashed,
-  Clock,
-  StickyNote,
-  Tag,
-  User,
-  UserCircle,
-} from 'lucide-react';
+import { ChevronDown, ChevronUp } from 'lucide-react';
 import Link from 'next/link';
 import React, { type Dispatch, type SetStateAction } from 'react';
 
 import { capitalizeFirstLetter } from '@/utils/text/textUtils';
+import IconFlex from './IconFlex';
 
 function DefaultViewRequestCard({
   props,
@@ -44,14 +35,13 @@ function DefaultViewRequestCard({
   return (
     <Card
       className={cn(
-        'group transition-shadow duration-300',
-        isCompactList &&
-          'rounded-none border-none shadow-none hover:bg-gray-50',
+        'duration-400 group border-border transition-all hover:border-foreground/50',
+        isCompactList && 'rounded-none border-none shadow-none',
       )}
     >
       <div
         className={cn(
-          'flex items-center justify-between p-4',
+          'flex items-center justify-between p-6',
           isCompactList && 'px-2 py-3',
         )}
       >
@@ -72,10 +62,6 @@ function DefaultViewRequestCard({
             >
               {props.title}
             </Label>
-            <div className='flex items-center text-sm'>
-              <Clock strokeWidth={1.5} className='mr-1 h-4 w-4' />
-              {dayjsLocal(props.updated_at).fromNow()}{' '}
-            </div>
             {isCompactList ? (
               <Button
                 variant='ghost'
@@ -101,203 +87,131 @@ function DefaultViewRequestCard({
       {(mode === 'expanded' || (isCompactList && isExpanded)) && (
         <CardContent
           className={cn(
-            'space-y-4 p-4 pt-0',
+            'space-y-4 p-6 pt-0',
             isCompactList && 'space-y-2 bg-gray-50 p-2',
           )}
         >
-          <div
-            className={cn(
-              '',
-              mode === 'compact-list' && '',
-              mode !== 'compact-list' && '',
-            )}
-          >
-            <div className='space-y-3'>
-              <div className='flex gap-8'>
-                <InfoItem
-                  icon={
-                    <CircleDashed className='h-4 w-4 text-muted-foreground' />
-                  }
-                  label='Request Status'
-                  variant='column'
-                  value={
-                    <Badge
-                      className='text-xs'
-                      variant={
-                        props?.status === 'to_do'
-                          ? 'secondary'
-                          : props?.status === 'in_progress'
-                            ? 'in_progress'
-                            : props?.status === 'completed'
-                              ? 'completed'
-                              : 'destructive'
-                      }
-                    >
-                      {capitalizeFirstLetter(props.status)}
-                    </Badge>
-                  }
-                />
-                <InfoItem
-                  label='Request Type'
-                  icon={<Tag className='h-4 w-4 text-muted-foreground' />}
-                  variant='column'
-                  value={
-                    <Badge
-                      className='text-xs'
-                      variant={
-                        props?.type === 'decline_request'
-                          ? 'destructive'
-                          : 'secondary'
-                      }
-                    >
-                      {capitalizeFirstLetter(props.type)}
-                    </Badge>
-                  }
-                />
-                {props.request_note[0]?.note && (
-                  <InfoItem
-                    label='Note'
-                    icon={
-                      <StickyNote className='h-4 w-4 text-muted-foreground' />
-                    }
-                    variant='column'
-                    value={
-                      <TooltipProvider>
-                        <Tooltip>
-                          <TooltipTrigger>
-                            {/* {props.request_note[0]?.note} */}
-                            <p className='line-clamp-1 max-w-[200px] items-start text-sm'>
-                              {props.request_note[0]?.note}
-                            </p>
-                          </TooltipTrigger>
-                          <TooltipContent className='max-w-xs p-2'>
-                            <p className='text-sm'>
-                              {props.request_note[0]?.note}
-                            </p>
-                          </TooltipContent>
-                        </Tooltip>
-                      </TooltipProvider>
-                    }
-                  />
-                )}
-              </div>
-              <div className='flex gap-8'>
-                <div className='flex items-start space-y-3'>
-                  <InfoItem
-                    icon={
-                      <UserCircle className='h-4 w-4 text-muted-foreground' />
-                    }
-                    label='Requested By'
-                    variant='column'
-                    value={
-                      <>
-                        <div className='flex flex-row items-center gap-4'>
-                          <div className='flex flex-row items-center space-x-2 text-sm'>
-                            <Link
-                              href={`/user/${props.assigner_id}`}
-                              target='_blank'
-                              className='hover:underline'
-                            >
-                              {getFullName(
-                                props?.assigner?.first_name ?? '',
-                                props?.assigner?.last_name ?? '',
-                              )}
-                              {props.assigner_id === currentUserId
-                                ? ' (You)'
-                                : ''}
-                            </Link>
-                          </div>
-                        </div>
-                      </>
-                    }
-                  />
+          <div className={`flex flex-col gap-4`}>
+            <div className='flex flex-row gap-6'>
+              <IconFlex icon='CircleDashed' size={16}>
+                <Badge
+                  className={`rounded-md text-sm font-normal 
+                    ${props.status === 'to_do' ? 'bg-blue-100 text-blue-600 dark:bg-blue-800 dark:text-white' : ''} 
+                    ${props.status === 'in_progress' ? 'bg-yellow-100 text-yellow-600 dark:bg-yellow-800 dark:dark:text-white' : ''} 
+                    ${props.status === 'completed' ? 'bg-green-100 text-green-600 dark:bg-green-800 dark:dark:text-white' : ''} 
+                    `}
+                >
+                  {capitalizeFirstLetter(props.status)}
+                </Badge>
+              </IconFlex>
+              <IconFlex icon='Tag' size={16}>
+                <Badge
+                  className='rounded-md text-sm font-normal bg-muted text-foreground '
+                >
+                  {capitalizeFirstLetter(props.type)}
+                </Badge>
+              </IconFlex>
+              <IconFlex icon='CalendarClock' size={18}>
+                <div className='text-sm'>
+                  {props.request_relation.length > 0}
+                  {dayjsLocal(props.schedule_start_date).format('MMMM D, YYYY')}
                 </div>
-                <div className='flex items-start space-y-3'>
-                  <InfoItem
-                    icon={
-                      <UserCircle className='h-4 w-4 text-muted-foreground' />
-                    }
-                    label='Assigned To'
-                    variant='column'
-                    value={
-                      <>
-                        <div className='flex flex-row items-center gap-4'>
-                          <div className='flex flex-row items-center space-x-2 text-sm'>
-                            <Link
-                              href={`/user/${props.assignee_id}`}
-                              target='_blank'
-                              className='hover:underline'
-                            >
-                              {getFullName(
-                                props?.assignee?.first_name ?? '',
-                                props?.assignee?.last_name ?? '',
-                              )}
-                              {props.assignee_id === currentUserId
-                                ? ' (You)'
-                                : ''}
-                            </Link>
-                          </div>
-                        </div>
-                      </>
-                    }
-                  />
-                </div>
-              </div>
-              <InfoItem
-                icon={<Calendar className='h-4 w-4' />}
-                label='Proposed Interview Date'
-                variant='column'
-                value={
-                  <>
-                    <div className='mt-1'>
-                      {props.request_relation.map((ele, index) => (
-                        <span key={index}>
-                          <Link
-                            href={`/application/${props.application_id}/sessions/${ele.interview_session?.id}`}
-                            className='hover:underline'
-                          >
-                            {ele.interview_session?.name}
-                          </Link>
-                          {index < props.request_relation.length - 1 &&
-                            (index === props.request_relation.length - 2
-                              ? ' and '
-                              : ', ')}
-                        </span>
-                      ))}
-                      {props.request_relation.length > 0 && ' on '}
-                      {dayjsLocal(props.schedule_start_date).format(
-                        'MMMM D, YYYY',
-                      )}
-                    </div>
-                  </>
-                }
-              />
-              <InfoItem
-                icon={<User className='h-4 w-4 text-muted-foreground' />}
-                label='Candidate and Job'
-                variant='column'
-                value={
-                  <>
-                    <Link
-                      href={`/candidate/${props?.applications?.candidates?.id}`}
-                      className='hover:underline'
-                    >
-                      {getFullName(
-                        props?.applications?.candidates?.first_name ?? '',
-                        props?.applications?.candidates?.last_name ?? '',
-                      )}
-                    </Link>
-                    {' for '}
-                    <Link
-                      href={`/job/${props?.applications?.public_jobs?.id}`}
-                      className='hover:underline'
-                    >
-                      {props?.applications?.public_jobs?.job_title}
-                    </Link>
-                  </>
-                }
-              />
+              </IconFlex>
             </div>
+            <div className='flex flex-row gap-6'>
+              <IconFlex icon='UserSquare' size={16}>
+                <div className='flex items-center gap-1.5'>
+                  {props.request_relation.map((ele, index) => (
+                    <Badge
+                      key={index}
+                      variant={'secondary'}
+                      className='rounded-md text-sm font-normal hover:bg-muted/80'
+                    >
+                      <Link
+                        href={`/application/${props.application_id}/sessions/${ele.interview_session?.id}`}
+                        className='hover:no-underline'
+                      >
+                        {ele.interview_session?.name}
+                      </Link>
+                    </Badge>
+                  ))}
+                </div>
+              </IconFlex>
+            </div>
+            <div className='flex flex-row gap-6'>
+              <IconFlex icon='Briefcase' size={16}>
+                <Link
+                  href={`/job/${props?.applications?.public_jobs?.id}`}
+                  className='text-sm hover:underline'
+                >
+                  {props?.applications?.public_jobs?.job_title}
+                </Link>
+              </IconFlex>
+              <IconFlex icon='User' size={16}>
+                <Link
+                  href={`/candidate/${props?.applications?.candidates?.id}`}
+                  className='hover:underline'
+                >
+                  {getFullName(
+                    props?.applications?.candidates?.first_name ?? '',
+                    props?.applications?.candidates?.last_name ?? '',
+                  )}
+                </Link>
+              </IconFlex>
+            </div>
+            <div className='flex flex-row gap-6'>
+              <IconFlex icon='CircleUser' size={16}>
+                <span className='mr-2 text-sm'>Assigned to</span>
+                <Link
+                  href={`/user/${props.assignee_id}`}
+                  target='_blank'
+                  className='text-sm hover:underline'
+                >
+                  {getFullName(
+                    props?.assignee?.first_name ?? '',
+                    props?.assignee?.last_name ?? '',
+                  )}
+                  {props.assignee_id === currentUserId ? ' (You)' : ''}
+                </Link>
+              </IconFlex>
+              <IconFlex icon='FileInput' size={16}>
+                <span className='mr-2 text-sm'>Requested by</span>
+                <Link
+                  href={`/user/${props.assigner_id}`}
+                  target='_blank'
+                  className='text-sm hover:underline'
+                >
+                  {getFullName(
+                    props?.assigner?.first_name ?? '',
+                    props?.assigner?.last_name ?? '',
+                  )}
+                  {props.assigner_id === currentUserId ? ' (You)' : ''}
+                </Link>
+                <span className='mr-1 text-sm'>,</span>
+                <span className='text-sm'>
+                  {' '}
+                  {dayjsLocal(props.updated_at).fromNow()}{' '}
+                </span>
+              </IconFlex>
+            </div>
+            {props.request_note[0]?.note && (
+              <div className='flex flex-row gap-6'>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger className='pt-[10px]'>
+                      <IconFlex icon='StickyNote' size={16}>
+                        <p className='line-clamp-1 max-w-[500px] items-start text-sm'>
+                          {props.request_note[0].note}
+                        </p>
+                      </IconFlex>
+                    </TooltipTrigger>
+                    <TooltipContent className='max-w-[500px]'>
+                      <p className='text-sm'>{props.request_note[0].note}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </div>
+            )}
           </div>
         </CardContent>
       )}
@@ -307,55 +221,55 @@ function DefaultViewRequestCard({
 
 export default DefaultViewRequestCard;
 
-const InfoItem = ({
-  icon,
-  label,
-  value,
-  variant = 'row', // Added variant prop
-}: {
-  icon: React.ReactNode;
-  label: string;
-  value: React.ReactNode;
-  variant?: 'row' | 'column'; // Added variant type
-}) => {
-  if (variant === 'column') {
-    return (
-      <div className='flex flex-col items-start space-x-2'>
-        <div className='mt-1 flex flex-row items-center gap-1'>
-          <div>
-            {React.cloneElement(icon as React.ReactElement, {
-              className: 'w-4 h-4',
-            })}
-          </div>
-          <p className='text-sm text-muted-foreground'>{label}</p>
-        </div>
-        <div className='flex flex-row items-start gap-1 pl-2'>
-          {typeof value === 'string' ? (
-            <p className='ml-3 text-sm font-medium'>{value}</p>
-          ) : (
-            <p className='text-sm'>{value}</p>
-          )}
-        </div>
-      </div>
-    );
-  }
+// const InfoItem = ({
+//   icon,
+//   label,
+//   value,
+//   variant = 'row', // Added variant prop
+// }: {
+//   icon: React.ReactNode;
+//   label: string;
+//   value: React.ReactNode;
+//   variant?: 'row' | 'column'; // Added variant type
+// }) => {
+//   if (variant === 'column') {
+//     return (
+//       <div className='flex flex-col items-start space-x-2'>
+//         <div className='mt-1 flex flex-row items-center gap-1'>
+//           <div>
+//             {React.cloneElement(icon as React.ReactElement, {
+//               className: 'w-4 h-4',
+//             })}
+//           </div>
+//           <p className='text-sm text-muted-foreground'>{label}</p>
+//         </div>
+//         <div className='flex flex-row items-start gap-1 pl-2'>
+//           {typeof value === 'string' ? (
+//             <p className='ml-3 text-sm font-medium'>{value}</p>
+//           ) : (
+//             <p className='text-sm'>{value}</p>
+//           )}
+//         </div>
+//       </div>
+//     );
+//   }
 
-  // Default row layout
-  return (
-    <div className='flex items-start space-x-2'>
-      <div className='mt-0.5'>
-        {React.cloneElement(icon as React.ReactElement, {
-          className: 'w-3 h-3',
-        })}
-      </div>
-      <div className='flex flex-row items-center gap-2'>
-        <p className='text-2xs text-muted-foreground'>{label}</p>
-        {typeof value === 'string' ? (
-          <p className='text-xs font-medium'>{value}</p>
-        ) : (
-          value
-        )}
-      </div>
-    </div>
-  );
-};
+//   // Default row layout
+//   return (
+//     <div className='flex items-start space-x-2'>
+//       <div className='mt-0.5'>
+//         {React.cloneElement(icon as React.ReactElement, {
+//           className: 'w-3 h-3',
+//         })}
+//       </div>
+//       <div className='flex flex-row items-center gap-2'>
+//         <p className='text-2xs text-muted-foreground'>{label}</p>
+//         {typeof value === 'string' ? (
+//           <p className='text-xs font-medium'>{value}</p>
+//         ) : (
+//           value
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
