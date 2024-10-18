ba6c92e4b75e53966ba950868b68d31a84de7e3d
Raimon T Simon <raimonrts@gmail.com>
2024-10-18 13:42:55 +0530

    added shortcut info

--- apps/aglint-recruiter/src/app/_common/components/themes/components/ThemeToggle.tsx
+++ apps/aglint-recruiter/src/app/_common/components/themes/components/ThemeToggle.tsx
@@ -1,9 +1,15 @@
+import { Command } from 'lucide-react';
+
 import { ColorToggle } from './ColorToggle';
 import { ModeToggle } from './ModeToggle';
 
 export const ThemeToggle = () => {
   return (
-    <div className='flex gap-2'>
+    <div className='flex gap-2 items-center'>
+      <div className='flex gap-4 items-center text-sm text-muted-foreground'>
+        <div className='flex gap-2 items-center'><Command size={16}/>+ K to toggle colors</div>
+        <div className='flex gap-2 items-center'><Command size={16}/>+  L to toggle theme</div>
+      </div>
       <ModeToggle />
       <ColorToggle />
     </div>
