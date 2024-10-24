1957cc109c77288dcd7e2cfdeb6b18a03c0e4d23
Dileep BC <dileep@aglinthq.com>
2024-10-24 19:53:18 +0530

    fix : report

--- apps/aglint-recruiter/src/utils/report-generation/utils/generateReportForJob.ts
+++ apps/aglint-recruiter/src/utils/report-generation/utils/generateReportForJob.ts
@@ -20,7 +20,9 @@ export const generateReportForJob = async (job_id: string) => {
     job_id,
     'schedule_request',
   );
-  const to_do_requests = allRequests.filter((app) => app.status === 'to_do');
+  const to_do_requests = allRequests
+    .filter((app) => app.status === 'to_do')
+    .slice(0, 5);
   await scheduleRequests({
     allRequests: to_do_requests,
     company_id: job_details.recruiter_id,
