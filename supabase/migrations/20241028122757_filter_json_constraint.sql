48d0c183305069cf019caf587a234ad78d610c1f
Dileep BC <dileep@aglinthq.com>
2024-10-28 20:06:43 +0530

    fix : migrations

--- supabase/migrations/20241028122757_filter_json_constraint.sql
+++ supabase/migrations/20241028122757_filter_json_constraint.sql
@@ -1,3 +1,3 @@
-CREATE UNIQUE INDEX interview_filter_json_request_id_key ON public.interview_filter_json USING btree (request_id);
+-- Add a unique constraint on the request_id column of the interview_filter_json table
 
-alter table "public"."interview_filter_json" add constraint "interview_filter_json_request_id_key" UNIQUE using index "interview_filter_json_request_id_key";
+alter table "public"."interview_filter_json" add constraint "interview_filter_json_request_id_key" UNIQUE (request_id);
