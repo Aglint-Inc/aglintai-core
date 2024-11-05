048d47d80ad4a9c0aeb066367627753fd8cabdce
crc <chinmai@aglinthq.com>
2024-11-05 14:53:22 +0530

    fix

--- apps/aglint-recruiter/src/app/(authenticated)/requests/[request]/_common/components/RequestNextSteps/DebriefSchedule.tsx
+++ apps/aglint-recruiter/src/app/(authenticated)/requests/[request]/_common/components/RequestNextSteps/DebriefSchedule.tsx
@@ -1,16 +1,13 @@
-import { dayjsLocal } from '@aglint/shared-utils';
-
 import { UIButton } from '@/common/UIButton';
 
 import { useFindAvailibility } from '../SelfSchedulingDrawer/_common/hooks/useFindAvailibility';
 import {
-  initialFilters,
   setIsSelfScheduleDrawerOpen,
   useSelfSchedulingFlowStore,
 } from '../SelfSchedulingDrawer/_common/store/store';
 
 const DebriefSchedule = () => {
-  const { findAvailibility } = useFindAvailibility();
+  const { onClickFindAvailability } = useFindAvailibility();
 
   const { fetchingPlan } = useSelfSchedulingFlowStore();
 
@@ -23,13 +20,7 @@ const DebriefSchedule = () => {
         isLoading={fetchingPlan}
         onClick={async () => {
           if (fetchingPlan) return;
-          await findAvailibility({
-            filters: initialFilters,
-            dateRange: {
-              start_date: dayjsLocal().toISOString(),
-              end_date: dayjsLocal().add(14, 'day').toISOString(),
-            },
-          });
+          await onClickFindAvailability();
           setIsSelfScheduleDrawerOpen(true);
         }}
       >
