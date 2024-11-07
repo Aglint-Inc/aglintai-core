9a22dc5b6a5c7a828e37375d7b1a799b2618cb1b
Punith G <punithg@aglinthq.com>
2024-11-07 13:48:06 +0530

    trpc test utils

--- apps/aglint-recruiter/src/server/api/routers/jobs/read.test.ts
+++ apps/aglint-recruiter/src/server/api/routers/jobs/read.test.ts
@@ -1,9 +1,7 @@
 import { privateTestProcedure } from 'playwright/utils/trpc';
 
-privateTestProcedure.describe.parallel(() => {
-  privateTestProcedure('job.read', async ({ api, log }) => {
-    log('Start job.read');
-    await api.jobs.read();
-    log('End job.read');
-  });
+privateTestProcedure('job.read', async ({ api, log }) => {
+  log('Start job.read');
+  await api.jobs.read();
+  log('End job.read');
 });
