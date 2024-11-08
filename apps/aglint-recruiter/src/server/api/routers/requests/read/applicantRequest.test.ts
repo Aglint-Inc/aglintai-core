b9ebaf38da30c9b0c5edebf3e4351d8dc2f06635
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-11-08 20:42:56 +0530

    fix: request api test updated

--- apps/aglint-recruiter/src/server/api/routers/requests/read/applicantRequest.test.ts
+++ apps/aglint-recruiter/src/server/api/routers/requests/read/applicantRequest.test.ts
@@ -3,17 +3,17 @@ import { privateTestProcedure } from 'playwright/utils/trpc';
 
 privateTestProcedure(
   'test case 1: Select first request applicant',
-  async ({ api, db, user_id }) => {
+  async ({ api, db, user_id, log }) => {
     const randomRequestId = await getRequestId({
       db,
       user_id,
     });
     if (randomRequestId) {
-      const requests = await api.requests.read.applicantRequest({
+      log('Start requests.read.applicantRequest');
+      await api.requests.read.applicantRequest({
         request_id: randomRequestId,
       });
-      // eslint-disable-next-line no-console
-      console.log(requests);
+      log('End requests.read.applicantRequest');
     }
   },
 );
